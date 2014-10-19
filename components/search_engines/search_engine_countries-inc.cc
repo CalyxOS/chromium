0a3860597d2d0517b196b4fb2e2fcc666910714a
Jan Engelhardt <jengelh@inai.de>
2014-10-19 14:49:10 +0200

    search_engines: add DuckDuckGo as selectable and default search engine
    Change-Id: Ic40e537471b4f6784e36c199b49e526f7218d0d7

--- components/search_engines/search_engine_countries-inc.cc
+++ components/search_engines/search_engine_countries-inc.cc
@@ -38,6 +38,7 @@ struct EngineAndTier {
 
 // Default (for countries with no better engine set)
 constexpr EngineAndTier engines_default[] = {
+    {SearchEngineTier::kTopEngines, &duckduckgo},
     {SearchEngineTier::kTopEngines, &googleen},
     {SearchEngineTier::kTopEngines, &google},
     {SearchEngineTier::kTopEngines, &bing},
@@ -224,6 +225,7 @@ constexpr EngineAndTier engines_CN[] = {
     {SearchEngineTier::kTopEngines, &sogou},
     {SearchEngineTier::kTopEngines, &so_360},
     {SearchEngineTier::kTopEngines, &google},
+    {SearchEngineTier::kTopEngines, &duckduckgo},
 };
 
 // Colombia
@@ -582,6 +584,7 @@ constexpr EngineAndTier engines_KR[] = {
     {SearchEngineTier::kTopEngines, &bing},
     {SearchEngineTier::kTopEngines, &daum},
     {SearchEngineTier::kTopEngines, &coccoc},
+    {SearchEngineTier::kTopEngines, &duckduckgo},
 };
 
 // Kuwait
