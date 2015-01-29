2890cca2bc434bd6c8964b90037a799695f2d65b
Jan Engelhardt <jengelh@inai.de>
2015-01-29 10:46:40 +0100

    mime_util: force text/x-suse-ymp to be downloaded
    YMP files (YaST One Click Install) are plaintext XML, but also not
    very interesting in themselves. Force them to be stored.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I342d6d898be7e08c2dc32d201d73363f19902403

--- third_party/blink/common/mime_util/mime_util.cc
+++ third_party/blink/common/mime_util/mime_util.cc
@@ -87,6 +87,7 @@ constexpr auto kUnsupportedTextTypes =
         "text/x-csv",
         "text/x-vcf",
         "text/rtf",
+        "text/x-suse-ymp",
         "text/comma-separated-values",
         "text/csv",
         "text/tab-separated-values",
