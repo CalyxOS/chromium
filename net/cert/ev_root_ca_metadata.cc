51015ebfdaa3a0c79ac1e7b09c0b8d166f7ed81c
Jan Engelhardt <jengelh@inai.de>
2015-04-02 12:44:23 +0200

    Remove EV certificates
    The team chose to let EV certificates appear just like normal
    certificates. The web of trust is considered a failure in itself, so
    do not give users a false sense of extra security with EV certs.
    Instead, let them appear just like regular ones.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ia943a861dc5cc173c982c969e4864d5ab2930106

--- net/cert/ev_root_ca_metadata.cc
+++ net/cert/ev_root_ca_metadata.cc
@@ -36,7 +36,19 @@ struct EVMetadata {
   const base::StringPiece policy_oids[kMaxOIDsPerCA];
 };
 
-#include "net/data/ssl/chrome_root_store/chrome-ev-roots-inc.cc"
+static const EVMetadata kEvRootCaMetadata[] = {
+    // need some dummy data to make compiler happy, because
+    // arraysize() is implemented as a convoluted template rather than
+    // the traditional sizeof(x)/sizeof(*x)
+    {
+       {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
+        {
+            "1.3.159.1.17.1",
+            "",
+        },
+    }
+};
 
 #endif  // defined(PLATFORM_USES_CHROMIUM_EV_METADATA)
 }  // namespace
@@ -115,7 +127,6 @@ bool EVRootCAMetadata::RemoveEVCA(const SHA256HashValue& fingerprint) {
 //
 
 bool EVRootCAMetadata::IsEVPolicyOID(der::Input policy_oid) const {
-  LOG(WARNING) << "Not implemented";
   return false;
 }
 
