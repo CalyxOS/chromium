87b79331819ad5f080c285a55ee6b263354c82db
Jan Engelhardt <jengelh@inai.de>
2015-06-02 11:01:50 +0200

    updater: disable updater pings
    Despite auto-updater being arguably disabled (see previous commit),
    Chromium would still send background requests. Kill it.
    (trk:170, trk:171)
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ib29fce5eacd52b6ae0a2b23b535dd49e7d0b20c0

--- components/component_updater/component_updater_url_constants.cc
+++ components/component_updater/component_updater_url_constants.cc
@@ -15,9 +15,9 @@ namespace component_updater {
 // The value of |kDefaultUrlSource| can be overridden with
 // --component-updater=url-source=someurl.
 const char kUpdaterJSONDefaultUrl[] =
-    "https://update.googleapis.com/service/update2/json";
+    "about:blank";
 
 const char kUpdaterJSONFallbackUrl[] =
-    "http://update.googleapis.com/service/update2/json";
+    "about:blank";
 
 }  // namespace component_updater
