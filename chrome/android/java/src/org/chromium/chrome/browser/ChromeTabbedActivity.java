4feef0d71887307847879b5db0d7e0e649b72001
Serg <serg.zhukovsky@gmail.com>
2017-01-31 22:12:27 -0500

    Add exit menu item
    Corrected Exit functionality
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I04b24400b97b1a8e4857b3a8be693746fd782eeb

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -267,6 +267,8 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.DoubleConsumer;
 
+import org.chromium.chrome.browser.ApplicationLifetime;
+
 /**
  * This is the main activity for ChromeMobile when not running in document mode. All the tabs are
  * accessible via a chrome specific tab switching UI.
@@ -2935,6 +2937,8 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
         } else if (id == R.id.close_tab) {
             getCurrentTabModel().closeTab(currentTab, true, false, true);
             RecordUserAction.record("MobileTabClosed");
+        } else if (id == R.id.exit_id) {
+            ApplicationLifetime.terminate(false);
         } else if (id == R.id.close_all_tabs_menu_id) {
             // Close both incognito and normal tabs.
             CloseAllTabsDialog.show(
