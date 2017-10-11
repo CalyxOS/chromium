bd43048c132b0ff759925d0ebed7c28d84d600f9
csagan5 <32685696+csagan5@users.noreply.github.com>
2017-10-11 23:24:58 +0200

    Disable omission of URL elements in Omnibox
    Disable all omissions because they are used throughout the code without
    a corresponding user option to individually disable their usage.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ia5e913b0c031953155077a111287446bde377cde

--- chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.cc
+++ chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.cc
@@ -261,5 +261,5 @@ TemplateURLService* ChromeLocationBarModelDelegate::GetTemplateURLService() {
 // static
 void ChromeLocationBarModelDelegate::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
-  registry->RegisterBooleanPref(omnibox::kPreventUrlElisionsInOmnibox, false);
+  registry->RegisterBooleanPref(omnibox::kPreventUrlElisionsInOmnibox, true);
 }
