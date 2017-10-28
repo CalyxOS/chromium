48625eec896cb6f36a12226aa54c498fabf574e7
csagan5 <32685696+csagan5@users.noreply.github.com>
2017-10-28 10:09:41 +0200

    Inject scripts for AMP, tracking, ads and video
    Remove AMP, tracking and ads from search/news results
    Break Page Visibility API and Fullscreen API for youtube.com and vimeo.com to allow playing videos in background (original Javascript code by timdream)
    Set proper injection script nonce
    Send a random key press to circumvent idle status detection
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I6f153e83d1c9953378289ea59686587b176b2ab6

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -300,6 +300,7 @@
 #include "third_party/blink/renderer/core/page/pointer_lock_controller.h"
 #include "third_party/blink/renderer/core/page/scrolling/fragment_anchor.h"
 #include "third_party/blink/renderer/core/page/scrolling/root_scroller_controller.h"
+#include "extensions/video_bg_play.h"
 #include "third_party/blink/renderer/core/page/scrolling/snap_coordinator.h"
 #include "third_party/blink/renderer/core/page/scrolling/top_document_root_scroller_controller.h"
 #include "third_party/blink/renderer/core/page/spatial_navigation_controller.h"
@@ -377,6 +378,8 @@
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_buffer.h"
 #include "third_party/blink/renderer/platform/wtf/text/text_encoding_registry.h"
+
+#include "extensions/anti_amp_cure.h"
 
 #ifndef NDEBUG
 using WeakDocumentSet = blink::HeapHashSet<blink::WeakMember<blink::Document>>;
@@ -7816,6 +7819,64 @@ void Document::OnPrepareToStopParsing() {
       MilestoneForDelayedAsyncScript::kFinishedParsing);
 }
 
+void Document::injectScripts() {
+  // determine whether this is a search results page
+  const WTF::String& host = url_.Host();
+  if ((host == nullptr) || host.empty())
+    return;
+
+  auto* bodyElement = body();
+  if (!bodyElement)
+    return;
+  int selected = 0;
+  size_t pos1 = host.Find("www.google."), pos2 = host.Find("news.google."), pos3 = url_.GetPath().Find("/search"), pos4 = host.Find("images.google.");
+  if (((pos1 == 0) && (pos3 == 0)) || (pos2 == 0) || (pos4 == 0)) {
+    LOG(INFO) << "injecting AMP removal Javascript payload";
+    selected = 1;
+    // check for eligibility of the video bg fix
+  } else if (
+              ((WTF::kNotFound != host.Find("youtube.com")) && (WTF::kNotFound == host.Find("accounts.youtube.com"))) ||
+              (WTF::kNotFound != host.Find("vimeo.com"))
+            ) {
+    LOG(INFO) << "injecting video-bg-play Javascript payload";
+    selected = 2;
+  } else
+    return;
+
+  // find out which nonce to use
+  const AtomicString& nonce = findFirstScriptNonce();
+
+  HTMLScriptElement* e = MakeGarbageCollected<HTMLScriptElement>(*this, CreateElementFlags());
+  if (selected == 1)
+    e->setTextContent(ANTI_AMP_CURE_JS);
+  else if (selected == 2)
+    e->setTextContent(VIDEO_BG_PLAY_JS);
+  else
+    NOTREACHED();
+
+  if (nonce != g_null_atom) {
+    e->setNonce(nonce);
+  } else
+    LOG(WARNING) << "could not find script nonce to use";
+
+  bodyElement->AppendChild(e);
+}
+
+const AtomicString& Document::findFirstScriptNonce() {
+  HTMLCollection* s = scripts();
+  unsigned source_length = (unsigned)s->length();
+  // all scripts are likely to have the nonce, thus scan only first 10
+  if (source_length > 10)
+    source_length = 10;
+  for (unsigned i = 0; i < source_length; ++i) {
+    Element* element = s->item(i);
+    const AtomicString& nonce = element->nonce();
+    if ((nonce != g_null_atom) && !nonce.empty())
+      return nonce;
+  }
+  return g_null_atom;
+}
+
 void Document::FinishedParsing() {
   TRACE_EVENT_WITH_FLOW0("blink", "Document::FinishedParsing",
                          TRACE_ID_LOCAL(this),
@@ -7896,6 +7957,9 @@ void Document::FinishedParsing() {
     }
 
     frame->Loader().FinishedParsing();
+    if (!IsPrefetchOnly()) {
+       injectScripts();
+    }
   }
 
   // Schedule dropping of the ElementDataCache. We keep it alive for a while
