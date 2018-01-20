a3f46fd6be97190329f1bf99e8d0ef9a5f23529b
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-01-20 21:17:27 +0100

    openH264: enable ARM/ARM64 optimizations
    Enable the optimizations not only for ChromeOS but for all compatbile ARM/ARM64 architectures
    Limit threads auto-detect only for iOS
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I45b8880d0e6fe13bc87d05b6db2a6c88eed20a1c

--- third_party/blink/renderer/modules/mediarecorder/h264_encoder.cc
+++ third_party/blink/renderer/modules/mediarecorder/h264_encoder.cc
@@ -228,11 +228,11 @@ bool H264Encoder::ConfigureEncoder(const gfx::Size& size) {
     init_params.iRCMode = RC_OFF_MODE;
   }
 
-#if BUILDFLAG(IS_CHROMEOS)
-  init_params.iMultipleThreadIdc = 0;
-#else
+#if BUILDFLAG(IS_MAC)
   // Threading model: Set to 1 due to https://crbug.com/583348.
   init_params.iMultipleThreadIdc = 1;
+#else
+  init_params.iMultipleThreadIdc = 0;
 #endif
 
   // TODO(mcasas): consider reducing complexity if there are few CPUs available.
