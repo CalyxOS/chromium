de7b54079c23db19ee7d58dd0f3e151080c9ab09
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:36:33 +0100

    ungoogled-chromium: no special hosts/domains
    Disables various detections of Google hosts and functionality specific to them
    Disable domain reliability detection
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I3dae6dbb89a90686eb1fc199685a3e7a06f8cfc7

--- components/page_load_metrics/browser/page_load_metrics_util.cc
+++ components/page_load_metrics/browser/page_load_metrics_util.cc
@@ -283,9 +283,7 @@ bool DidObserveLoadingBehaviorInAnyFrame(
 }
 
 bool IsGoogleSearchHostname(const GURL& url) {
-  std::optional<std::string> result =
-      page_load_metrics::GetGoogleHostnamePrefix(url);
-  return result && result.value() == "www";
+  return false;
 }
 
 bool IsGoogleSearchResultUrl(const GURL& url) {
