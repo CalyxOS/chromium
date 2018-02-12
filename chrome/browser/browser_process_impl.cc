a2f05ba675620ee3f0bf1faf84ae4a3fc328a4c8
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:36:33 +0100

    ungoogled-chromium: no special hosts/domains
    Disables various detections of Google hosts and functionality specific to them
    Disable domain reliability detection
    Partial reverts b9b74a7d927626928c9821b6bc5c04b29fe22ae1 to prevent metrics
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3dae6dbb89a90686eb1fc199685a3e7a06f8cfc7

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -1190,7 +1190,6 @@ BrowserProcessImpl::component_updater() {
       std::make_unique<component_updater::TimerUpdateScheduler>();
 
   std::string brand;
-  google_brand::GetBrand(&brand);
   component_updater_ = component_updater::ComponentUpdateServiceFactory(
       component_updater::MakeChromeComponentUpdaterConfigurator(
           base::CommandLine::ForCurrentProcess(),
