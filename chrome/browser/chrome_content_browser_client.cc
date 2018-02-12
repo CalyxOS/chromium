5ae532de8e2d0d4d3d3f5bf4bb8701dfbbd98732
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:36:33 +0100

    ungoogled-chromium: no special hosts/domains
    Disables various detections of Google hosts and functionality specific to them
    Disable domain reliability detection
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3dae6dbb89a90686eb1fc199685a3e7a06f8cfc7

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3023,7 +3023,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kForcePNaClSubzero,
 #endif
       switches::kForceUIDirection,
-      switches::kIgnoreGooglePortNumbers,
       switches::kJavaScriptHarmony,
       switches::kEnableExperimentalWebAssemblyFeatures,
       embedder_support::kOriginTrialDisabledFeatures,
@@ -5459,9 +5458,6 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
   }
 #endif
 
-  MaybeAddThrottle(
-      SupervisedUserGoogleAuthNavigationThrottle::MaybeCreate(handle),
-      &throttles);
 
   if (base::FeatureList::IsEnabled(
           supervised_user::kClassifyUrlOnProcessResponseEvent)) {
@@ -6056,7 +6052,8 @@ GetClientDataHeader(content::FrameTreeNodeId frame_tree_node_id) {
 }
 #endif
 
-std::unique_ptr<blink::URLLoaderThrottle> CreateGoogleURLLoaderThrottle(
+
+#if 0 // std::unique_ptr<blink::URLLoaderThrottle> CreateGoogleURLLoaderThrottle(
 #if BUILDFLAG(IS_ANDROID)
     const std::string& client_data_header,
 #endif
@@ -6096,7 +6093,7 @@ std::unique_ptr<blink::URLLoaderThrottle> CreateGoogleURLLoaderThrottle(
       std::move(bound_session_request_throttled_handler),
 #endif
       std::move(dynamic_params));
-}
+#endif // }
 
 std::vector<std::unique_ptr<blink::URLLoaderThrottle>>
 ChromeContentBrowserClient::CreateURLLoaderThrottles(
@@ -6153,15 +6150,6 @@ ChromeContentBrowserClient::CreateURLLoaderThrottles(
       GetClientDataHeader(frame_tree_node_id);
 #endif
 
-  if (auto google_throttle = CreateGoogleURLLoaderThrottle(
-#if BUILDFLAG(IS_ANDROID)
-          client_data_header,
-#endif
-          profile);
-      google_throttle) {
-    result.push_back(std::move(google_throttle));
-  }
-
   {
     auto* factory =
         ProtocolHandlerRegistryFactory::GetForBrowserContext(browser_context);
@@ -6215,20 +6203,6 @@ ChromeContentBrowserClient::CreateURLLoaderThrottlesForKeepAlive(
     result.push_back(std::move(safe_browsing_throttle));
   }
 #endif
-
-#if BUILDFLAG(IS_ANDROID)
-  auto [client_data_header, unused_is_custom_tab] =
-      GetClientDataHeader(frame_tree_node_id);
-#endif
-
-  if (auto google_throttle = CreateGoogleURLLoaderThrottle(
-#if BUILDFLAG(IS_ANDROID)
-          client_data_header,
-#endif
-          profile);
-      google_throttle) {
-    result.push_back(std::move(google_throttle));
-  }
 
   return result;
 }
