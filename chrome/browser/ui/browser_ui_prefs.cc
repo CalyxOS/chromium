be9444101764cb06627b8fc573b9ba50ec7a8a69
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:21:36 +0100

    ungoogled-chromium: Disable webRTC log uploader
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ieb553d3b7c839dee6c5b06c1d3587842b95cba1c

--- chrome/browser/ui/browser_ui_prefs.cc
+++ chrome/browser/ui/browser_ui_prefs.cc
@@ -113,7 +113,7 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterStringPref(prefs::kWebRTCUDPPortRange, std::string());
   registry->RegisterBooleanPref(prefs::kWebRtcEventLogCollectionAllowed, false);
   registry->RegisterListPref(prefs::kWebRtcLocalIpsAllowedUrls);
-  registry->RegisterBooleanPref(prefs::kWebRtcTextLogCollectionAllowed, true);
+  registry->RegisterBooleanPref(prefs::kWebRtcTextLogCollectionAllowed, false);
 
   // We need to register the type of these preferences in order to query
   // them even though they're only typically controlled via policy.
