94d7def537661e8a017fdaee2b80746fa05ce8ca
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:28:53 +0100

    ungoogled-chromium: Disable translate integration
    Remove translate offer
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I7f7a436ca02fac8c1cce5a73df3e51c5c71feb6b

--- chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/settings/LanguageSettings.java
+++ chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/settings/LanguageSettings.java
@@ -48,12 +48,6 @@ public class LanguageSettings extends ChromeBaseSettingsFragment
     static final String APP_LANGUAGE_PREFERENCE_KEY = "app_language_preference";
     static final String PREFERRED_LANGUAGES_KEY = "preferred_languages";
     static final String CONTENT_LANGUAGES_KEY = "content_languages_preference";
-    static final String TRANSLATE_SWITCH_KEY = "translate_switch";
-
-    static final String TRANSLATION_ADVANCED_SECTION = "translation_advanced_settings_section";
-    static final String TARGET_LANGUAGE_KEY = "translate_settings_target_language";
-    static final String ALWAYS_LANGUAGES_KEY = "translate_settings_always_languages";
-    static final String NEVER_LANGUAGES_KEY = "translate_settings_never_languages";
 
     private static final String TAG = "LanguageSettings";
 
@@ -96,35 +90,6 @@ public class LanguageSettings extends ChromeBaseSettingsFragment
         ContentLanguagesPreference mLanguageListPref =
                 (ContentLanguagesPreference) findPreference(PREFERRED_LANGUAGES_KEY);
         mLanguageListPref.initialize(this, getPrefService());
-
-        ChromeSwitchPreference translateSwitch =
-                (ChromeSwitchPreference) findPreference(TRANSLATE_SWITCH_KEY);
-        boolean isTranslateEnabled = getPrefService().getBoolean(Pref.OFFER_TRANSLATE_ENABLED);
-        translateSwitch.setChecked(isTranslateEnabled);
-
-        translateSwitch.setOnPreferenceChangeListener(
-                new Preference.OnPreferenceChangeListener() {
-                    @Override
-                    public boolean onPreferenceChange(Preference preference, Object newValue) {
-                        boolean enabled = (boolean) newValue;
-                        getPrefService().setBoolean(Pref.OFFER_TRANSLATE_ENABLED, enabled);
-                        mLanguageListPref.notifyPrefChanged();
-                        LanguagesManager.recordAction(
-                                enabled
-                                        ? LanguagesManager.LanguageSettingsActionType
-                                                .ENABLE_TRANSLATE_GLOBALLY
-                                        : LanguagesManager.LanguageSettingsActionType
-                                                .DISABLE_TRANSLATE_GLOBALLY);
-                        return true;
-                    }
-                });
-        translateSwitch.setManagedPreferenceDelegate(
-                new ChromeManagedPreferenceDelegate(getProfile()) {
-                    @Override
-                    public boolean isPreferenceControlledByPolicy(Preference preference) {
-                        return getPrefService().isManagedPreference(Pref.OFFER_TRANSLATE_ENABLED);
-                    }
-                });
     }
 
     /**
@@ -144,8 +109,6 @@ public class LanguageSettings extends ChromeBaseSettingsFragment
         ContentLanguagesPreference mLanguageListPref =
                 (ContentLanguagesPreference) findPreference(CONTENT_LANGUAGES_KEY);
         mLanguageListPref.initialize(this, getPrefService());
-
-        setupTranslateSection(mLanguageListPref);
     }
 
     /** Setup the App Language section with a title and preference to choose the app language. */
@@ -175,7 +138,7 @@ public class LanguageSettings extends ChromeBaseSettingsFragment
      * @param contentLanguagesPreference ContentLanguagesPreference reference to update about state
      *         changes.
      */
-    private void setupTranslateSection(ContentLanguagesPreference contentLanguagesPreference) {
+/*    private void setupTranslateSection(ContentLanguagesPreference contentLanguagesPreference) {
         // Setup expandable advanced settings section.
         PreferenceCategory translationAdvancedSection =
                 (PreferenceCategory) findPreference(TRANSLATION_ADVANCED_SECTION);
@@ -251,7 +214,7 @@ public class LanguageSettings extends ChromeBaseSettingsFragment
                         return getPrefService().isManagedPreference(Pref.OFFER_TRANSLATE_ENABLED);
                     }
                 });
-    }
+    } */
 
     @Override
     public void onStart() {
@@ -290,12 +253,6 @@ public class LanguageSettings extends ChromeBaseSettingsFragment
             // Set the default target language to match the new app language.
             TranslateBridge.setDefaultTargetLanguage(code);
         } else if (requestCode == REQUEST_CODE_CHANGE_TARGET_LANGUAGE) {
-            LanguageItemPickerPreference targetLanguagePreference =
-                    (LanguageItemPickerPreference) findPreference(TARGET_LANGUAGE_KEY);
-            targetLanguagePreference.setLanguageItem(code);
-            TranslateBridge.setDefaultTargetLanguage(code);
-            LanguagesManager.recordAction(
-                    LanguagesManager.LanguageSettingsActionType.CHANGE_TARGET_LANGUAGE);
         }
     }
 
