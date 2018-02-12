03beafd64c139bb6796c54d7c1e155f468a1a778
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:36:33 +0100

    ungoogled-chromium: no special hosts/domains
    Disables various detections of Google hosts and functionality specific to them
    Disable domain reliability detection
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3dae6dbb89a90686eb1fc199685a3e7a06f8cfc7

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -2836,7 +2836,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kForcePNaClSubzero,
 #endif
       switches::kForceUIDirection,
-      switches::kIgnoreGooglePortNumbers,
       switches::kJavaScriptHarmony,
       switches::kEnableExperimentalWebAssemblyFeatures,
       embedder_support::kOriginTrialDisabledFeatures,
@@ -5202,9 +5201,6 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
   }
 #endif
 
-  MaybeAddThrottle(
-      SupervisedUserGoogleAuthNavigationThrottle::MaybeCreate(handle),
-      &throttles);
 
   MaybeAddThrottle(
       SupervisedUserNavigationThrottle::MaybeCreateThrottleFor(handle),
@@ -5775,7 +5771,8 @@ GetClientDataHeader(int frame_tree_node_id) {
 }
 #endif
 
-std::unique_ptr<blink::URLLoaderThrottle> CreateGoogleURLLoaderThrottle(
+
+#if 0 // std::unique_ptr<blink::URLLoaderThrottle> CreateGoogleURLLoaderThrottle(
 #if BUILDFLAG(IS_ANDROID)
     const std::string& client_data_header,
 #endif
@@ -5815,7 +5812,7 @@ std::unique_ptr<blink::URLLoaderThrottle> CreateGoogleURLLoaderThrottle(
       std::move(bound_session_request_throttled_handler),
 #endif
       std::move(dynamic_params));
-}
+#endif // }
 
 std::vector<std::unique_ptr<blink::URLLoaderThrottle>>
 ChromeContentBrowserClient::CreateURLLoaderThrottles(
@@ -5862,15 +5859,6 @@ ChromeContentBrowserClient::CreateURLLoaderThrottles(
   auto [client_data_header, is_custom_tab] =
       GetClientDataHeader(frame_tree_node_id);
 #endif
-
-  if (auto google_throttle = CreateGoogleURLLoaderThrottle(
-#if BUILDFLAG(IS_ANDROID)
-          client_data_header,
-#endif
-          profile);
-      google_throttle) {
-    result.push_back(std::move(google_throttle));
-  }
 
   {
     auto* factory =
@@ -5925,20 +5913,6 @@ ChromeContentBrowserClient::CreateURLLoaderThrottlesForKeepAlive(
     result.push_back(std::move(safe_browsing_throttle));
   }
 #endif
-
-#if BUILDFLAG(IS_ANDROID)
-  auto [client_data_header, unused_is_custom_tab] =
-      GetClientDataHeader(frame_tree_node_id);
-#endif
-
-  if (auto google_throttle = CreateGoogleURLLoaderThrottle(
-#if BUILDFLAG(IS_ANDROID)
-          client_data_header,
-#endif
-          profile);
-      google_throttle) {
-    result.push_back(std::move(google_throttle));
-  }
 
   return result;
 }
--- chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.cc
+++ chrome/browser/feedback/system_logs/log_sources/chrome_internal_log_source.cc
@@ -123,7 +123,6 @@ constexpr char kOsVersionTag[] = "OS VERSION";
 #if BUILDFLAG(IS_WIN)
 constexpr char kUsbKeyboardDetected[] = "usb_keyboard_detected";
 constexpr char kIsEnrolledToDomain[] = "enrolled_to_domain";
-constexpr char kInstallerBrandCode[] = "installer_brand_code";
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 constexpr char kUpdateErrorCode[] = "update_error_code";
 constexpr char kUpdateHresult[] = "update_hresult";
@@ -626,10 +625,6 @@ void ChromeInternalLogSource::PopulateEnrolledToDomain(
 
 void ChromeInternalLogSource::PopulateInstallerBrandCode(
     SystemLogsResponse* response) {
-  std::string brand;
-  google_brand::GetBrand(&brand);
-  response->emplace(kInstallerBrandCode,
-                    brand.empty() ? "Unknown brand code" : brand);
 }
 
 void ChromeInternalLogSource::PopulateLastUpdateState(
