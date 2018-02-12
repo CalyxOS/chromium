1cbd2b2751d4eec058125e8e6814d6c4d0945a73
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:29:30 +0100

    ungoogled-chromium: Disable profile avatar
    Added some Bromite-specific further improvements
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Iffe2de02bb8b3cd1f125e9b2d7e4d1f50837a486

--- chrome/browser/profiles/profile_attributes_storage.cc
+++ chrome/browser/profiles/profile_attributes_storage.cc
@@ -962,7 +962,7 @@ void ProfileAttributesStorage::DownloadHighResAvatarIfNeeded(
 void ProfileAttributesStorage::DownloadHighResAvatar(
     size_t icon_index,
     const base::FilePath& profile_path) {
-#if !BUILDFLAG(IS_ANDROID)
+#if false
   const char* file_name =
       profiles::GetDefaultAvatarIconFileNameAtIndex(icon_index);
   DCHECK(file_name);
