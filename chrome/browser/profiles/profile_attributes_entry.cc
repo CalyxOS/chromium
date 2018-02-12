bbd8054f311ac68149d08baa00ba6d69ce2d20d1
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-12 21:29:30 +0100

    ungoogled-chromium: Disable profile avatar
    Added some Bromite-specific further improvements
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Iffe2de02bb8b3cd1f125e9b2d7e4d1f50837a486

--- chrome/browser/profiles/profile_attributes_entry.cc
+++ chrome/browser/profiles/profile_attributes_entry.cc
@@ -337,12 +337,6 @@ std::u16string ProfileAttributesEntry::GetUserName() const {
 gfx::Image ProfileAttributesEntry::GetAvatarIcon(
     int size_for_placeholder_avatar,
     bool use_high_res_file) const {
-  if (IsUsingGAIAPicture()) {
-    const gfx::Image* image = GetGAIAPicture();
-    if (image)
-      return *image;
-  }
-
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS_ASH)
   // TODO(crbug.com/1100835): After launch, remove the treatment of placeholder
   // avatars from GetHighResAvatar() and from any other places.
