b1cf2653b89e62b34bb69d5966011c94d740d18c
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-02-21 00:32:09 +0100

    Use 4 tile rows, never show logo
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I7f0bfc3adfab3a20171e80cab19abb971db871bf

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
@@ -113,7 +113,7 @@ public class NewTabPageLayout extends LinearLayout {
      */
     private boolean mHasShownView;
 
-    private boolean mSearchProviderHasLogo = true;
+    private boolean mSearchProviderHasLogo = false;
     private boolean mSearchProviderIsGoogle;
     private boolean mShowingNonStandardLogo;
 
@@ -361,7 +361,7 @@ public class NewTabPageLayout extends LinearLayout {
             boolean isScrollableMvtEnabled, boolean searchProviderIsGoogle) {
         assert mMvTilesContainerLayout != null;
 
-        int maxRows = 2;
+        int maxRows = 4;
         if (searchProviderIsGoogle && QueryTileUtils.isQueryTilesEnabledOnNTP()) {
             maxRows = QueryTileSection.getMaxRowsForMostVisitedTiles(getContext());
         }
@@ -541,7 +541,7 @@ public class NewTabPageLayout extends LinearLayout {
                 && mInitialized) {
             return;
         }
-        mSearchProviderHasLogo = hasLogo;
+        mSearchProviderHasLogo = false;
         mSearchProviderIsGoogle = isGoogle;
 
         updateTilesLayoutMargins();
