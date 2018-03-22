496fdf9b359f35a01ead77d5904866dc9e2da1b7
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-03-22 22:38:00 +0100

    Disable plugins enumeration
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I4c4fb396cdcd9b5691a8e8549def233f308eee8f

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -2004,9 +2004,7 @@ WebContentSettingsClient* LocalFrame::GetContentSettingsClient() {
 }
 
 PluginData* LocalFrame::GetPluginData() const {
-  if (!Loader().AllowPlugins())
     return nullptr;
-  return GetPage()->GetPluginData();
 }
 
 void LocalFrame::SetAdTrackerForTesting(AdTracker* ad_tracker) {
