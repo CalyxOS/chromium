c6f5e96d8f8988cfade23d0c5fcb361108ebd334
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-03-25 21:49:37 +0200

    AudioBuffer, AnalyserNode: fp mitigations
    Truncate base latency precision to two digits
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ic9f8cdd7619ebc2646c8b1fb87f0d22757f21fa0

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2104,6 +2104,9 @@ extern const char kWebrtcUseMinMaxVEADimensionsDescription[];
 extern const char kWebTransportDeveloperModeName[];
 extern const char kWebTransportDeveloperModeDescription[];
 
+extern const char kAudioContextShuffleEnabledName[];
+extern const char kAudioContextShuffleEnabledDescription[];
+
 extern const char kWebUsbDeviceDetectionName[];
 extern const char kWebUsbDeviceDetectionDescription[];
 
