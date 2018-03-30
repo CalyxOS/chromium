e13ad2046fa134bdfbeda4198fc82c9b4d7f192a
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-03-30 10:09:03 +0200

    PARTIAL Multiple fingerprinting mitigations
    (CalyxOS Note: Our build procedures currently do not accommodate
    making changes to submodule repos, in this case third_party/skia,
    so that portion of this match needed to be removed, hence the
    "Partial" in the subject.)
    
    1. getClientRects, getBoundingClientRect, measureText: add fingerprinting mitigation
    
    Scale the result of Range::getClientRects, Element::getBoundingClientRect and
    Canvas::measureText by a random +/-3/1000000th of the original value for each
    float in the returned Rect/Quad.
    
    It contains improvements from ungoogled-chromium which add two flags:
    1. --fingerprinting-client-rects-noise to enable fingerprinting deception for Range::getClientRects and Element::getBoundingClientRect
    2. --fingerprinting-canvas-measuretext-noise to enable fingerprinting deception for Canvas::measureText
    
    2. Canvas: fingerprinting mitigations for image data
    
    Modify the color data returned by ToBlob, ToDataURL and getImageData so that
    it will contain randomly manipulated pixels (maximum 20) that slightly change
    the color of the R,G,B components without a visible effect.
    
    Credits to Slaviro (https://github.com/Slaviro) for coming up with a better
    approach to change color components.
    
    Added flag --fingerprinting-canvas-image-data-noise to disable Canvas image data fingerprinting deception
    Add noise also to SVGs (credits to uazo)
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I5c083699db91443331e3cc4ac059e4f21ec79ca7

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -44,6 +44,7 @@
 #include "base/debug/dump_without_crashing.h"
 #include "base/i18n/time_formatting.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/rand_util.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
 #include "base/task/single_thread_task_runner.h"
@@ -908,6 +909,17 @@ Document::Document(const DocumentInit& initializer,
   TRACE_EVENT_WITH_FLOW0("blink", "Document::Document", TRACE_ID_LOCAL(this),
                          TRACE_EVENT_FLAG_FLOW_OUT);
   DCHECK(agent_);
+  if (RuntimeEnabledFeatures::FingerprintingClientRectsNoiseEnabled() ||
+      RuntimeEnabledFeatures::FingerprintingCanvasMeasureTextNoiseEnabled() ||
+      RuntimeEnabledFeatures::FingerprintingCanvasImageDataNoiseEnabled()) {
+    // Precompute -0.0003% to 0.0003% noise factor for get*ClientRect*() fingerprinting
+    noise_factor_x_ = 1.0 + (base::RandDouble() - 0.5) * 0.0003;
+    noise_factor_y_ = 1.0 + (base::RandDouble() - 0.5) * 0.0003;
+  } else {
+    noise_factor_x_ = 1;
+    noise_factor_y_ = 1;
+  }
+
   if (base::FeatureList::IsEnabled(features::kDelayAsyncScriptExecution) &&
       features::kDelayAsyncScriptExecutionDelayByDefaultParam.Get()) {
     script_runner_delayer_->Activate();
@@ -2495,6 +2507,14 @@ void Document::UpdateStyleAndLayoutTreeForThisDocument() {
 #if DCHECK_IS_ON()
   AssertLayoutTreeUpdated(*this, true /* allow_dirty_container_subtrees */);
 #endif
+}
+
+double Document::GetNoiseFactorX() {
+  return noise_factor_x_;
+}
+
+double Document::GetNoiseFactorY() {
+  return noise_factor_y_;
 }
 
 void Document::InvalidateStyleAndLayoutForFontUpdates() {
