b875fa760893e9845404ed3561aa98bd3065da32
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-03-30 10:09:03 +0200

    Multiple fingerprinting mitigations
    1. getClientRects, getBoundingClientRect, measureText: add fingerprinting mitigation
    
    Scale the result of Range::getClientRects, Element::getBoundingClientRect and
    Canvas::measureText by a random +/-3/1000000th of the original value for each
    float in the returned Rect/Quad.
    
    It contains improvements from ungoogled-chromium which add two flags:
    1. --fingerprinting-client-rects-noise to enable fingerprinting deception for Range::getClientRects and Element::getBoundingClientRect
    2. --fingerprinting-canvas-measuretext-noise to enable fingerprinting deception for Canvas::measureText
    
    2. Canvas: fingerprinting mitigations for image data
    
    Modify the color data returned by ToBlob, ToDataURL and getImageData so that
    it will contain randomly manipulated pixels (maximum 20) that slightly change
    the color of the R,G,B components without a visible effect.
    
    Credits to Slaviro (https://github.com/Slaviro) for coming up with a better
    approach to change color components.
    
    Added flag --fingerprinting-canvas-image-data-noise to disable Canvas image data fingerprinting deception
    Add noise also to SVGs (credits to uazo)
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2065,6 +2065,7 @@ void Element::ClientQuads(Vector<gfx::QuadF>& quads) const {
       quads.push_back(element_layout_object->LocalToAbsoluteQuad(
           gfx::QuadF(element_layout_object->ObjectBoundingBox())));
     }
+    //TODO: cover this as well
     return;
   }
 
@@ -2072,6 +2073,11 @@ void Element::ClientQuads(Vector<gfx::QuadF>& quads) const {
   if (element_layout_object->IsBoxModelObject() ||
       element_layout_object->IsBR())
     element_layout_object->AbsoluteQuads(quads);
+
+  if (RuntimeEnabledFeatures::FingerprintingClientRectsNoiseEnabled()) {
+    for (auto& quad : quads)
+     quad.Scale(GetDocument().GetNoiseFactorX(), GetDocument().GetNoiseFactorY());
+  }
 }
 
 DOMRectList* Element::getClientRects() {
@@ -2110,6 +2116,9 @@ gfx::RectF Element::GetBoundingClientRectNoLifecycleUpdate() const {
   DCHECK(element_layout_object);
   GetDocument().AdjustRectForScrollAndAbsoluteZoom(result,
                                                    *element_layout_object);
+  if (RuntimeEnabledFeatures::FingerprintingClientRectsNoiseEnabled()) {
+    result.Scale(GetDocument().GetNoiseFactorX(), GetDocument().GetNoiseFactorY());
+  }
   return result;
 }
 
