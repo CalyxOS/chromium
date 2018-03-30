4d9b53b22e948c78dea0e85cdb61cc569990b58e
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-03-30 10:09:03 +0200

    PARTIAL Multiple fingerprinting mitigations
    (CalyxOS Note: Our build procedures currently do not accommodate
    making changes to submodule repos, in this case third_party/skia,
    so that portion of this match needed to be removed, hence the
    "PARTIAL" in the subject. -tmw)
    
    1. getClientRects, getBoundingClientRect, measureText: add fingerprinting mitigation
    
    Scale the result of Range::getClientRects, Element::getBoundingClientRect and
    Canvas::measureText by a random +/-3/1000000th of the original value for each
    float in the returned Rect/Quad.
    
    It contains improvements from ungoogled-chromium which add two flags:
    1. --fingerprinting-client-rects-noise to enable fingerprinting deception for Range::getClientRects and Element::getBoundingClientRect
    2. --fingerprinting-canvas-measuretext-noise to enable fingerprinting deception for Canvas::measureText
    
    2. Canvas: fingerprinting mitigations for image data
    
    Modify the color data returned by ToBlob, ToDataURL and getImageData so that
    it will contain randomly manipulated pixels (maximum 20) that slightly change
    the color of the R,G,B components without a visible effect.
    
    Credits to Slaviro (https://github.com/Slaviro) for coming up with a better
    approach to change color components.
    
    Added flag --fingerprinting-canvas-image-data-noise to disable Canvas image data fingerprinting deception
    Add noise also to SVGs (credits to uazo)
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: If29db53656dc4f8163ef145eb3bf153bef9058f5

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -205,6 +205,11 @@
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/size_f.h"
 #include "ui/gfx/geometry/skia_conversions.h"
+
+#include "third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
+
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 #include "v8/include/v8-local-handle.h"
@@ -2964,6 +2969,10 @@ ImageData* BaseRenderingContext2D::getImageDataInternal(
           snapshot->PaintImageForCurrentFrame().GetSkImageInfo().bounds();
       DCHECK(!bounds.intersect(SkIRect::MakeXYWH(sx, sy, sw, sh)));
     }
+
+    if (read_pixels_successful && RuntimeEnabledFeatures::FingerprintingCanvasImageDataNoiseEnabled()) {
+      StaticBitmapImage::ShuffleSubchannelColorData(image_data_pixmap, sx, sy);
+    }
   }
 
   return image_data;
@@ -3763,12 +3772,26 @@ TextMetrics* BaseRenderingContext2D::measureText(const String& text) {
   TextDirection direction = ToTextDirection(
       state.GetDirection(), GetCanvasRenderingContextHost(), computed_style);
 
-  return MakeGarbageCollected<TextMetrics>(
+  auto* text_metrics = MakeGarbageCollected<TextMetrics>(
       font, direction, state.GetTextBaseline().AsEnum(),
       state.GetTextAlign().AsEnum(), text,
       RuntimeEnabledFeatures::CanvasTextNgEnabled()
           ? &GetCanvasRenderingContextHost()->GetPlainTextPainter()
           : nullptr);
+  // Scale text metrics if enabled
+  if (RuntimeEnabledFeatures::FingerprintingCanvasMeasureTextNoiseEnabled()) {
+    OffscreenCanvas* offscreen_canvas = HostAsOffscreenCanvas();
+    if (offscreen_canvas) {
+      ExecutionContext* execution_context = GetTopExecutionContext();
+      if (auto* window = DynamicTo<LocalDOMWindow>(execution_context)) {
+        Document* document = window->GetFrame()->GetDocument();
+        text_metrics->Shuffle(document->GetNoiseFactorX());
+      }
+    }
+    else
+      text_metrics->Shuffle(canvas->GetDocument().GetNoiseFactorX());
+  }
+  return text_metrics;
 }
 
 void BaseRenderingContext2D::SnapshotStateForFilter() {
