032855f069d0314f552d59cf033cfd33f7c69012
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-03-30 10:09:03 +0200

    Multiple fingerprinting mitigations
    1. getClientRects, getBoundingClientRect, measureText: add fingerprinting mitigation
    
    Scale the result of Range::getClientRects, Element::getBoundingClientRect and
    Canvas::measureText by a random +/-3/1000000th of the original value for each
    float in the returned Rect/Quad.
    
    It contains improvements from ungoogled-chromium which add two flags:
    1. --fingerprinting-client-rects-noise to enable fingerprinting deception for Range::getClientRects and Element::getBoundingClientRect
    2. --fingerprinting-canvas-measuretext-noise to enable fingerprinting deception for Canvas::measureText
    
    2. Canvas: fingerprinting mitigations for image data
    
    Modify the color data returned by ToBlob, ToDataURL and getImageData so that
    it will contain randomly manipulated pixels (maximum 20) that slightly change
    the color of the R,G,B components without a visible effect.
    
    Credits to Slaviro (https://github.com/Slaviro) for coming up with a better
    approach to change color components.
    
    Added flag --fingerprinting-canvas-image-data-noise to disable Canvas image data fingerprinting deception
    Add noise also to SVGs (credits to uazo)
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I5c083699db91443331e3cc4ac059e4f21ec79ca7

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -514,6 +514,10 @@ class CORE_EXPORT Document : public ContainerNode,
     has_xml_declaration_ = has_xml_declaration ? 1 : 0;
   }
 
+  // Values for get*ClientRect fingerprint deception
+  double GetNoiseFactorX();
+  double GetNoiseFactorY();
+
   AtomicString visibilityState() const;
   bool IsPageVisible() const;
   bool hidden() const;
@@ -2331,6 +2335,9 @@ class CORE_EXPORT Document : public ContainerNode,
   bool is_freezing_in_progress_;
 
   base::ElapsedTimer start_time_;
+
+  double noise_factor_x_;
+  double noise_factor_y_;
 
   Member<ScriptRunner> script_runner_;
   Member<ScriptRunnerDelayer> script_runner_delayer_;
