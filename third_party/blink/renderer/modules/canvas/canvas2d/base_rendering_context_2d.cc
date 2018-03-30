f583e1e53d0f1d707da8175de8871f53a90bde33
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-03-30 10:09:03 +0200

    Multiple fingerprinting mitigations
    1. getClientRects, getBoundingClientRect, measureText: add fingerprinting mitigation
    
    Scale the result of Range::getClientRects, Element::getBoundingClientRect and
    Canvas::measureText by a random +/-3/1000000th of the original value for each
    float in the returned Rect/Quad.
    
    It contains improvements from ungoogled-chromium which add two flags:
    1. --fingerprinting-client-rects-noise to enable fingerprinting deception for Range::getClientRects and Element::getBoundingClientRect
    2. --fingerprinting-canvas-measuretext-noise to enable fingerprinting deception for Canvas::measureText
    
    2. Canvas: fingerprinting mitigations for image data
    
    Modify the color data returned by ToBlob, ToDataURL and getImageData so that
    it will contain randomly manipulated pixels (maximum 20) that slightly change
    the color of the R,G,B components without a visible effect.
    
    Credits to Slaviro (https://github.com/Slaviro) for coming up with a better
    approach to change color components.
    
    Added flag --fingerprinting-canvas-image-data-noise to disable Canvas image data fingerprinting deception
    Add noise also to SVGs (credits to uazo)
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I5c083699db91443331e3cc4ac059e4f21ec79ca7

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -34,6 +34,7 @@
 #include "third_party/blink/renderer/platform/bindings/string_resource.h"
 #include "third_party/blink/renderer/platform/graphics/bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_context.h"
+#include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/stroke_data.h"
 #include "third_party/blink/renderer/platform/graphics/video_frame_image_util.h"
@@ -2124,6 +2125,10 @@ ImageData* BaseRenderingContext2D::getImageDataInternal(
       SkIRect bounds =
           snapshot->PaintImageForCurrentFrame().GetSkImageInfo().bounds();
       DCHECK(!bounds.intersect(SkIRect::MakeXYWH(sx, sy, sw, sh)));
+    }
+
+    if (read_pixels_successful && RuntimeEnabledFeatures::FingerprintingCanvasImageDataNoiseEnabled()) {
+      StaticBitmapImage::ShuffleSubchannelColorData(image_data_pixmap.addr(), image_data_pixmap.info(), sx, sy);
     }
   }
 
