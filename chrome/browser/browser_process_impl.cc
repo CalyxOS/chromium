f0f1528f59db2e5fb0fca16fc54a75253388aef9
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-04-25 10:23:51 +0200

    Disable various metrics
    Disable metrics on all I/O threads
    Set initial metrics opt-out
    Disable metrics collection for NTP tiles
    Fix startup crash for debug builds (uazo)
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I6a0b0fe3c16fde5349ed2bb90dc7ff4cd7821efb

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -1042,7 +1042,7 @@ void BrowserProcessImpl::RegisterPrefs(PrefRegistrySimple* registry) {
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   registry->RegisterBooleanPref(metrics::prefs::kMetricsReportingEnabled,
-                                GoogleUpdateSettings::GetCollectStatsConsent());
+                                false);
   registry->RegisterBooleanPref(prefs::kDevToolsRemoteDebuggingAllowed, true);
 }
 
