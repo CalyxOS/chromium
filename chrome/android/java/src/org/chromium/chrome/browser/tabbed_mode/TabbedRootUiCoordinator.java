937a7514ea74f1cdb6eafdc824f052cbbbb69148
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-12 15:09:26 +0200

    Disable all promo dialogs
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -902,7 +902,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
      * displayed.
      */
     private boolean triggerPromo(boolean intentWithEffect) {
-        try (TraceEvent e = TraceEvent.scoped("TabbedRootUiCoordinator.triggerPromo")) {
+        /*try (TraceEvent e = TraceEvent.scoped("TabbedRootUiCoordinator.triggerPromo")) {
             if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_STARTUP_PROMOS)) {
                 return false;
             }
@@ -932,8 +932,8 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                 preferenceManager.writeBoolean(
                         ChromePreferenceKeys.PROMOS_SKIPPED_ON_FIRST_START, true);
             }
-            return isShowingPromo;
-        }
+            return isShowingPromo;        } */
+            return false;
     }
 
     @VisibleForTesting
