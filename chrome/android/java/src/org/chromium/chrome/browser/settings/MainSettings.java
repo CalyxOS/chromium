b876a7d8bd5694553685649fb2c2a77db8e418d5
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-12 14:23:07 +0200

    Remove Google Services and signin from settings
    Taken from "Remove signin and sync integrations" Bromite patch.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I152a6713d12ebc4764e9a7bba1c101a17a88c393

--- chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
@@ -178,24 +178,6 @@ public class MainSettings extends ChromeBaseSettingsFragment
         ProfileDataCache profileDataCache =
                 ProfileDataCache.createWithDefaultImageSizeAndNoBadge(getContext());
         AccountManagerFacade accountManagerFacade = AccountManagerFacadeProvider.getInstance();
-        SigninManager signinManager = IdentityServicesProvider.get().getSigninManager(getProfile());
-        IdentityManager identityManager =
-                IdentityServicesProvider.get().getIdentityManager(getProfile());
-
-        SyncPromoPreference syncPromoPreference = findPreference(PREF_SYNC_PROMO);
-        syncPromoPreference.initialize(
-                profileDataCache,
-                accountManagerFacade,
-                signinManager,
-                identityManager,
-                new SyncPromoController(
-                        getProfile(),
-                        SigninAccessPoint.SETTINGS,
-                        SyncConsentActivityLauncherImpl.get(),
-                        SigninAndHistoryOptInActivityLauncherImpl.get()));
-
-        SignInPreference signInPreference = findPreference(PREF_SIGN_IN);
-        signInPreference.initialize(getProfile(), profileDataCache, accountManagerFacade);
 
         cachePreferences();
 
@@ -279,14 +261,6 @@ public class MainSettings extends ChromeBaseSettingsFragment
     }
 
     private void updatePreferences() {
-        if (IdentityServicesProvider.get()
-                .getSigninManager(getProfile())
-                .isSigninSupported(/* requireUpdatedPlayServices= */ false)) {
-            addPreferenceIfAbsent(PREF_SIGN_IN);
-        } else {
-            removePreferenceIfPresent(PREF_SIGN_IN);
-        }
-
         updateManageSyncPreference();
         updateSearchEnginePreference();
         updateAutofillPreferences();
@@ -330,6 +304,7 @@ public class MainSettings extends ChromeBaseSettingsFragment
     }
 
     private void updateManageSyncPreference() {
+        if (true) return;
         String primaryAccountName =
                 CoreAccountInfo.getEmailFrom(
                         IdentityServicesProvider.get()
