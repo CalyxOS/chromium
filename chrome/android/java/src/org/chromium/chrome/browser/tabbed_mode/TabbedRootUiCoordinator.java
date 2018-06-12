0a125b5609c8a19ae85f1f14fc0052afc8d3baf5
uazo <uazo@users.noreply.github.com>
2018-06-12 15:09:26 +0200

    Disable all promo dialogs
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I9e71112a185671ce7aae2f836840bf949597eb0a

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -1465,6 +1465,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
     }
 
     private boolean maybeShowPromo(Profile profile) {
+        if ((true)) return false;
         // NOTE: Only one promo can be shown in one run to avoid nagging users too much.
 
         // The PWA Restore promotion runs when we've detected that a user has switched to a new
