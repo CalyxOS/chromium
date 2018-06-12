e1bd90c752a7d1905c4e317aa5f334b00880df82
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-12 14:23:07 +0200

    Remove Google Services and signin from settings
    Taken from "Remove signin and sync integrations" Bromite patch.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I152a6713d12ebc4764e9a7bba1c101a17a88c393

--- chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
@@ -235,13 +235,6 @@ public class MainSettings extends PreferenceFragmentCompat
     }
 
     private void updatePreferences() {
-        if (IdentityServicesProvider.get().getSigninManager(mProfile).isSigninSupported(
-                    /*requireUpdatedPlayServices=*/false)) {
-            addPreferenceIfAbsent(PREF_SIGN_IN);
-        } else {
-            removePreferenceIfPresent(PREF_SIGN_IN);
-        }
-
         updateManageSyncPreference();
         updateSearchEnginePreference();
         updatePasswordsPreference();
@@ -277,6 +270,7 @@ public class MainSettings extends PreferenceFragmentCompat
     }
 
     private void updateManageSyncPreference() {
+        if (true) return;
         String primaryAccountName = CoreAccountInfo.getEmailFrom(
                 IdentityServicesProvider.get().getIdentityManager(mProfile).getPrimaryAccountInfo(
                         ConsentLevel.SIGNIN));
