95098f6baafb7e9996c0b154a9abe28a0854fa44
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-27 01:32:28 +0200

    Remove help menu item
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I7047a850eaa75fdbed2e85d54825e5eaadb050e8

--- chrome/browser/safe_browsing/android/java/src/org/chromium/chrome/browser/safe_browsing/settings/SafeBrowsingSettingsFragmentBase.java
+++ chrome/browser/safe_browsing/android/java/src/org/chromium/chrome/browser/safe_browsing/settings/SafeBrowsingSettingsFragmentBase.java
@@ -66,6 +66,7 @@ public abstract class SafeBrowsingSettingsFragmentBase extends ChromeBaseSetting
         help.setIcon(
                 TraceEventVectorDrawableCompat.create(
                         getResources(), R.drawable.ic_help_and_feedback, getActivity().getTheme()));
+        help.setVisible(false);
     }
 
     @Override
