ca09f25cbfeacd621d1985ce7e45dbd4cc284c8f
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-27 01:32:28 +0200

    Remove help menu item
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I7047a850eaa75fdbed2e85d54825e5eaadb050e8

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
@@ -721,7 +721,6 @@ public abstract class BaseCustomTabActivity extends ChromeActivity<BaseCustomTab
         // Disable creating new tabs, bookmark, history, print, help, focus_url, etc.
         if (id == R.id.focus_url_bar
                 || id == R.id.all_bookmarks_menu_id
-                || id == R.id.help_id
                 || id == R.id.recent_tabs_menu_id
                 || id == R.id.new_incognito_tab_menu_id
                 || id == R.id.new_tab_menu_id
--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsBaseFragment.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsBaseFragment.java
@@ -78,6 +78,7 @@ public abstract class PrivacySandboxSettingsBaseFragment extends ChromeBaseSetti
         help.setIcon(
                 TraceEventVectorDrawableCompat.create(
                         getResources(), R.drawable.ic_help_and_feedback, getActivity().getTheme()));
+        help.setVisible(false);
     }
 
     @Override
