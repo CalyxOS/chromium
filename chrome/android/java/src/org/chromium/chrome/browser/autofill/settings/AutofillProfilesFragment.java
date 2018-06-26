c67a5a0b3f68bba3c41524e5933585078f1d4497
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-27 01:32:28 +0200

    Remove help menu item
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I7047a850eaa75fdbed2e85d54825e5eaadb050e8

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
@@ -112,6 +112,7 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
         MenuItem help =
                 menu.add(Menu.NONE, R.id.menu_id_targeted_help, Menu.NONE, R.string.menu_help);
         help.setIcon(R.drawable.ic_help_and_feedback);
+        help.setVisible(false);
     }
 
     @Override
