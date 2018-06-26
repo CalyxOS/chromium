39312f39aada79371636a0fff22f56cc47051428
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-27 01:32:28 +0200

    Remove help menu item
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I7047a850eaa75fdbed2e85d54825e5eaadb050e8

--- chrome/browser/password_check/android/java/src/org/chromium/chrome/browser/password_check/PasswordCheckFragmentView.java
+++ chrome/browser/password_check/android/java/src/org/chromium/chrome/browser/password_check/PasswordCheckFragmentView.java
@@ -60,6 +60,7 @@ public class PasswordCheckFragmentView extends PreferenceFragmentCompat implemen
         help.setIcon(
                 TraceEventVectorDrawableCompat.create(
                         getResources(), R.drawable.ic_help_and_feedback, getActivity().getTheme()));
+        help.setVisible(false);
     }
 
     @Override
