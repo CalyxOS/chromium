28784944ee7cb72c7cc4f70dc1f093ae86af7c5f
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-27 11:02:38 +0200

    Always use new tab page for default home page
    Ignore any partner-provided home page.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: If9da4e34c5cf72ffc00e08934e72c3120b2ce27a

--- components/search_engines/android/java/src/org/chromium/components/search_engines/TemplateUrl.java
+++ components/search_engines/android/java/src/org/chromium/components/search_engines/TemplateUrl.java
@@ -72,6 +72,7 @@ public class TemplateUrl {
      *     prepopulated_engines.json.
      */
     public String getNewTabURL() {
+        if ((true)) return null;
         return TemplateUrlJni.get().getNewTabURL(mTemplateUrlPtr);
     }
 
