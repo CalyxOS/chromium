5e4d8586c22665ffc349587314e983b3f6350ebb
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-06-27 11:02:38 +0200

    Always use new tab page for default home page
    Ignore any partner-provided home page.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: If9da4e34c5cf72ffc00e08934e72c3120b2ce27a

--- components/search_engines/android/template_url_service_android.cc
+++ components/search_engines/android/template_url_service_android.cc
@@ -404,10 +404,7 @@ jboolean TemplateUrlServiceAndroid::SetPlayAPISearchEngine(
         jimage_translate_source_language_param_key,
     const base::android::JavaParamRef<jstring>&
         jimage_translate_target_language_param_key) {
-  // The function is scheduled to run only when the service is loaded, see
-  // `TemplateUrlService#runWhenLoaded()`.
-  CHECK(template_url_service_->loaded(), base::NotFatalUntil::M128);
-
+  if ((true)) return false;
   // Check if there is already a search engine created from Play API.
   TemplateURLService::TemplateURLVector template_urls =
       template_url_service_->GetTemplateURLs();
