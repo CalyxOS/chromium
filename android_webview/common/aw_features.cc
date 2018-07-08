9c0a0efc9e0f2cb97188f33e09e412579bbb04b3
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-07-08 18:16:34 +0200

    Disable fetching of all field trials
    Disable seed-based field trials.
    Disable reporting of Omnibox suggestions header.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ibc8a7b748877a8480a5c2e7228a99a9e8990c5b8

--- android_webview/common/aw_features.cc
+++ android_webview/common/aw_features.cc
@@ -107,8 +107,8 @@ BASE_FEATURE(kWebViewMetricsFiltering,
 
 // Field trial feature for controlling support of Origin Trials on WebView.
 BASE_FEATURE(kWebViewOriginTrials,
-             "WebViewOriginTrials",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             "WebViewOriginTrials",                 // disabled
+             base::FEATURE_DISABLED_BY_DEFAULT);    // by default
 
 // Whether to record size of the embedding app's data directory to the UMA
 // histogram Android.WebView.AppDataDirectorySize.
