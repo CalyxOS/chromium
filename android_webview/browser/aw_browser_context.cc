2061467eefc151a0ca0a3556b0498953c7b11f97
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-07-08 18:16:34 +0200

    Disable fetching of all field trials
    Disable seed-based field trials.
    Disable reporting of Omnibox suggestions header.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ibc8a7b748877a8480a5c2e7228a99a9e8990c5b8

--- android_webview/browser/aw_browser_context.cc
+++ android_webview/browser/aw_browser_context.cc
@@ -537,6 +537,7 @@ AwBrowserContext::RetrieveInProgressDownloadManager() {
 
 content::OriginTrialsControllerDelegate*
 AwBrowserContext::GetOriginTrialsControllerDelegate() {
+  if ((true)) return nullptr;
   if (!origin_trials_controller_delegate_) {
     origin_trials_controller_delegate_ =
         std::make_unique<origin_trials::OriginTrials>(
