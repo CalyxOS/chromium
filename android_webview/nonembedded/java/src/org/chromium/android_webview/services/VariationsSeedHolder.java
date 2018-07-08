07e3f77fe229cea98af066688d9b8d416a0b8e58
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-07-08 18:16:34 +0200

    Disable fetching of all field trials
    Disable seed-based field trials.
    Disable reporting of Omnibox suggestions header.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ibc8a7b748877a8480a5c2e7228a99a9e8990c5b8

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
@@ -145,7 +145,7 @@ public class VariationsSeedHolder {
 
     @VisibleForTesting
     public void scheduleFetchIfNeeded() {
-        AwVariationsSeedFetcher.scheduleIfNeeded();
+        //AwVariationsSeedFetcher.scheduleIfNeeded();
     }
 
     // overridden by tests
