6f8575232ab4cf0ac216f0a952cd9be405e615e5
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-07-08 18:16:34 +0200

    Disable fetching of all field trials
    Disable seed-based field trials.
    Disable reporting of Omnibox suggestions header.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ibc8a7b748877a8480a5c2e7228a99a9e8990c5b8

--- content/browser/loader/url_loader_throttles.cc
+++ content/browser/loader/url_loader_throttles.cc
@@ -52,8 +52,6 @@ CreateContentBrowserURLLoaderThrottles(
   // TODO(crbug.com/40135370): Consider whether we want to use the WebContents
   // to determine the value for variations::Owner. Alternatively, this is the
   // browser side, and we might be fine with Owner::kUnknown.
-  variations::VariationsURLLoaderThrottle::AppendThrottleIfNeeded(
-      browser_context->GetVariationsClient(), &throttles);
 
   ClientHintsControllerDelegate* client_hint_delegate =
       browser_context->GetClientHintsControllerDelegate();
@@ -132,11 +130,6 @@ CreateContentBrowserURLLoaderThrottlesForKeepAlive(
       GetContentClient()->browser()->CreateURLLoaderThrottlesForKeepAlive(
           request, browser_context, wc_getter, frame_tree_node_id);
   variations::OmniboxURLLoaderThrottle::AppendThrottleIfNeeded(&throttles);
-  // TODO(crbug.com/40135370): Consider whether we want to use the WebContents
-  // to determine the value for variations::Owner. Alternatively, this is the
-  // browser side, and we might be fine with Owner::kUnknown.
-  variations::VariationsURLLoaderThrottle::AppendThrottleIfNeeded(
-      browser_context->GetVariationsClient(), &throttles);
 
   auto throttle = MaybeCreateIdentityUrlLoaderThrottle(base::BindRepeating(
       webid::SetIdpSigninStatus, browser_context, frame_tree_node_id));
