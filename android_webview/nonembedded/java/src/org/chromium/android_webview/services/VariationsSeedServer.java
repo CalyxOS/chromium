5ddd2c3b6c2ef559c79aafcb8efc0b3a42f4a573
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-07-08 18:16:34 +0200

    Disable fetching of all field trials
    Disable seed-based field trials.
    Disable reporting of Omnibox suggestions header.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedServer.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedServer.java
@@ -31,8 +31,6 @@ public class VariationsSeedServer extends Service {
         @Override
         public void getSeed(ParcelFileDescriptor newSeedFile, long oldSeedDate,
                 IVariationsSeedServerCallback callback) {
-            maybeReportMetrics(callback);
-            VariationsSeedHolder.getInstance().writeSeedIfNewer(newSeedFile, oldSeedDate);
         }
     };
 
