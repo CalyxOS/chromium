e664a67467106bf199cd4c9721afab634ab71d68
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-07-08 18:16:34 +0200

    Disable fetching of all field trials
    Disable seed-based field trials.
    Disable reporting of Omnibox suggestions header.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ibc8a7b748877a8480a5c2e7228a99a9e8990c5b8

--- components/variations/service/variations_field_trial_creator_base.cc
+++ components/variations/service/variations_field_trial_creator_base.cc
@@ -132,6 +132,7 @@ Study::CpuArchitecture GetCurrentCpuArchitecture() {
 }
 
 #if BUILDFLAG(FIELDTRIAL_TESTING_ENABLED)
+#error FIELDTRIAL_TESTING_ENABLED should be disabled.
 // Determines whether the field trial testing config defined in
 // testing/variations/fieldtrial_testing_config.json should be applied. If the
 // "disable_fieldtrial_testing_config" GN flag is set to true, then the testing
@@ -338,9 +339,6 @@ bool VariationsFieldTrialCreatorBase::SetUpFieldTrials(
         entropy_providers, feature_list.get(), safe_seed_manager,
         synthetic_trial_registry, std::move(client_filterable_state));
   }
-
-  platform_field_trials->SetUpClientSideFieldTrials(
-      used_seed, entropy_providers, feature_list.get());
 
   platform_field_trials->RegisterFeatureOverrides(feature_list.get());
 
@@ -653,6 +651,7 @@ bool VariationsFieldTrialCreatorBase::CreateTrialsFromSeed(
     SafeSeedManagerBase* safe_seed_manager,
     SyntheticTrialRegistry* synthetic_trial_registry,
     std::unique_ptr<ClientFilterableState> client_state) {
+  if ((true)) return false;
   // This histogram name uses "VariationsFieldTrialCreator" rather than
   // "VariationsFieldTrialCreatorBase" for consistency with historical data
   TRACE_EVENT0("startup", "VariationsFieldTrialCreator::CreateTrialsFromSeed");
@@ -765,6 +764,7 @@ bool VariationsFieldTrialCreatorBase::CreateTrialsFromSeed(
 
 void VariationsFieldTrialCreatorBase::LoadSeedFromJsonFile(
     const base::FilePath& json_seed_path) {
+  if ((true)) return;
   VLOG(1) << "Loading seed from JSON file:" << json_seed_path;
   JSONFileValueDeserializer file_deserializer(json_seed_path);
   int error_code;
