ba1d9adf0d24e4d14354f981950097237582f6a6
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-07-08 18:16:34 +0200

    Disable fetching of all field trials
    Disable seed-based field trials.
    Disable reporting of Omnibox suggestions header.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ibc8a7b748877a8480a5c2e7228a99a9e8990c5b8

--- components/variations/android/java/src/org/chromium/components/variations/firstrun/VariationsSeedFetcher.java
+++ components/variations/android/java/src/org/chromium/components/variations/firstrun/VariationsSeedFetcher.java
@@ -451,35 +451,13 @@ public class VariationsSeedFetcher {
      */
     public void fetchSeed(String restrictMode, String milestone, String channel) {
         assert !ThreadUtils.runningOnUiThread();
-        // Prevent multiple simultaneous fetches
-        synchronized (sLock) {
-            SharedPreferences prefs = ContextUtils.getAppSharedPreferences();
-            // Early return if an attempt has already been made to fetch the seed, even if it
-            // failed. Only attempt to get the initial Java seed once, since a failure probably
-            // indicates a network problem that is unlikely to be resolved by a second attempt.
-            // Note that VariationsSeedBridge.hasNativePref() is a pure Java function, reading an
-            // Android preference that is set when the seed is fetched by the native code.
-            if (prefs.getBoolean(VARIATIONS_INITIALIZED_PREF, false)
-                    || VariationsSeedBridge.hasNativePref()) {
-                return;
-            }
+         synchronized (sLock) {
+            VariationsSeedBridge.clearFirstRunPrefs();
 
-            SeedFetchParameters params =
-                    SeedFetchParameters.Builder.newBuilder()
-                            .setPlatform(VariationsSeedFetcher.VariationsPlatform.ANDROID)
-                            .setRestrictMode(null)
-                            .setMilestone(milestone)
-                            .setChannel(channel)
-                            .build();
-            SeedFetchInfo fetchInfo = downloadContent(params, null);
-            if (fetchInfo.seedInfo != null) {
-                SeedInfo info = fetchInfo.seedInfo;
-                VariationsSeedBridge.setVariationsFirstRunSeed(info.seedData, info.signature,
-                        info.country, info.date, info.isGzipCompressed);
-            }
+            SharedPreferences prefs = ContextUtils.getAppSharedPreferences();
             // VARIATIONS_INITIALIZED_PREF should still be set to true when exceptions occur
             prefs.edit().putBoolean(VARIATIONS_INITIALIZED_PREF, true).apply();
-        }
+         }
     }
 
     private void recordFetchResultOrCode(int resultOrCode) {
@@ -523,7 +501,7 @@ public class VariationsSeedFetcher {
      * @param curSeedInfo optional currently saved seed info to set the `If-None-Match` header.
      * @return the object holds the request result and seed data with its related header fields.
      */
-    public SeedFetchInfo downloadContent(SeedFetchParameters params, SeedInfo currInfo) {
+    private SeedFetchInfo downloadContent(SeedFetchParameters params, SeedInfo currInfo) {
         SeedFetchInfo fetchInfo = new SeedFetchInfo();
         HttpURLConnection connection = null;
         try {
