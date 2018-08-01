43777e0ca6f1a9d8233a2a5a0179595639e42174
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-08-01 09:19:40 +0200

    Add bookmark import/export actions
    Add bookmark import/export actions in bookmarks activity and page
    Reduce permissions needed for bookmarks import/export
    Completely remove contacts picker permission from the file dialog
    
    Requires: Adds-support-for-writing-URIs.patch
    Requires: Restore-BookmarkToolbar-setCurrentFolder.patch
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I708110ab421d1710ff5c5bc1774512691d49ff3f

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediator.java
@@ -53,6 +53,8 @@ import org.chromium.components.browser_ui.widget.selectable_list.SelectionDelega
 import org.chromium.components.commerce.core.CommerceSubscription;
 import org.chromium.components.commerce.core.ShoppingService;
 import org.chromium.components.commerce.core.SubscriptionsObserver;
+import org.chromium.ui.base.ActivityWindowAndroid;
+import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.components.favicon.LargeIconBridge;
 import org.chromium.components.power_bookmarks.PowerBookmarkMeta;
 import org.chromium.components.power_bookmarks.PowerBookmarkType;
@@ -80,6 +82,9 @@ class BookmarkManagerMediator
     private static final int SEARCH_BOX_MAX_INDEX = 0;
 
     private static boolean sPreventLoadingForTesting;
+
+    private ActivityWindowAndroid mWindowAndroid;
+    private ModalDialogManager mModalDialogManager;
 
     /** Keeps track of whether drag is enabled / active for bookmark lists. */
     private class BookmarkDragStateDelegate implements DragStateDelegate {
@@ -544,6 +549,14 @@ class BookmarkManagerMediator
         mNativePage = nativePage;
     }
 
+    /**
+     * Sets the Android window that is used by further intents created by the bookmark activity.
+     */
+    public void setWindow(ActivityWindowAndroid window, ModalDialogManager modalDialogManager) {
+        mWindowAndroid = window;
+        mModalDialogManager = modalDialogManager;
+    }
+
     /** See BookmarkManager(Coordinator)#updateForUrl */
     void updateForUrl(String url) {
         // Bookmark model is null if the manager has been destroyed.
@@ -722,6 +735,16 @@ class BookmarkManagerMediator
         if (mBookmarkOpener.openBookmarksInNewTabs(bookmarks, incognito)) {
             BookmarkUtils.finishActivityOnPhone(mContext);
         }
+    }
+
+    @Override
+    public void importBookmarks() {
+        mBookmarkModel.importBookmarks(mWindowAndroid);
+    }
+
+    @Override
+    public void exportBookmarks() {
+        mBookmarkModel.exportBookmarks(mWindowAndroid, mModalDialogManager);
     }
 
     @Override
