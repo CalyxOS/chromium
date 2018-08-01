aa271f182848a9993ecd95fedf80dfd44d0e2611
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-08-01 09:19:40 +0200

    Add bookmark import/export actions
    Add bookmark import/export actions in bookmarks activity and page
    Reduce permissions needed for bookmarks import/export
    Completely remove contacts picker permission from the file dialog
    
    Requires: Adds-support-for-writing-URIs.patch
    Requires: Restore-BookmarkToolbar-setCurrentFolder.patch
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I708110ab421d1710ff5c5bc1774512691d49ff3f

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -216,6 +216,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTAuthTab,
     &kCCTAuthTabDisableAllExternalIntents,
     &kCCTAuthTabEnableHttpsRedirects,
+    &kBookmarksExportUseSaf,
     &kCCTBeforeUnload,
     &kCCTClientDataHeader,
     &kCCTEarlyNav,
