e4061709a13bd6cf3acc3f8cb44b384b1547ce32
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-08-01 09:19:40 +0200

    Add bookmark import/export actions
    Add bookmark import/export actions in bookmarks activity and page
    Reduce permissions needed for bookmarks import/export
    Completely remove contacts picker permission from the file dialog
    
    Requires: Adds-support-for-writing-URIs.patch
    Requires: Restore-BookmarkToolbar-setCurrentFolder.patch
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I708110ab421d1710ff5c5bc1774512691d49ff3f

--- ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
+++ ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
@@ -46,6 +46,7 @@ import org.chromium.base.task.AsyncTask;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.ui.R;
+import org.chromium.ui.widget.Toast;
 import org.chromium.ui.UiUtils;
 
 import java.io.File;
@@ -68,6 +69,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
     private static final String TAG = "SelectFileDialog";
     private static final String IMAGE_TYPE = "image";
     private static final String VIDEO_TYPE = "video";
+    private static final String HTML_TYPE = "html";
     private static final String AUDIO_TYPE = "audio";
     private static final String ALL_TYPES = "*/*";
     private static final String GENERIC_TYPE = "application/octet-stream";
@@ -313,6 +315,11 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
         mMimeTypes = convertToSupportedMimeTypes(mFileTypes);
     }
 
+    @CalledByNative
+    private void showToast(String message) {
+        Toast.makeText(ContextUtils.getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     /**
      * Creates and starts an intent based on the passed fileTypes and capture value.
      *
@@ -359,7 +366,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
         List<String> missingPermissions = new ArrayList<>();
         String storagePermission = Manifest.permission.READ_EXTERNAL_STORAGE;
         boolean shouldUsePhotoPicker = shouldUsePhotoPicker();
-        if (shouldUsePhotoPicker) {
+        if (shouldUsePhotoPicker || shouldShowHtmlTypes()) {
             // The permission scenario for accessing media has evolved a bit over the years:
             // Early on, READ_EXTERNAL_STORAGE was required to access media, but that permission was
             // later deprecated. In its place (starting with Android T) READ_MEDIA_IMAGES and
@@ -412,7 +419,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
 
                                 // TODO(finnur): Remove once we figure out the cause of
                                 // crbug.com/950024.
-                                if (shouldUsePhotoPicker) {
+                                if (shouldUsePhotoPicker || shouldShowHtmlTypes()) {
                                     if (permissions.length != requestPermissions.length) {
                                         throw new RuntimeException(
                                                 String.format(
@@ -429,7 +436,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
                                     }
                                 }
 
-                                if (shouldUsePhotoPicker) {
+                                if (shouldUsePhotoPicker || shouldShowHtmlTypes()) {
                                     if (permissions[i].equals(storagePermission)
                                             || permissions[i].equals(
                                                     Manifest.permission.READ_MEDIA_IMAGES)
@@ -740,6 +747,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
                 mimeTypes.add(mimeType);
             }
         }
+        if (mimeTypes.size() == 0) return null;
         return mimeTypes;
     }
 
@@ -1118,6 +1126,10 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
      */
     private boolean acceptsOnlyType(String superType) {
         return countAcceptTypesFor(superType) == mMimeTypes.size();
+    }
+
+    private boolean shouldShowHtmlTypes() {
+        return countAcceptTypesFor(HTML_TYPE) > 0;
     }
 
     /**
