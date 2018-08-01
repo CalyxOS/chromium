3d71fc309c88bbc6d5ded182ccf8b2d259aabab5
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-08-01 09:19:40 +0200

    Add bookmark import/export actions
    Add bookmark import/export actions in bookmarks activity and page
    Reduce permissions needed for bookmarks import/export
    Completely remove contacts picker permission from the file dialog
    
    Requires: Adds-support-for-writing-URIs.patch
    Requires: Restore-BookmarkToolbar-setCurrentFolder.patch
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I708110ab421d1710ff5c5bc1774512691d49ff3f

--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkPage.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkPage.java
@@ -16,6 +16,9 @@ import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.ui.native_page.BasicNativePage;
 import org.chromium.chrome.browser.ui.native_page.NativePageHost;
 import org.chromium.components.embedder_support.util.UrlConstants;
+import org.chromium.chrome.browser.app.ChromeActivity;
+import org.chromium.ui.modaldialog.ModalDialogManager;
+import org.chromium.components.browser_ui.modaldialog.AppModalPresenter;
 
 /** A native page holding a {@link BookmarkManagerCoordinator} on _tablet_. */
 public class BookmarkPage extends BasicNativePage {
@@ -35,7 +38,8 @@ public class BookmarkPage extends BasicNativePage {
             @NonNull SnackbarManager snackbarManager,
             @NonNull Profile profile,
             @NonNull NativePageHost host,
-            @Nullable ComponentName componentName) {
+            @Nullable ComponentName componentName,
+            ChromeActivity activity) {
         super(host);
         mTitle = host.getContext().getString(R.string.bookmarks);
 
@@ -55,6 +59,9 @@ public class BookmarkPage extends BasicNativePage {
                         new BookmarkManagerOpenerImpl(),
                         PriceDropNotificationManagerFactory.create(profile));
         mBookmarkManagerCoordinator.setBasicNativePage(this);
+        mBookmarkManagerCoordinator.setWindow(activity.getWindowAndroid(),
+            new ModalDialogManager(
+                new AppModalPresenter(activity), ModalDialogManager.ModalDialogType.APP));
         initWithView(mBookmarkManagerCoordinator.getView());
     }
 
