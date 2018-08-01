a2776e23d5f4bdde1fad9d0da414435a67beb560
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-08-01 09:19:40 +0200

    Add bookmark import/export actions
    Add bookmark import/export actions in bookmarks activity and page
    Reduce permissions needed for bookmarks import/export
    Completely remove contacts picker permission from the file dialog
    
    Requires: Adds-support-for-writing-URIs.patch
    Requires: Restore-BookmarkToolbar-setCurrentFolder.patch
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I708110ab421d1710ff5c5bc1774512691d49ff3f

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkPage.java
@@ -13,6 +13,9 @@ import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.ui.native_page.BasicNativePage;
 import org.chromium.chrome.browser.ui.native_page.NativePageHost;
 import org.chromium.components.embedder_support.util.UrlConstants;
+import org.chromium.chrome.browser.app.ChromeActivity;
+import org.chromium.ui.modaldialog.ModalDialogManager;
+import org.chromium.components.browser_ui.modaldialog.AppModalPresenter;
 
 /** A native page holding a {@link BookmarkManagerCoordinator} on _tablet_. */
 public class BookmarkPage extends BasicNativePage {
@@ -31,7 +34,7 @@ public class BookmarkPage extends BasicNativePage {
             ComponentName componentName,
             SnackbarManager snackbarManager,
             Profile profile,
-            NativePageHost host) {
+            NativePageHost host, ChromeActivity activity) {
         super(host);
 
         mBookmarkManagerCoordinator =
@@ -44,6 +47,9 @@ public class BookmarkPage extends BasicNativePage {
                         new BookmarkUiPrefs(ChromeSharedPreferences.getInstance()),
                         /* bookmarkOpenedCallback= */ null);
         mBookmarkManagerCoordinator.setBasicNativePage(this);
+        mBookmarkManagerCoordinator.setWindow(activity.getWindowAndroid(),
+                           new ModalDialogManager(
+                                new AppModalPresenter(activity), ModalDialogManager.ModalDialogType.APP));
         mTitle = host.getContext().getString(R.string.bookmarks);
 
         initWithView(mBookmarkManagerCoordinator.getView());
--- ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
+++ ui/android/java/src/org/chromium/ui/base/SelectFileDialog.java
@@ -51,6 +51,7 @@ import org.chromium.build.annotations.Initializer;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.ui.R;
+import org.chromium.ui.widget.Toast;
 import org.chromium.ui.UiUtils;
 
 import java.io.File;
@@ -74,6 +75,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
     private static final String TAG = "SelectFileDialog";
     private static final String IMAGE_TYPE = "image";
     private static final String VIDEO_TYPE = "video";
+    private static final String HTML_TYPE = "html";
     private static final String AUDIO_TYPE = "audio";
     private static final String ALL_TYPES = "*/*";
     private static final String GENERIC_TYPE = "application/octet-stream";
@@ -323,6 +325,11 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
         mMimeTypes = convertToSupportedMimeTypes(mFileTypes);
     }
 
+    @CalledByNative
+    private void showToast(String message) {
+        Toast.makeText(ContextUtils.getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
     /**
      * Creates and starts an intent based on the passed fileTypes and capture value.
      *
@@ -407,7 +414,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
         List<String> missingPermissions = new ArrayList<>();
         String storagePermission = Manifest.permission.READ_EXTERNAL_STORAGE;
         boolean shouldUsePhotoPicker = shouldUsePhotoPicker();
-        if (shouldUsePhotoPicker) {
+        if (shouldUsePhotoPicker || shouldShowHtmlTypes()) {
             // The permission scenario for accessing media has evolved a bit over the years:
             // Early on, READ_EXTERNAL_STORAGE was required to access media, but that permission was
             // later deprecated. In its place (starting with Android T) READ_MEDIA_IMAGES and
@@ -460,7 +467,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
 
                                 // TODO(finnur): Remove once we figure out the cause of
                                 // crbug.com/950024.
-                                if (shouldUsePhotoPicker) {
+                                if (shouldUsePhotoPicker || shouldShowHtmlTypes()) {
                                     if (permissions.length != requestPermissions.length) {
                                         throw new RuntimeException(
                                                 String.format(
@@ -477,7 +484,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
                                     }
                                 }
 
-                                if (shouldUsePhotoPicker) {
+                                if (shouldUsePhotoPicker || shouldShowHtmlTypes()) {
                                     if (permissions[i].equals(storagePermission)
                                             || permissions[i].equals(
                                                     Manifest.permission.READ_MEDIA_IMAGES)
@@ -783,6 +790,7 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
                 mimeTypes.add(mimeType);
             }
         }
+        if (mimeTypes.size() == 0) return null;
         return mimeTypes;
     }
 
@@ -1153,6 +1161,10 @@ public class SelectFileDialog implements WindowAndroid.IntentCallback, PhotoPick
      */
     private boolean acceptsOnlyType(String superType) {
         return countAcceptTypesFor(superType) == mMimeTypes.size();
+    }
+
+    private boolean shouldShowHtmlTypes() {
+        return countAcceptTypesFor(HTML_TYPE) > 0;
     }
 
     /**
