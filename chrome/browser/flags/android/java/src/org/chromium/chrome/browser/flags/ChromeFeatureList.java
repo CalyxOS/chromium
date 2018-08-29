38a5897578f3a38f1a4486bf664dac388d1534b2
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-08-29 11:03:44 +0200

    Add custom tab intents privacy option
    Add custom tab intents privacy option and force
    open external links in incognito flag.
    Use the CCT ephemeral mode.
    
    Flags are mutually exclusive.
    
    See also: https://github.com/bromite/bromite/issues/1474
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I0707a6083443b02ae6432485eacba224b497b1e3

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -768,6 +768,8 @@ public abstract class ChromeFeatureList {
     public static final CachedFlag sMagicStackAndroid = newCachedFlag(MAGIC_STACK_ANDROID, true);
     public static final CachedFlag sMostVisitedTilesCustomization =
             newCachedFlag(MOST_VISITED_TILES_CUSTOMIZATION, false);
+    public static final CachedFlag sMayLaunchurlUsesSeparateStoragePartition =
+            newCachedFlag(MAYLAUNCHURL_USES_SEPARATE_STORAGE_PARTITION, false);
     public static final CachedFlag sMostVisitedTilesReselect =
             newCachedFlag(MOST_VISITED_TILES_RESELECT, false);
     public static final CachedFlag sMultiInstanceApplicationStatusCleanup =
@@ -915,6 +917,7 @@ public abstract class ChromeFeatureList {
                     sNotificationTrampoline,
                     sMagicStackAndroid,
                     sMostVisitedTilesCustomization,
+                    sMayLaunchurlUsesSeparateStoragePartition,
                     sMostVisitedTilesReselect,
                     sMultiInstanceApplicationStatusCleanup,
                     sNavBarColorAnimation,
