a686708e6f866546563da8cc5d07e000fddbc23d
csagan5 <32685696+csagan5@users.noreply.github.com>
2018-08-29 11:03:44 +0200

    Add custom tab intents privacy option
    Add custom tab intents privacy option and force
    open external links in incognito flag.
    Use the CCT ephemeral mode.
    
    Flags are mutually exclusive.
    
    See also: https://github.com/bromite/bromite/issues/1474
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I0707a6083443b02ae6432485eacba224b497b1e3

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProvider.java
@@ -1229,7 +1229,9 @@ public class CustomTabIntentDataProvider extends BrowserServicesIntentDataProvid
     public @CustomTabProfileType int getCustomTabMode() {
         return AlwaysIncognitoLinkInterceptor.isAlwaysIncognito()
                 ? CustomTabProfileType.INCOGNITO
-                : CustomTabProfileType.REGULAR;
+                : (ChromeFeatureList.sMayLaunchurlUsesSeparateStoragePartition.isEnabled()
+                    ? CustomTabProfileType.EPHEMERAL
+                    : CustomTabProfileType.REGULAR);
     }
 
     @Override
