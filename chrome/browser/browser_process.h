20827e03c5f774f3eeaf25315808c70a7eb18dc3
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-09-14 10:20:08 +0200

    Bromite subresource adblocker
    Add option to configure the ad blocker filters URL
    Disable look-alike, metrics, ablation and navigation throttles
    Do not use experiments to enable/disable presets
    Always enable ad filtering
    Download filters by checking Last-Modified header first
    Fix RestoreForeignSessionTab by recreating the tab (issue #681)
    Enable AutomaticLazyFrameLoadingToAds and AutomaticLazyFrameLoadingToEmbeds features
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I09c0504269c659e0f125dfa982148b705cd6528b

--- chrome/browser/browser_process.h
+++ chrome/browser/browser_process.h
@@ -31,6 +31,7 @@
 #include "build/chromeos_buildflags.h"
 #include "chrome/common/buildflags.h"
 #include "media/media_buildflags.h"
+#include "components/component_updater/adblock_updater_service.h"
 
 class BackgroundModeManager;
 class BrowserProcessPlatformPart;
@@ -84,6 +85,10 @@ class VariationsService;
 
 namespace component_updater {
 class ComponentUpdateService;
+}
+
+namespace adblock_updater {
+class AdBlockUpdaterService;
 }
 
 namespace gcm {
@@ -264,6 +269,7 @@ class BrowserProcess {
 #endif
 
   virtual component_updater::ComponentUpdateService* component_updater() = 0;
+  virtual adblock_updater::AdBlockUpdaterService* adblock_updater() = 0;
 
   virtual MediaFileSystemRegistry* media_file_system_registry() = 0;
 
