2fa08091d2466f10751d73fe4dc3c01f94358f78
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-09-14 10:20:08 +0200

    Bromite subresource adblocker
    Add option to configure the ad blocker filters URL
    Disable look-alike, metrics, ablation and navigation throttles
    Do not use experiments to enable/disable presets
    Always enable ad filtering
    Download filters by checking Last-Modified header first
    Fix RestoreForeignSessionTab by recreating the tab (issue #681)
    Enable AutomaticLazyFrameLoadingToAds and AutomaticLazyFrameLoadingToEmbeds features
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I09c0504269c659e0f125dfa982148b705cd6528b

--- components/subresource_filter/core/browser/verified_ruleset_dealer.cc
+++ components/subresource_filter/core/browser/verified_ruleset_dealer.cc
@@ -11,6 +11,7 @@
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
+#include "base/logging.h"
 #include "base/not_fatal_until.h"
 #include "base/notreached.h"
 #include "base/task/sequenced_task_runner.h"
@@ -40,6 +41,9 @@ RulesetFilePtr VerifiedRulesetDealer::OpenAndSetRulesetFile(
   TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("loading"),
                "VerifiedRulesetDealer::OpenAndSetRulesetFile", "file_valid",
                file->IsValid());
+
+  LOG(INFO) << "OpenAndSetRulesetFile: " << file_path << " is valid: " << file->IsValid();
+
   if (file->IsValid()) {
     SetRulesetFile(file->Duplicate());
     expected_checksum_ = expected_checksum;
