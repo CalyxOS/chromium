698b856cc4e06bf437d69c18e2dbc6f20672f5fb
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-09-14 10:20:08 +0200

    Bromite subresource adblocker
    Add option to configure the ad blocker filters URL
    Disable look-alike, metrics, ablation and navigation throttles
    Do not use experiments to enable/disable presets
    Always enable ad filtering
    Download filters by checking Last-Modified header first
    Fix RestoreForeignSessionTab by recreating the tab (issue #681)
    Enable AutomaticLazyFrameLoadingToAds and AutomaticLazyFrameLoadingToEmbeds features
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I09c0504269c659e0f125dfa982148b705cd6528b

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFeatureFlags.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFeatureFlags.java
@@ -186,6 +186,14 @@ public class CachedFeatureFlags {
                 ChromeFeatureList.isEnabled(ChromeFeatureList.BACKGROUND_THREAD_POOL));
     }
 
+     public static void setAdBlockFiltersURL(String url) {
+         CachedFeatureFlagsJni.get().setAdBlockFiltersURL(url);
+     }
+
+     public static String getAdBlockFiltersURL() {
+         return CachedFeatureFlagsJni.get().getAdBlockFiltersURL();
+     }
+
     /**
      * Caches flags that must take effect on startup but are set via native code.
      */
@@ -423,5 +431,7 @@ public class CachedFeatureFlags {
     @NativeMethods
     interface Natives {
         boolean isNetworkServiceWarmUpEnabled();
+        void setAdBlockFiltersURL(String url);
+        String getAdBlockFiltersURL();
     }
 }
