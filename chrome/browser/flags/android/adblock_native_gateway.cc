6fce4f8c4a0f99b850d102e7025aa11f110ecfb9
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-09-14 10:20:08 +0200

    Bromite subresource adblocker
    Add option to configure the ad blocker filters URL
    Disable look-alike, metrics, ablation and navigation throttles
    Do not use experiments to enable/disable presets
    Always enable ad filtering
    Download filters by checking Last-Modified header first
    Fix RestoreForeignSessionTab by recreating the tab (issue #681)
    Enable AutomaticLazyFrameLoadingToAds and AutomaticLazyFrameLoadingToEmbeds features
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I09c0504269c659e0f125dfa982148b705cd6528b

---
+++ chrome/browser/flags/android/adblock_native_gateway.cc
@@ -0,0 +1,31 @@
+#include "chrome/browser/flags/jni_headers/AdBlockNativeGateway_jni.h"
+
+#include "base/android/jni_string.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/common/pref_names.h"
+#include "components/prefs/pref_service.h"
+
+using base::android::ScopedJavaLocalRef;
+using base::android::JavaParamRef;
+
+static ScopedJavaLocalRef<jstring> JNI_AdBlockNativeGateway_GetAdBlockFiltersURL(
+    JNIEnv* env) {
+  return base::android::ConvertUTF8ToJavaString(env,
+      g_browser_process->local_state()->GetString(prefs::kAdBlockFiltersURL));
+}
+
+static void JNI_AdBlockNativeGateway_SetAdBlockFiltersURL(
+    JNIEnv* env, const JavaParamRef<jstring>& url) {
+  g_browser_process->local_state()->SetString(prefs::kAdBlockFiltersURL,
+      base::android::ConvertJavaStringToUTF8(env, url));
+}
+
+static jboolean JNI_AdBlockNativeGateway_GetAdBlockEnabled(
+    JNIEnv* env) {
+  return g_browser_process->local_state()->GetBoolean(prefs::kAdBlockEnabled);
+}
+
+static void JNI_AdBlockNativeGateway_SetAdBlockEnabled(
+    JNIEnv* env, jboolean enabled) {
+  g_browser_process->local_state()->SetBoolean(prefs::kAdBlockEnabled, enabled);
+}
