43e7ad50da1a4a8ca06d9930425efc022095a90b
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-10-27 10:00:13 +0100

    Replace DoH probe domain with Cloudflare domain
    Original patch: "Replace DoH probe domain with RIPE domain".
    Modified to use Cloudflare.
    Authorship and the remainder of the commit message have been retained.
    
    Replace known good hostname
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ib40a378d47fef111fa3602acc5519f4809cad4ee

--- net/dns/dns_transaction.h
+++ net/dns/dns_transaction.h
@@ -29,7 +29,7 @@ class NetLogWithSource;
 class ResolveContext;
 
 // The hostname probed by CreateDohProbeRunner().
-inline constexpr base::StringPiece kDohProbeHostname = "www.ripe.net";
+inline constexpr base::StringPiece kDohProbeHostname = "cloudflare-dns.com";
 
 // DnsTransaction implements a stub DNS resolver as defined in RFC 1034.
 // The DnsTransaction takes care of retransmissions, name server fallback (or
