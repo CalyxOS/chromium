72224afcde2616c2269ebbd2cfa22c88a9d917a3
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-10-30 11:50:13 +0100

    Block 'qjz9zk' or 'trk:' requests
    An info bar is displayed unless the --disable-trkbar command-line flag or the chrome://flag option is used.
    This patch is based on Iridium's 'net: add "trk:" scheme and help identify URLs being retrieved'
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Idc70e3076794d2e4a85c25ae66327ce9f2c8f7e9

--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -864,6 +864,7 @@ ChildProcessSecurityPolicyImpl::ChildProcessSecurityPolicyImpl()
   RegisterPseudoScheme(url::kJavaScriptScheme);
   RegisterPseudoScheme(kViewSourceScheme);
   RegisterPseudoScheme(kGoogleChromeScheme);
+  RegisterWebSafeScheme(url::kTraceScheme);
 }
 
 ChildProcessSecurityPolicyImpl::~ChildProcessSecurityPolicyImpl() {
