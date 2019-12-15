5b72c0ef826d7c53111d729bb67a9f87930d7305
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-12-15 19:40:37 +0100

    Use dummy DFM installer
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I6336b599175f31e7dd750d90d3b6f882e28a421c

--- components/module_installer/android/java/src/org/chromium/components/module_installer/logger/Logger.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/logger/Logger.java
@@ -4,9 +4,6 @@
 
 package org.chromium.components.module_installer.logger;
 
-import com.google.android.play.core.splitinstall.model.SplitInstallErrorCode;
-import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus;
-
 /**
  * Logger for SplitCompat Engine.
  */
@@ -17,7 +14,7 @@ public interface Logger {
      * @param moduleName The module name.
      * @param errorCode The error code.
      */
-    void logRequestFailure(String moduleName, @SplitInstallErrorCode int errorCode);
+    void logRequestFailure(String moduleName, int errorCode);
 
     /**
      * Logs exceptions that happen during the installation process.
@@ -25,7 +22,7 @@ public interface Logger {
      * @param moduleName The module name.
      * @param errorCode The error code.
      */
-    void logStatusFailure(String moduleName, @SplitInstallErrorCode int errorCode);
+    void logStatusFailure(String moduleName, int errorCode);
 
     /**
      * Logs the status count and duration during a module installation process.
@@ -33,7 +30,7 @@ public interface Logger {
      * @param moduleName The module name
      * @param status The status code
      */
-    void logStatus(String moduleName, @SplitInstallSessionStatus int status);
+    void logStatus(String moduleName, int status);
 
     /**
      * Logs the request start time.
@@ -48,11 +45,4 @@ public interface Logger {
      * @param moduleName The module name.
      */
     void logRequestDeferredStart(String moduleName);
-
-    /**
-     * Gets the error code for an unknown error thrown at module request time.
-     *
-     * @return The error code.
-     */
-    int getUnknownRequestErrorCode();
 }
