aded58fd869a5d36aa861f2ac1a91ec8bedf7582
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-12-15 19:40:37 +0100

    Use dummy DFM installer
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I6336b599175f31e7dd750d90d3b6f882e28a421c

--- components/module_installer/android/java/src/org/chromium/components/module_installer/engine/SplitCompatEngine.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/engine/SplitCompatEngine.java
@@ -8,10 +8,6 @@ import android.app.Activity;
 
 import androidx.annotation.VisibleForTesting;
 
-import com.google.android.play.core.splitinstall.SplitInstallRequest;
-import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener;
-import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus;
-
 import org.chromium.base.ThreadUtils;
 
 import java.util.ArrayList;
@@ -24,7 +20,6 @@ import java.util.Set;
 /** Install engine that uses Play Core and SplitCompat to install modules. */
 class SplitCompatEngine implements InstallEngine {
     private final SplitCompatEngineFacade mFacade;
-    private final SplitInstallStateUpdatedListener mUpdateListener = getStatusUpdateListener();
     private static final Map<String, List<InstallListener>> sSessions = new HashMap<>();
 
     public SplitCompatEngine() {
@@ -42,70 +37,19 @@ class SplitCompatEngine implements InstallEngine {
 
     @Override
     public boolean isInstalled(String moduleName) {
-        Set<String> installedModules = mFacade.getSplitManager().getInstalledModules();
-        return installedModules.contains(moduleName);
+       return false;
     }
 
     @Override
     public void installDeferred(String moduleName) {
-        mFacade.getSplitManager().deferredInstall(Collections.singletonList(moduleName));
     }
 
     @Override
     public void install(String moduleName, InstallListener listener) {
         ThreadUtils.assertOnUiThread();
-
-        List<InstallListener> listeners = sSessions.get(moduleName);
-        if (listeners != null) {
-            listeners.add(listener);
-            return;
-        }
-
-        registerUpdateListener();
-
-        listeners = new ArrayList<>();
-        listeners.add(listener);
-        sSessions.put(moduleName, listeners);
-
-        SplitInstallRequest request = mFacade.createSplitInstallRequest(moduleName);
-
-        mFacade.getSplitManager()
-                .startInstall(request)
-                .addOnFailureListener(
-                        ex -> {
-                            notifyListeners(moduleName, false);
-                        });
-    }
-
-    private SplitInstallStateUpdatedListener getStatusUpdateListener() {
-        return state -> {
-            assert !state.moduleNames().isEmpty();
-
-            int status = state.status();
-            List<String> modules = state.moduleNames();
-
-            if (status == SplitInstallSessionStatus.INSTALLED) {
-                mFacade.updateCrashKeys();
-            }
-
-            for (String moduleName : modules) {
-                switch (status) {
-                    case SplitInstallSessionStatus.INSTALLED:
-                        notifyListeners(moduleName, true);
-                        break;
-                    case SplitInstallSessionStatus.FAILED:
-                        notifyListeners(moduleName, false);
-                        break;
-                }
-            }
-        };
     }
 
     private void notifyListeners(String moduleName, Boolean success) {
-        for (InstallListener listener : sSessions.get(moduleName)) {
-            notifyListener(listener, success);
-        }
-
         sSessions.remove(moduleName);
 
         unregisterUpdateListener();
@@ -120,15 +64,9 @@ class SplitCompatEngine implements InstallEngine {
     }
 
     private void registerUpdateListener() {
-        if (sSessions.size() == 0) {
-            mFacade.getSplitManager().registerListener(mUpdateListener);
-        }
     }
 
     private void unregisterUpdateListener() {
-        if (sSessions.size() == 0) {
-            mFacade.getSplitManager().unregisterListener(mUpdateListener);
-        }
     }
 
     @VisibleForTesting
