fc9686a078a72fae99b7003d8c662d6c6d453e7e
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-12-15 19:40:37 +0100

    Use dummy DFM installer
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I6336b599175f31e7dd750d90d3b6f882e28a421c

--- components/module_installer/android/java/src/org/chromium/components/module_installer/engine/SplitCompatEngineFacade.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/engine/SplitCompatEngineFacade.java
@@ -6,31 +6,28 @@ package org.chromium.components.module_installer.engine;
 
 import android.app.Activity;
 
-import com.google.android.play.core.splitcompat.SplitCompat;
+/*import com.google.android.play.core.splitcompat.SplitCompat;
 import com.google.android.play.core.splitinstall.SplitInstallManager;
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory;
-import com.google.android.play.core.splitinstall.SplitInstallRequest;
+import com.google.android.play.core.splitinstall.SplitInstallRequest;*/
 
 import org.chromium.base.ContextUtils;
 import org.chromium.components.module_installer.logger.Logger;
 import org.chromium.components.module_installer.logger.PlayCoreLogger;
-import org.chromium.components.module_installer.util.ModuleUtil;
+//import org.chromium.components.module_installer.util.ModuleUtil;
 
 /**
  * PlayCore SplitCompatEngine Context. Class used to segregate external dependencies that
  * cannot be easily mocked and simplify the engine's design.
  */
 class SplitCompatEngineFacade {
-    private final SplitInstallManager mSplitManager;
     private final Logger mLogger;
 
     public SplitCompatEngineFacade() {
-        this(SplitInstallManagerFactory.create(ContextUtils.getApplicationContext()),
-                new PlayCoreLogger());
+        this(new PlayCoreLogger());
     }
 
-    public SplitCompatEngineFacade(SplitInstallManager manager, Logger umaLogger) {
-        mSplitManager = manager;
+    public SplitCompatEngineFacade(Logger umaLogger) {
         mLogger = umaLogger;
     }
 
@@ -38,25 +35,14 @@ class SplitCompatEngineFacade {
         return mLogger;
     }
 
-    public SplitInstallManager getSplitManager() {
-        return mSplitManager;
-    }
-
     public void installActivity(Activity activity) {
-        // Note that SplitCompat (install) needs to be called on the Application Context prior
-        // to calling this method - this is guaranteed by the behavior of SplitCompatEngine.
-        SplitCompat.installActivity(activity);
     }
 
     public void notifyObservers() {
-        ModuleUtil.notifyModuleInstalled();
-    }
-
-    public SplitInstallRequest createSplitInstallRequest(String moduleName) {
-        return SplitInstallRequest.newBuilder().addModule(moduleName).build();
+        //ModuleUtil.notifyModuleInstalled();
     }
 
     public void updateCrashKeys() {
-        ModuleUtil.updateCrashKeys();
+        //ModuleUtil.updateCrashKeys();
     }
 }
