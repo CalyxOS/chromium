bec4d6492979a6e7ca2a5fa1a858b4df64a216fc
csagan5 <32685696+csagan5@users.noreply.github.com>
2019-12-15 19:40:37 +0100

    Use dummy DFM installer
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I6336b599175f31e7dd750d90d3b6f882e28a421c

--- components/module_installer/android/java/src/org/chromium/components/module_installer/util/SplitCompatInitializer.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/util/SplitCompatInitializer.java
@@ -4,8 +4,6 @@
 
 package org.chromium.components.module_installer.util;
 
-import com.google.android.play.core.splitcompat.SplitCompat;
-
 import org.chromium.base.ContextUtils;
 import org.chromium.base.StrictModeContext;
 import org.chromium.base.ThreadUtils;
@@ -23,10 +21,6 @@ class SplitCompatInitializer {
             return;
         }
 
-        // SplitCompat.install may copy modules into Chrome's internal folder or clean them up.
-        try (StrictModeContext ignored = StrictModeContext.allowDiskWrites()) {
-            SplitCompat.install(ContextUtils.getApplicationContext());
-        }
         sIsInitialized = true;
     }
 
