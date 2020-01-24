ef2632a966385b89d49b9d8f047bd4ea1550af6d
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-01-24 19:17:22 +0100

    Disable smart selection by default
    Allow web search in incognito mode; Smart Selection still
    disabled in incognito as per upstream.
    
    Partial revert of da1d809c003749846cb4ade8c11b6d038e44416b to restore
    the ChromeSmartSelection feature flag.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: If123bca2e8ae02c746c3bf12f8287b8080c12ff7

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -220,6 +220,7 @@ public abstract class ChromeFeatureList {
     public static final String CCT_PAGE_INSIGHTS_HUB_BETTER_SCROLL =
             "CCTPageInsightsHubBetterScroll";
 
+    public static final String CHROME_SMART_SELECTION = "ChromeSmartSelection";
     public static final String CCT_GOOGLE_BOTTOM_BAR = "CCTGoogleBottomBar";
     public static final String CCT_REPORT_PARALLEL_REQUEST_STATUS =
             "CCTReportParallelRequestStatus";
