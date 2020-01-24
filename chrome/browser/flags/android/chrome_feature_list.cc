be0fcdef258d0b35e181db3cb284d3730d6b7bd6
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-01-24 19:17:22 +0100

    Disable smart selection by default
    Allow web search in incognito mode; Smart Selection still
    disabled in incognito as per upstream.
    
    Partial revert of da1d809c003749846cb4ade8c11b6d038e44416b to restore
    the ChromeSmartSelection feature flag.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -192,6 +192,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTBrandTransparency,
     &kCCTClientDataHeader,
     &kCCTIncognito,
+    &kChromeSmartSelection,
     &kCCTIncognitoAvailableToThirdParty,
     &kCCTNewDownloadTab,
     &kCCTPackageNameRecording,
@@ -971,6 +972,10 @@ BASE_FEATURE(kTabStripImprovements,
 
 BASE_FEATURE(kDiscoverFeedMultiColumn,
              "DiscoverFeedMultiColumn",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kChromeSmartSelection,
+             "ChromeSmartSelection",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTabStripRedesign,
