32934ec27eee1dd0b698a938d7889c752ddce279
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-01-24 19:17:22 +0100

    Disable smart selection by default
    Allow web search in incognito mode; Smart Selection still
    disabled in incognito as per upstream.
    
    Partial revert of da1d809c003749846cb4ade8c11b6d038e44416b to restore
    the ChromeSmartSelection feature flag.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: If123bca2e8ae02c746c3bf12f8287b8080c12ff7

--- content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
@@ -1089,7 +1089,6 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
     public boolean canWebSearch() {
         return hasSelection()
                 && !isFocusedNodeEditable()
-                && !isIncognito()
                 && isSelectActionModeAllowed(MENU_ITEM_WEB_SEARCH);
     }
 
