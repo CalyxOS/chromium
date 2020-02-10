9a0ef9ff6a80de3f524dfc712388fe2e7967d655
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-02-10 23:13:13 +0100

    Guard for user-agent reduction
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I9be0a2fe34dbef6349493265c1fac4d11d4a2890

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1449,8 +1449,8 @@ BASE_FEATURE(kReducedReferrerGranularity,
 const base::FeatureParam<std::string> kUserAgentFrozenBuildVersion{
     &kReduceUserAgentMinorVersion, "build_version", "0"};
 
-const base::FeatureParam<bool> kAllExceptLegacyWindowsPlatform = {
-    &kReduceUserAgentPlatformOsCpu, "all_except_legacy_windows_platform", true};
+const base::FeatureParam<bool> kAllExceptLegacyWindowsPlatform = {                 // must be enabled
+    &kReduceUserAgentPlatformOsCpu, "all_except_legacy_windows_platform", true};   // in Bromite
 const base::FeatureParam<bool> kLegacyWindowsPlatform = {
     &kReduceUserAgentPlatformOsCpu, "legacy_windows_platform", true};
 
