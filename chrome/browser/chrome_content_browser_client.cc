2adc62960e2a8e2f0afa2af81b50aa0489a3fbde
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-02-10 23:13:13 +0100

    Guard for user-agent reduction
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I9be0a2fe34dbef6349493265c1fac4d11d4a2890

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1605,7 +1605,7 @@ void ChromeContentBrowserClient::RegisterProfilePrefs(
   registry->RegisterIntegerPref(
       prefs::kUserAgentReduction,
       static_cast<int>(
-          embedder_support::UserAgentReductionEnterprisePolicyState::kDefault));
+          embedder_support::UserAgentReductionEnterprisePolicyState::kForceEnabled));
   registry->RegisterBooleanPref(prefs::kOriginAgentClusterDefaultEnabled, true);
 
   registry->RegisterBooleanPref(
