c4f57d886a0265745d514c2cacaf3f35b7d7e261
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-05-02 00:25:25 +0200

    Modify default preferences
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I73b8c04221d5b4bc2bbf7958dd85ebe3c17c2aa8

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -76,6 +76,7 @@
 #include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/autofill/core/common/autofill_switches.h"
 #include "components/autofill/core/common/autofill_util.h"
+#include "components/bookmarks/browser/features.h"
 #include "components/browser_sync/browser_sync_switches.h"
 #include "components/browser_ui/site_settings/android/features.h"
 #include "components/browsing_data/core/features.h"
@@ -6189,6 +6190,11 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          reading_list::switches::kReadLaterReminderNotification)},
 #endif
+
+    {"apps-shortcut-default-off",
+     flag_descriptions::kAppsShortcutDefaultOffName,
+     flag_descriptions::kAppsShortcutDefaultOffDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(bookmarks::features::kAppsShortcutDefaultOff)},
 
     {"tab-groups-new-badge-promo",
      flag_descriptions::kTabGroupsNewBadgePromoName,
