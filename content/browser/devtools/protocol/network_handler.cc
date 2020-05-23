0baa377d1d02e177442bd530ec7eca5d804b7f2f
Zoraver Kang <Zoraver@users.noreply.github.com>
2020-05-22 22:43:27 -0400

    Allow building without enable_reporting
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I80bdf53df8e93dc0dd36e516b3bdc2e215b4481b

--- content/browser/devtools/protocol/network_handler.cc
+++ content/browser/devtools/protocol/network_handler.cc
@@ -1473,6 +1473,7 @@ String BuildReportStatus(const net::ReportingReport::Status status) {
   }
 }
 
+#if BUILDFLAG(ENABLE_REPORTING)
 std::vector<GURL> ComputeReportingURLs(RenderFrameHostImpl* frame_host) {
   std::vector<GURL> urls;
   frame_host->ForEachRenderFrameHostImplWithAction(
@@ -1486,6 +1487,7 @@ std::vector<GURL> ComputeReportingURLs(RenderFrameHostImpl* frame_host) {
       });
   return urls;
 }
+#endif  // BUILDFLAG(ENABLE_REPORTING)
 
 }  // namespace
 
