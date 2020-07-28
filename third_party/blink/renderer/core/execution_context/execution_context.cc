12deb635898095a64c90b9b5dd6417ec37af5045
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-07-28 12:28:58 +0200

    Block gateway attacks via websockets
    This approach is not comprehensive, see also:
    * https://bugs.chromium.org/p/chromium/issues/detail?id=590714
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Iaa6823daecc7fbe6f3592b03674b20d99387ad6b

--- third_party/blink/renderer/core/execution_context/execution_context.cc
+++ third_party/blink/renderer/core/execution_context/execution_context.cc
@@ -721,4 +721,20 @@ void ExecutionContext::WriteIntoTrace(
   proto->set_world_type(GetWorldType(*this));
 }
 
+String ExecutionContext::addressSpaceForBindings() const {
+  switch (AddressSpace()) {
+    case network::mojom::IPAddressSpace::kPublic:
+    case network::mojom::IPAddressSpace::kUnknown:
+      return "public";
+
+    case network::mojom::IPAddressSpace::kLoopback:
+      return "loopback";
+
+    case network::mojom::IPAddressSpace::kLocal:
+      return "local";
+  }
+  NOTREACHED();
+  return "public";
+}
+
 }  // namespace blink
