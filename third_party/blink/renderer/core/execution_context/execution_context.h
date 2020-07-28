6a0cf392bc4b301d0f4c44aa5aa5aa8119ee911e
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-07-28 12:28:58 +0200

    Block gateway attacks via websockets
    This approach is not comprehensive, see also:
    * https://bugs.chromium.org/p/chromium/issues/detail?id=590714
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Iaa6823daecc7fbe6f3592b03674b20d99387ad6b

--- third_party/blink/renderer/core/execution_context/execution_context.h
+++ third_party/blink/renderer/core/execution_context/execution_context.h
@@ -390,6 +390,7 @@ class CORE_EXPORT ExecutionContext : public Supplementable<ExecutionContext>,
   void SetAddressSpace(network::mojom::blink::IPAddressSpace ip_address_space);
 
   HeapObserverSet<ContextLifecycleObserver>& ContextLifecycleObserverSet();
+  String addressSpaceForBindings() const;
   unsigned ContextLifecycleStateObserverCountForTesting() const;
 
   // Implementation of WindowOrWorkerGlobalScope.crossOriginIsolated.
