447e71f208bfb20961bdc6a704aa1c6dfa67c462
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-07-28 12:28:58 +0200

    Block gateway attacks via websockets
    This approach is not comprehensive, see also:
    * https://bugs.chromium.org/p/chromium/issues/detail?id=590714
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Iaa6823daecc7fbe6f3592b03674b20d99387ad6b

--- third_party/blink/renderer/modules/websockets/websocket_common.h
+++ third_party/blink/renderer/modules/websockets/websocket_common.h
@@ -11,6 +11,8 @@
 
 #include <optional>
 
+#include "services/network/public/mojom/ip_address_space.mojom.h"
+#include "third_party/blink/renderer/platform/network/network_utils.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -57,6 +59,8 @@ class MODULES_EXPORT WebSocketCommon {
   State GetState() const { return state_; }
   void SetState(State state) { state_ = state; }
   const KURL& Url() const { return url_; }
+
+  bool ShouldBlockGateWayAttacks(network::mojom::IPAddressSpace requestor_space, const KURL& url) const;
 
   // The following methods are public for testing.
 
