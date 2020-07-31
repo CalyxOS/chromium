16cd01ed2cd52591353daf81d1a1e62771eb26d5
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-07-31 08:27:26 +0200

    Enable prefetch-privacy-changes by default
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I7c26acb46381297e108d29b65a498568a62436e5

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1308,8 +1308,8 @@ BASE_FEATURE(kPrefetchFontLookupTables,
 // Prefetch request properties are updated to be privacy-preserving. See
 // crbug.com/988956.
 BASE_FEATURE(kPrefetchPrivacyChanges,
-             "PrefetchPrivacyChanges",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "PrefetchPrivacyChanges",            // must be enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);   // in Bromite
 
 BASE_FEATURE(kPrerender2InNewTab,
              "Prerender2InNewTab",
