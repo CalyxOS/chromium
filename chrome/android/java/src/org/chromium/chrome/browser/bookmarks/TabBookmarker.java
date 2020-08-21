66fb8e53064a0ddac38c3a3d8cdeb73713afbd36
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-08-21 22:39:23 +0200

    Remove blocklisted URLs upon bookmark creation
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I14caadcabe7c7127f5cac20411de7126f80f24cf

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/TabBookmarker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/TabBookmarker.java
@@ -19,6 +19,12 @@ import org.chromium.components.bookmarks.BookmarkItem;
 import org.chromium.components.bookmarks.BookmarkType;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.profiles.ProfileManager;
+import org.chromium.chrome.browser.suggestions.SuggestionsDependencyFactory;
+import org.chromium.chrome.browser.suggestions.mostvisited.MostVisitedSites;
+import org.chromium.url.GURL;
+
 /**
  * Helper class for managing the UI flow for bookmarking the active tab and kicking off the backend.
  * Shows a snackbar if a new bookmark was added. If the bookmark already exists, kicks off edit
@@ -30,6 +36,7 @@ public class TabBookmarker {
     private final Supplier<BottomSheetController> mBottomSheetControllerSupplier;
     private final Supplier<SnackbarManager> mSnackbarManagerSupplier;
     private final boolean mIsCustomTab;
+    private MostVisitedSites mMostVisitedSites;
 
     /**
      * Constructor.
@@ -104,6 +111,13 @@ public class TabBookmarker {
         if (tabToBookmark == null || tabToBookmark.isFrozen()) {
             return;
         }
+
+	    // remove blocklisted URL from most visited sites
+	    if (mMostVisitedSites == null) {
+            mMostVisitedSites =
+                SuggestionsDependencyFactory.getInstance().createMostVisitedSites(ProfileManager.getLastUsedRegularProfile());
+        }
+        mMostVisitedSites.removeBlocklistedUrl(tabToBookmark.getOriginalUrl());
 
         // Defense in depth against the UI being erroneously enabled.
         final BookmarkModel bookmarkModel = mBookmarkModelSupplier.get();
