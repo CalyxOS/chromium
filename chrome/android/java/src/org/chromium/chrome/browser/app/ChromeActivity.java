be5289d36e3aa4f5f1aa0cafed4a36d918f243f0
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-08-21 22:39:23 +0200

    Remove blocklisted URLs upon bookmark creation
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -296,7 +296,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
     private TabContentManager mTabContentManager;
 
     private UmaSessionStats mUmaSessionStats;
-    private ContextReporter mContextReporter;
 
     private boolean mPartnerBrowserRefreshNeeded;
 
