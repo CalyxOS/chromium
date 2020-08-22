2a64777e654424a6db528617b5bd402e70f01468
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-08-22 12:46:20 +0200

    Disable text fragments by default
    Revert "[Text Fragment] Unflag fragment directive removal."
    
    Original License: BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I145be3cf3de31f7c92fc97c74fede22addf8621a

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -744,6 +744,9 @@ extern const char kReduceHorizontalFlingVelocityDescription[];
 
 extern const char kRetailCouponsName[];
 extern const char kRetailCouponsDescription[];
+extern const char kEnableTextFragmentAnchorName[];
+extern const char kEnableTextFragmentAnchorDescription[];
+
 
 extern const char kDropInputEventsBeforeFirstPaintName[];
 extern const char kDropInputEventsBeforeFirstPaintDescription[];
--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -290,6 +290,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
      raw_ref(autofill::features::kAutofillSharedAutofill)},
     {wf::EnableTouchDragAndContextMenu,
      raw_ref(features::kTouchDragAndContextMenu)},
+    {wf::EnableTextFragmentIdentifiers,
+     raw_ref(blink::features::kTextFragmentAnchor)},
     {wf::EnableUserActivationSameOriginVisibility,
      raw_ref(features::kUserActivationSameOriginVisibility)},
     {wf::EnableVideoPlaybackQuality, raw_ref(features::kVideoPlaybackQuality)},
