47069c006463d6b5cabbfeb503f62edcef2b25bf
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-08-22 12:46:20 +0200

    Disable text fragments by default
    Revert "[Text Fragment] Unflag fragment directive removal."
    
    Original License: BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I145be3cf3de31f7c92fc97c74fede22addf8621a

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -5917,6 +5917,11 @@ const FeatureEntry kFeatureEntries[] = {
          "")},
 #endif
 
+    {"enable-text-fragment-anchor",
+     flag_descriptions::kEnableTextFragmentAnchorName,
+     flag_descriptions::kEnableTextFragmentAnchorDescription, kOsAll,
+     FEATURE_VALUE_TYPE(blink::features::kTextFragmentAnchor)},
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     {"enable-web-authentication-chromeos-authenticator",
      flag_descriptions::kEnableWebAuthenticationChromeOSAuthenticatorName,
