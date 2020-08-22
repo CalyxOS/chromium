5428b493a82c131845ad6df519744de9b6455865
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-08-22 12:46:20 +0200

    Disable text fragments by default
    Revert "[Text Fragment] Unflag fragment directive removal."
    
    Original License: BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I145be3cf3de31f7c92fc97c74fede22addf8621a

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -767,6 +767,11 @@ BASE_FEATURE(kExtendScriptResourceLifetime,
 // trials.
 BASE_FEATURE(kFencedFrames, "FencedFrames", base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enable text snippets in URL fragments. https://crbug.com/919204.
+BASE_FEATURE(kTextFragmentAnchor,
+             "TextFragmentAnchor",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // File handling icons. https://crbug.com/1218213
 BASE_FEATURE(kFileHandlingIcons,
              "FileHandlingIcons",
