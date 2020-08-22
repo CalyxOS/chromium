cd82305d48ca680c1e263504ec0f17a63c33132c
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-08-22 12:46:20 +0200

    Disable text fragments by default
    Revert "[Text Fragment] Unflag fragment directive removal."
    
    Original License: BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I145be3cf3de31f7c92fc97c74fede22addf8621a

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -296,6 +296,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
 #endif
           {wf::EnableTouchDragAndContextMenu,
            raw_ref(features::kTouchDragAndContextMenu)},
+          {wf::EnableTextFragmentIdentifiers,
+           raw_ref(blink::features::kTextFragmentAnchor)},
           {wf::EnableUserActivationSameOriginVisibility,
            raw_ref(features::kUserActivationSameOriginVisibility)},
           {wf::EnableVideoPlaybackQuality,
