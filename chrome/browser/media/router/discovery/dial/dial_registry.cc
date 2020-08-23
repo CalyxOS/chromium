82cedc54c9650ae146b00989c8a5a84b9491d513
Blaise <BlaiseD@GMail.com>
2020-08-23 14:32:55 -0500

    Disable the DIAL repeating discovery
    This causes unnecessary SSDP network spam
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/browser/media/router/discovery/dial/dial_registry.cc
+++ chrome/browser/media/router/discovery/dial/dial_registry.cc
@@ -154,10 +154,6 @@ void DialRegistry::StartPeriodicDiscovery() {
     return;
 
   dial_ = CreateDialService();
-  DoDiscovery();
-  repeating_timer_ = std::make_unique<base::RepeatingTimer>();
-  repeating_timer_->Start(FROM_HERE, refresh_interval_delta_, this,
-                          &DialRegistry::DoDiscovery);
   // Always send the current device list with the next discovery request.  This
   // may not be necessary, but is done to match previous behavior.
   ++registry_generation_;
@@ -174,8 +170,6 @@ void DialRegistry::StopPeriodicDiscovery() {
   if (!dial_)
     return;
 
-  repeating_timer_->Stop();
-  repeating_timer_.reset();
   ClearDialService();
 }
 
