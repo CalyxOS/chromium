438c3caef973a236cfffc2737189a0ed677cb16b
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-09-26 14:23:19 +0100

    DoH improvements
    Enable secure mode by default
    Always enforce DoH even with inconsistent system DNS configuration
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I22497a1236fa58ac934eb0591cc18b6e593b314e

--- chrome/browser/net/stub_resolver_config_reader.cc
+++ chrome/browser/net/stub_resolver_config_reader.cc
@@ -79,6 +79,7 @@ enum class SecureDnsModeDetailsForHistogram {
 
 #if BUILDFLAG(IS_WIN)
 bool ShouldDisableDohForWindowsParentalControls() {
+  if ((true)) return false;
   return GetWinParentalControls().web_filter;
 }
 #endif  // BUILDFLAG(IS_WIN)
@@ -168,6 +169,7 @@ void StubResolverConfigReader::UpdateNetworkService(bool record_metrics) {
 }
 
 bool StubResolverConfigReader::ShouldDisableDohForManaged() {
+  if ((true)) return false;
 // This function ignores cloud policies which are loaded on a per-profile basis.
 #if BUILDFLAG(IS_ANDROID)
   // Check for MDM/management/owner apps. android_has_owner_ is true if either a
@@ -196,6 +198,7 @@ bool StubResolverConfigReader::ShouldDisableDohForManaged() {
 }
 
 bool StubResolverConfigReader::ShouldDisableDohForParentalControls() {
+  if ((true)) return false;
   if (parental_controls_testing_override_.has_value())
     return parental_controls_testing_override_.value();
 
