ff384d57ffe0e4a0c8be00a28e10f5a3e301f9cc
uazo <uazo@users.noreply.github.com>
2020-09-30 07:40:01 +0000

    Use BoringSSL for RandBytes
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: Ica0f748240bc4e2ba612b84fc4f1fe8e962b03a8

--- base/rand_util_win.cc
+++ base/rand_util_win.cc
@@ -40,7 +40,7 @@ std::atomic<bool> g_use_boringssl;
 
 BASE_FEATURE(kUseBoringSSLForRandBytes,
              "UseBoringSSLForRandBytes",
-             FEATURE_DISABLED_BY_DEFAULT);
+             FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace
 
