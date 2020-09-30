10fa8f35fb4d0cdc5004f6b0fa24ab4b1902f667
uazo <uazo@users.noreply.github.com>
2020-09-30 07:40:01 +0000

    Timezone customization
    Allow specifying a custom timezone, or using a random one.
    
    See also: https://github.com/bromite/bromite/wiki/TimezoneOverride
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: Content-settings-infrastructure.patch
    
    Change-Id: I8175bb9412c477b8b341dd519b89f5e4406176f4

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePreferenceBridge.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePreferenceBridge.java
@@ -416,6 +416,14 @@ public class WebsitePreferenceBridge {
         return WebsitePreferenceBridgeJni.get().toHostOnlyPattern(pattern);
     }
 
+    public static String getCustomTimezone(BrowserContextHandle browserContextHandle) {
+        return WebsitePreferenceBridgeJni.get().getCustomTimezone(browserContextHandle);
+    }
+
+    public static void setCustomTimezone(BrowserContextHandle browserContextHandle, String custom_timezone) {
+        WebsitePreferenceBridgeJni.get().setCustomTimezone(browserContextHandle, custom_timezone);
+    }
+
     @NativeMethods
     public interface Natives {
         boolean isNotificationEmbargoedForOrigin(
@@ -485,5 +493,7 @@ public class WebsitePreferenceBridge {
         boolean getLocationAllowedByPolicy(BrowserContextHandle browserContextHandle);
         String toDomainWildcardPattern(String pattern);
         String toHostOnlyPattern(String pattern);
+        String getCustomTimezone(BrowserContextHandle browserContextHandle);
+        void setCustomTimezone(BrowserContextHandle browserContextHandle, String custom_timezone);
     }
 }
