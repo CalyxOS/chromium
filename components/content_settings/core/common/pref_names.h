050e3f3bc609798c8d34d300d95413668a9d063e
uazo <uazo@users.noreply.github.com>
2020-09-30 07:40:01 +0000

    Timezone customization
    Allow specifying a custom timezone, or using a random one.
    
    See also: https://github.com/bromite/bromite/wiki/TimezoneOverride
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: Content-settings-infrastructure.patch
    
    Change-Id: I8175bb9412c477b8b341dd519b89f5e4406176f4

--- components/content_settings/core/common/pref_names.h
+++ components/content_settings/core/common/pref_names.h
@@ -102,6 +102,8 @@ extern const char kDesktopSiteDisplaySettingEnabled[];
 extern const char kDesktopSiteWindowSettingEnabled[];
 #endif
 
+extern const char kContentSettingsCustomTimezone[];
+
 }  // namespace prefs
 
 #endif  // COMPONENTS_CONTENT_SETTINGS_CORE_COMMON_PREF_NAMES_H_
