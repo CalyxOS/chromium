cbea94d50c3379bf727eb753221964d15f99eaa7
uazo <uazo@users.noreply.github.com>
2020-09-30 07:40:01 +0000

    Timezone customization
    Allow specifying a custom timezone, or using a random one.
    
    See also: https://github.com/bromite/bromite/wiki/TimezoneOverride
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I8175bb9412c477b8b341dd519b89f5e4406176f4

--- components/content_settings/core/browser/content_settings_pref_provider.cc
+++ components/content_settings/core/browser/content_settings_pref_provider.cc
@@ -88,6 +88,8 @@ void PrefProvider::RegisterProfilePrefs(
                                      info->GetPrefRegistrationFlags());
   }
 
+  registry->RegisterStringPref(prefs::kContentSettingsCustomTimezone, std::string());
+
   // Obsolete prefs ----------------------------------------------------------
 
   // These prefs have been removed, but need to be registered so they can
@@ -164,6 +166,10 @@ PrefProvider::PrefProvider(PrefService* prefs,
     event_args->set_number_of_exceptions(
         num_exceptions);  // PrefProvider::PrefProvider.
   });
+
+  custom_timezone_ =
+    prefs_->GetString(
+          prefs::kContentSettingsCustomTimezone);
 }
 
 PrefProvider::~PrefProvider() {
@@ -346,6 +352,16 @@ void PrefProvider::DiscardOrMigrateObsoletePreferences() {
 
 void PrefProvider::SetClockForTesting(base::Clock* clock) {
   clock_ = clock;
+}
+
+void PrefProvider::GetPrefTimezoneOverrideValue(std::string& timezone) const {
+  timezone = custom_timezone_;
+}
+
+void PrefProvider::SetPrefTimezoneOverrideValue(const std::string& timezone) {
+  prefs_->SetString(
+        prefs::kContentSettingsCustomTimezone, timezone);
+  custom_timezone_ = timezone;
 }
 
 }  // namespace content_settings
