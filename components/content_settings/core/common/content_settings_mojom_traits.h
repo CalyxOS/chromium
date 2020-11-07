d644fa111f182f547fd5cd6868f6e3bde0254dd1
uazo <uazo@users.noreply.github.com>
2020-11-07 21:59:18 +0000

    Revert the removal of an option to block autoplay
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I0800ab1fc452d0a57932b7e0653a353c3fdf464c

--- components/content_settings/core/common/content_settings_mojom_traits.h
+++ components/content_settings/core/common/content_settings_mojom_traits.h
@@ -179,6 +179,11 @@ struct StructTraits<
     return r.script_rules;
   }
 
+  static const std::vector<ContentSettingPatternSource>& autoplay_rules(
+      const RendererContentSettingRules& r) {
+    return r.autoplay_rules;
+  }
+
   static const std::vector<ContentSettingPatternSource>& popup_redirect_rules(
       const RendererContentSettingRules& r) {
     return r.popup_redirect_rules;
