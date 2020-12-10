7fbd293f66ea36e08dab24378c1671922ad23f7c
csagan5 <32685696+csagan5@users.noreply.github.com>
2020-12-10 21:52:35 +0100

    Enable network isolation features
    Enable SplitCacheByNetworkIsolationKey, kSplitHostCacheByNetworkIsolationKey,
    kPartitionConnectionsByNetworkIsolationKey, kPartitionHttpServerPropertiesByNetworkIsolationKey,
    kPartitionSSLSessionsByNetworkIsolationKey, PartitionExpectCTStateByNetworkIsolationKey,
    PartitionNelAndReportingByNetworkIsolationKey
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Iae5bf07bc19de8f37435812c0cd860d2584ad9c2

--- net/base/features.cc
+++ net/base/features.cc
@@ -104,37 +104,37 @@ BASE_FEATURE(kSplitCacheByIncludeCredentials,
              "SplitCacheByIncludeCredentials",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kSplitCacheByNetworkIsolationKey,
-             "SplitCacheByNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kSplitCacheByNetworkIsolationKey,             // must be enabled
+             "SplitCacheByNetworkIsolationKey",            // enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);            // in Bromite
 
-BASE_FEATURE(kSplitCodeCacheByNetworkIsolationKey,
-             "SplitCodeCacheByNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kSplitCodeCacheByNetworkIsolationKey,         // must be enabled
+             "SplitCodeCacheByNetworkIsolationKey",        // enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);            // in Bromite
 
-BASE_FEATURE(kSplitHostCacheByNetworkIsolationKey,
-             "SplitHostCacheByNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kSplitHostCacheByNetworkIsolationKey,         // must be
+             "SplitHostCacheByNetworkIsolationKey",        // enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);            // in Bromite
 
-BASE_FEATURE(kPartitionConnectionsByNetworkIsolationKey,
-             "PartitionConnectionsByNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPartitionConnectionsByNetworkIsolationKey,   // must be
+             "PartitionConnectionsByNetworkIsolationKey",  // enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);            // in Bromite
 
-BASE_FEATURE(kPartitionHttpServerPropertiesByNetworkIsolationKey,
-             "PartitionHttpServerPropertiesByNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPartitionHttpServerPropertiesByNetworkIsolationKey,     // must be
+             "PartitionHttpServerPropertiesByNetworkIsolationKey",    // enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);                       // in Bromite
 
-BASE_FEATURE(kPartitionSSLSessionsByNetworkIsolationKey,
-             "PartitionSSLSessionsByNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPartitionSSLSessionsByNetworkIsolationKey,              // must be
+             "PartitionSSLSessionsByNetworkIsolationKey",             // enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);                       // in Bromite
 
-BASE_FEATURE(kPartitionNelAndReportingByNetworkIsolationKey,
-             "PartitionNelAndReportingByNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPartitionNelAndReportingByNetworkIsolationKey,          // must be
+             "PartitionNelAndReportingByNetworkIsolationKey",         // enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);                       // in Bromite
 
-BASE_FEATURE(kEnableCrossSiteFlagNetworkIsolationKey,
-             "EnableCrossSiteFlagNetworkIsolationKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableCrossSiteFlagNetworkIsolationKey,             // must be
+             "EnableCrossSiteFlagNetworkIsolationKey",            // disabled
+             base::FEATURE_DISABLED_BY_DEFAULT);                  // in Bromite
 BASE_FEATURE(kEnableFrameSiteSharedOpaqueNetworkIsolationKey,
              "EnableFrameSiteSharedOpaqueNetworkIsolationKey",
              base::FEATURE_DISABLED_BY_DEFAULT);
