292bbf47e5705fc65d0d61bc8cab37405e5c7712
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-01-26 00:21:51 +0100

    Ignore enterprise policies for secure DNS
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I54ab520f6990c2c8d82a32327c93c3359427deda

--- chrome/browser/enterprise/util/android/java/src/org/chromium/chrome/browser/enterprise/util/EnterpriseInfo.java
+++ chrome/browser/enterprise/util/android/java/src/org/chromium/chrome/browser/enterprise/util/EnterpriseInfo.java
@@ -96,13 +96,7 @@ public abstract class EnterpriseInfo {
         Callback<OwnedState> callback =
                 (result) -> {
                     Log.i(TAG, "#getManagedStateForNative() " + result);
-                    if (result == null) {
-                        // Unable to determine the owned state, assume it's not owned.
-                        EnterpriseInfoJni.get().updateNativeOwnedState(false, false);
-                    } else {
-                        EnterpriseInfoJni.get()
-                                .updateNativeOwnedState(result.mDeviceOwned, result.mProfileOwned);
-                    }
+                    EnterpriseInfoJni.get().updateNativeOwnedState(false, false);
                 };
 
         EnterpriseInfo.getInstance().getDeviceEnterpriseInfo(callback);
