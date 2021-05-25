98e505613c73bef123fc34b8feb3207f92a25ee1
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-05-25 19:46:14 +0200

    Add flag to disable external intent requests
    Adds a new flag that allows to control the switch with same name;
    when flag is disabled no external intent will ever be allowed,
    for any URL except for the tel: schema.
    This also reverts commit b710cefb53b558a8bcd884f6baf0229ba4225721 and
    enables IntentBlockExternalFormRedirectsNoGesture.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: If8f9d484b7d3414fc7a628107b779a39f3c7d755

---
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/cromite/sAllowExternalIntentRequests.java
@@ -0,0 +1,33 @@
+/*
+    This file is part of Cromite.
+
+    Cromite is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cromite is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cromite. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package org.chromium.chrome.browser.flags.cromite;
+
+import org.chromium.base.cached_flags.CachedFlag;
+import org.chromium.chrome.browser.flags.ChromeFeatureMap;
+
+public class sAllowExternalIntentRequests {
+    private static final CachedFlag sInstance =
+        new CachedFlag(ChromeFeatureMap.getInstance(),
+            "AllowExternalIntentRequests", false);
+
+    private sAllowExternalIntentRequests() {}
+
+    public static CachedFlag getInstance() {
+        return sInstance;
+    }
+}
