853a9f2e2a641a5062880194036cf8d679dc6674
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-05-25 19:46:14 +0200

    Add flag to disable external intent requests
    Adds a new flag that allows to control the switch with same name;
    when flag is disabled no external intent will ever be allowed, for any URL.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: If8f9d484b7d3414fc7a628107b779a39f3c7d755

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2225,6 +2225,9 @@ extern const char kQueryTilesNTPDescription[];
 extern const char kQueryTilesOnStartName[];
 extern const char kQueryTilesOnStartDescription[];
 extern const char kQueryTilesSingleTierName[];
+extern const char kDisableExternalIntentRequestsName[];
+extern const char kDisableExternalIntentRequestsDescription[];
+
 extern const char kQueryTilesSingleTierDescription[];
 extern const char kQueryTilesEnableQueryEditingName[];
 extern const char kQueryTilesEnableQueryEditingDescription[];
