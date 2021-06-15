96d74ef2119df6fc71664fd8192a02a80c69a49c
uazo <uazo@users.noreply.github.com>
2021-06-15 11:49:43 +0000

    Logcat crash reports UI
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Id2db21e92bca486876565ab3223f9de95e4cd763

--- chrome/browser/ui/webui/crashes_ui.cc
+++ chrome/browser/ui/webui/crashes_ui.cc
@@ -42,6 +42,19 @@
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
+#include "base/logging.h"
+#include "base/debug/dump_without_crashing.h"
+#include "base/files/file_util.h"
+#include "base/files/file_enumerator.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/path_utils.h"
+#endif
+#include "net/base/filename_util.h"
+#include "third_party/zlib/google/zip.h"
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chromeos/ash/components/dbus/debug_daemon/debug_daemon_client.h"
 #endif
@@ -77,6 +90,10 @@ void CreateAndAddCrashesUIHTMLSource(Profile* profile) {
   source->SetDefaultResource(IDR_CRASH_CRASHES_HTML);
 }
 
+constexpr base::TaskTraits kLoadingTaskTraits = {
+    base::MayBlock(), base::TaskPriority::USER_BLOCKING,
+    base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN};
+
 ////////////////////////////////////////////////////////////////////////////////
 //
 // CrashesDOMHandler
@@ -86,7 +103,7 @@ void CreateAndAddCrashesUIHTMLSource(Profile* profile) {
 // The handler for Javascript messages for the chrome://crashes/ page.
 class CrashesDOMHandler : public WebUIMessageHandler {
  public:
-  CrashesDOMHandler();
+  CrashesDOMHandler(content::WebContents* web_contents);
 
   CrashesDOMHandler(const CrashesDOMHandler&) = delete;
   CrashesDOMHandler& operator=(const CrashesDOMHandler&) = delete;
@@ -103,6 +120,8 @@ class CrashesDOMHandler : public WebUIMessageHandler {
   // Asynchronously fetches the list of crashes. Called from JS.
   void HandleRequestCrashes(const base::Value::List& args);
 
+  void RequestCrashesList();
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   // Asynchronously triggers crash uploading. Called from JS.
   void HandleRequestUploads(const base::Value::List& args);
@@ -114,14 +133,30 @@ class CrashesDOMHandler : public WebUIMessageHandler {
   // Asynchronously requests a user triggered upload. Called from JS.
   void HandleRequestSingleCrashUpload(const base::Value::List& args);
 
+  std::string RequestSingleUpload(const std::string& local_id) const;
+  void RequestSingleUploadCallback(const std::string& local_id, const std::string& filename);
+
+  // Asynchronously requests a user log extraction. Called from JS.
+  void HandleRequestNewExtraction(const base::Value::List& args);
+  void RequestNewExtraction();
+
+  // Requests remove all crash files. Called from JS.
+  void HandleRequestClearAll(const base::Value::List& args);
+  void ClearAll();
+
   scoped_refptr<UploadList> upload_list_;
   bool list_available_;
   bool first_load_;
+  raw_ptr<content::WebContents> web_contents_;
 };
 
-CrashesDOMHandler::CrashesDOMHandler()
-    : list_available_(false), first_load_(true) {
+CrashesDOMHandler::CrashesDOMHandler(content::WebContents* web_contents)
+    : list_available_(false), first_load_(true),
+      web_contents_(web_contents) {
   upload_list_ = CreateCrashUploadList();
+#if !BUILDFLAG(IS_ANDROID)
+  web_contents_ = nullptr;
+#endif
 }
 
 CrashesDOMHandler::~CrashesDOMHandler() {
@@ -147,6 +182,16 @@ void CrashesDOMHandler::RegisterMessages() {
       crash_reporter::kCrashesUIRequestSingleCrashUpload,
       base::BindRepeating(&CrashesDOMHandler::HandleRequestSingleCrashUpload,
                           base::Unretained(this)));
+
+  web_ui()->RegisterMessageCallback(
+      crash_reporter::kCrashesUIHandleClearAll,
+      base::BindRepeating(&CrashesDOMHandler::HandleRequestClearAll,
+                          base::Unretained(this)));
+
+  web_ui()->RegisterMessageCallback(
+      crash_reporter::kCrashesUIHandleRequestNewExtraction,
+      base::BindRepeating(&CrashesDOMHandler::HandleRequestNewExtraction,
+                          base::Unretained(this)));
 }
 
 void CrashesDOMHandler::OnJavascriptDisallowed() {
@@ -155,6 +200,10 @@ void CrashesDOMHandler::OnJavascriptDisallowed() {
 
 void CrashesDOMHandler::HandleRequestCrashes(const base::Value::List& args) {
   AllowJavascript();
+  RequestCrashesList();
+}
+
+void CrashesDOMHandler::RequestCrashesList() {
   if (first_load_) {
     first_load_ = false;
     if (list_available_)
@@ -186,8 +235,7 @@ void CrashesDOMHandler::OnUploadListAvailable() {
 }
 
 void CrashesDOMHandler::UpdateUI() {
-  bool crash_reporting_enabled =
-      ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled();
+  bool crash_reporting_enabled = true;
 
   bool system_crash_reporter = false;
 #if BUILDFLAG(IS_CHROMEOS)
@@ -235,14 +283,117 @@ void CrashesDOMHandler::UpdateUI() {
 
 void CrashesDOMHandler::HandleRequestSingleCrashUpload(
     const base::Value::List& args) {
-  // Only allow manual uploads if crash uploads aren’t disabled by policy.
-  if (!ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled() &&
-      IsMetricsReportingPolicyManaged()) {
-    return;
+  std::string local_id = args[0].GetString();
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, kLoadingTaskTraits,
+      base::BindOnce(&CrashesDOMHandler::RequestSingleUpload, base::Unretained(this), local_id),
+      base::BindOnce(&CrashesDOMHandler::RequestSingleUploadCallback, base::Unretained(this), local_id));
+}
+
+std::string CrashesDOMHandler::RequestSingleUpload(const std::string& local_id) const {
+#if BUILDFLAG(IS_ANDROID)
+  // get crash file path
+  std::string info_file_path = upload_list_->GetFilePathByLocalId(local_id);
+  if (info_file_path.empty()) {
+    LOG(ERROR) << "Crash report: file path is not set for " << local_id;
+    return std::string();
+  }
+  base::FilePath crash_file_path(info_file_path);
+
+  // get android crash report dir
+  base::FilePath cache_dir;
+  base::android::GetCacheDirectory(&cache_dir);
+  base::FilePath upload_log_path = cache_dir.Append("Crash Reports");
+
+  // crash reports can have multiple extensions (e.g. foo.dmp, foo.dmp.try1,
+  // foo.skipped.try0), remove it
+  base::FilePath zip_file_name = crash_file_path;
+  while (zip_file_name != zip_file_name.RemoveExtension())
+    zip_file_name = zip_file_name.RemoveExtension();
+
+  // make zip file name, like "ec708a7b-cb17-44e7-8dae-e32f6c45cb8c.zip"
+  zip_file_name = upload_log_path.Append(zip_file_name.BaseName())
+                            .AddExtensionASCII(".zip");
+  // since the download is always allowed, the generation takes place only
+  // at the first request, so if exists return it
+  if (base::PathExists(zip_file_name))
+    return zip_file_name.value();
+
+  // original code remove the file immediately after upload.
+  // we changed this behavior but it is still possible that the file no longer exists
+  // because in uploads.log it could be indicated but the file was deleted by self-cleaning
+  if (!base::PathExists(crash_file_path)) {
+    LOG(ERROR) << "Crash report: file " << crash_file_path
+               << " no more available";
+    return std::string();
   }
 
-  std::string local_id = args[0].GetString();
-  upload_list_->RequestSingleUploadAsync(local_id);
+  std::vector<base::FilePath> files_list;
+  files_list.push_back(crash_file_path.BaseName());
+
+  // open zip file
+  base::File zip_f(zip_file_name,
+                      base::File::FLAG_CREATE | base::File::FLAG_WRITE);
+  auto result = zip::ZipFiles(crash_file_path.DirName(), files_list, zip_f.GetPlatformFile());
+  zip_f.Close();
+  if (result) {
+    return zip_file_name.value();
+  }
+#endif
+  LOG(ERROR) << "Crash report: cannot create zip content";
+  return std::string();
+}
+
+void CrashesDOMHandler::RequestSingleUploadCallback(const std::string& local_id,
+                                                    const std::string& file_name) {
+#if BUILDFLAG(IS_ANDROID)
+  if (!file_name.empty()) {
+    upload_list_->RequestSingleUploadAsync(local_id);
+
+    base::FilePath file_path(file_name);
+    web_contents_->GetController().LoadURL(
+        net::FilePathToFileURL(file_path), {}, {}, {});
+  }
+#endif
+}
+
+void CrashesDOMHandler::HandleRequestNewExtraction(
+    const base::Value::List& args) {
+  base::ThreadPool::PostTask(
+      FROM_HERE, kLoadingTaskTraits,
+      base::BindOnce(&CrashesDOMHandler::RequestNewExtraction, base::Unretained(this)));
+}
+
+void CrashesDOMHandler::RequestNewExtraction() {
+  base::debug::DumpWithoutCrashing();
+  // ask java to get file from crashpad and to add logcat
+  upload_list_->RequestNewExtraction();
+}
+
+void CrashesDOMHandler::HandleRequestClearAll(
+    const base::Value::List& args) {
+  base::ThreadPool::PostTaskAndReply(
+      FROM_HERE, kLoadingTaskTraits,
+      base::BindOnce(&CrashesDOMHandler::ClearAll, base::Unretained(this)),
+      base::BindOnce(&CrashesDOMHandler::RequestCrashesList, base::Unretained(this)));
+}
+
+void CrashesDOMHandler::ClearAll() {
+#if BUILDFLAG(IS_ANDROID)
+  // get android crash report dir
+  base::FilePath cache_dir;
+  base::android::GetCacheDirectory(&cache_dir);
+  base::FilePath upload_log_path = cache_dir.Append("Crash Reports");
+
+  base::FileEnumerator dir_enum(
+    upload_log_path,
+    /*recursive=*/false, base::FileEnumerator::FILES);
+  base::FilePath full_name;
+  while (full_name = dir_enum.Next(), !full_name.empty()) {
+    // remove all files, don't care for result
+    base::DeleteFile(full_name);
+   }
+#endif
 }
 
 }  // namespace
@@ -254,7 +405,8 @@ void CrashesDOMHandler::HandleRequestSingleCrashUpload(
 ///////////////////////////////////////////////////////////////////////////////
 
 CrashesUI::CrashesUI(content::WebUI* web_ui) : WebUIController(web_ui) {
-  web_ui->AddMessageHandler(std::make_unique<CrashesDOMHandler>());
+  web_ui->AddMessageHandler(std::make_unique<CrashesDOMHandler>(
+      web_ui->GetWebContents()));
 
   // Set up the chrome://crashes/ source.
   CreateAndAddCrashesUIHTMLSource(Profile::FromWebUI(web_ui));
