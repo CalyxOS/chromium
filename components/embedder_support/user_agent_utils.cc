4ddfd953a3f358a6ea5c1d838f25c6016ea2a26e
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-07-22 23:10:52 +0200

    Client hints overrides
    Disable critical client hints and network early hints
    Use Google Chrome branding for client hints
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I85c134e85ede5ab1ad30643cb84b4c7f3aca08f7

--- components/embedder_support/user_agent_utils.cc
+++ components/embedder_support/user_agent_utils.cc
@@ -191,9 +191,7 @@ const blink::UserAgentBrandList GetUserAgentBrandList(
   bool parse_result = base::StringToInt(major_version, &major_version_number);
   DCHECK(parse_result);
   std::optional<std::string> brand;
-#if !BUILDFLAG(CHROMIUM_BRANDING)
-  brand = version_info::GetProductName();
-#endif
+  brand = "Google Chrome";
   std::optional<std::string> maybe_brand_override =
       base::GetFieldTrialParamValueByFeature(features::kGreaseUACH,
                                              "brand_override");
