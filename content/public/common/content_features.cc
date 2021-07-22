08e72b3786f49eb2e1a7d893a01ad8ea3a802a42
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-07-22 23:10:52 +0200

    Client hints overrides
    Disable critical client hints
    Use Google Chrome branding for client hints
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ia94dbd2e88983a466e5d3c758aba3de1c5259914

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -282,8 +282,8 @@ BASE_FEATURE(kCrashReporting,
 // Enables support for the `Critical-CH` response header.
 // https://github.com/WICG/client-hints-infrastructure/blob/master/reliability.md#critical-ch
 BASE_FEATURE(kCriticalClientHint,
-             "CriticalClientHint",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             "CriticalClientHint",                  // disabled by default
+             base::FEATURE_DISABLED_BY_DEFAULT);    // in Bromite
 
 // Enable debugging the issue crbug.com/1201355
 BASE_FEATURE(kDebugHistoryInterventionNoUserActivation,
