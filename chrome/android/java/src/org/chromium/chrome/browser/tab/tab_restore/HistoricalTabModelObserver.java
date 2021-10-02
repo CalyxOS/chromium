9da83a33793c79560bac78198f3bafabb0b63585
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-10-02 13:20:36 +0200

    Add an always-incognito mode
    Add a preference that causes all new tabs and all clicked links to launch in incognito.
    Make sure initial incognito status is correctly recognized.
    Enable incognito custom tabs and fix crashes for incognito/custom tab intents
    Use a native flag to correctly start new tabs on app startup
    
    Add history, recents, offlinepages and send to home screen support for always incognito.
    History, recent tabs and offline pages require the INCOGNITO_TAB_HISTORY_ENABLED
    flag turned on.
    IncognitoPlaceholder is also deactivated, both in the phone and tablet version.
    The relative tests are also present.
    
    based on the original work by Ryan Archer <ryan.bradley.archer@gmail.com>
    Major contributions by uazo.
    See also: https://github.com/bromite/bromite/pull/1427
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I9c35d6d686f8091e4dcd96c1906ff3f8cbed1a58

--- chrome/android/java/src/org/chromium/chrome/browser/tab/tab_restore/HistoricalTabModelObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/tab_restore/HistoricalTabModelObserver.java
@@ -22,6 +22,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+import org.chromium.chrome.browser.AlwaysIncognitoLinkInterceptor;
+
 /** A tab model observer for managing bulk closures. */
 public class HistoricalTabModelObserver implements TabModelObserver {
     private final TabGroupModelFilter mTabGroupModelFilter;
@@ -53,9 +55,10 @@ public class HistoricalTabModelObserver implements TabModelObserver {
         if (tabs.isEmpty() || !canRestore) return;
 
         if (tabs.size() == 1) {
+            boolean is_always_incognito = AlwaysIncognitoLinkInterceptor.isAlwaysIncognito();
             Tab tab = tabs.get(0);
             if (!isTabGroupWithOneTab(tab)) {
-                mHistoricalTabSaver.createHistoricalTab(tab);
+                mHistoricalTabSaver.createHistoricalTab(tab, is_always_incognito);
                 return;
             }
         }
