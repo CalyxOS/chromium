38b390b3c0f2ee0b8f38f71e5da7bccbb92dbf46
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-10-02 13:20:36 +0200

    Add an always-incognito mode
    Add a preference that causes all new tabs and all clicked links to launch in incognito.
    Make sure initial incognito status is correctly recognized.
    Enable incognito custom tabs and fix crashes for incognito/custom tab intents
    Use a native flag to correctly start new tabs on app startup
    
    Add history, recents, offlinepages and send to home screen support for always incognito.
    History, recent tabs and offline pages require the INCOGNITO_TAB_HISTORY_ENABLED
    flag turned on.
    IncognitoPlaceholder is also deactivated, both in the phone and tablet version.
    The relative tests are also present.
    
    based on the original work by Ryan Archer <ryan.bradley.archer@gmail.com>
    Major contributions by uazo.
    See also: https://github.com/bromite/bromite/pull/1427
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I9c35d6d686f8091e4dcd96c1906ff3f8cbed1a58

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
@@ -29,6 +29,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.shared_preferences.SharedPreferencesManager;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.AlwaysIncognitoLinkInterceptor;
 import org.chromium.chrome.browser.bookmarks.BookmarkUtils;
 import org.chromium.chrome.browser.contextmenu.ChromeContextMenuItem.Item;
 import org.chromium.chrome.browser.contextmenu.ContextMenuCoordinator.ListItemType;
@@ -293,6 +294,9 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
     public List<Pair<Integer, ModelList>> buildContextMenu() {
         mShowEphemeralTabNewLabel = null;
 
+        boolean always_incognito =
+            AlwaysIncognitoLinkInterceptor.isAlwaysIncognito();
+
         List<Pair<Integer, ModelList>> groupedItems = new ArrayList<>();
 
         if (mParams.isPage() && shouldShowEmptySpaceContextMenu()) {
@@ -350,7 +354,7 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
                 }
             }
             if (FirstRunStatus.getFirstRunFlowComplete()) {
-                if (!mItemDelegate.isIncognito()
+                if ((always_incognito || !mItemDelegate.isIncognito())
                         && UrlUtilities.isDownloadableScheme(mParams.getLinkUrl())) {
                     linkGroup.add(
                             createListItem(
--- chrome/browser/ui/android/native_page/java/src/org/chromium/chrome/browser/ui/native_page/NativePage.java
+++ chrome/browser/ui/android/native_page/java/src/org/chromium/chrome/browser/ui/native_page/NativePage.java
@@ -17,6 +17,8 @@ import org.chromium.url.GURL;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import org.chromium.base.ContextUtils;
+
 /** An interface for pages that will be using Android views instead of html/rendered Web content. */
 @NullMarked
 public interface NativePage {
@@ -179,7 +181,8 @@ public interface NativePage {
      */
     static boolean isNativePageUrl(GURL url, boolean isIncognito, boolean hasPdfDownload) {
         return url != null
-                && nativePageType(url, null, isIncognito, hasPdfDownload) != NativePageType.NONE;
+                && nativePageType(url, null, isIncognito, hasPdfDownload, /*isAlwaysIncognito*/ false)
+                        != NativePageType.NONE;
     }
 
     /**
@@ -189,7 +192,7 @@ public interface NativePage {
      *     not have chrome or chrome-native scheme.
      */
     static boolean isChromePageUrl(GURL url, boolean isIncognito) {
-        return url != null && chromePageType(url, null, isIncognito) != NativePageType.NONE;
+        return url != null && chromePageType(url, null, isIncognito, /*isAlwaysIncognito*/false) != NativePageType.NONE;
     }
 
     /**
@@ -201,11 +204,12 @@ public interface NativePage {
      */
     // TODO(crbug.com/40549331) - Convert to using GURL.
     static @NativePageType int nativePageType(
-            String url, NativePage candidatePage, boolean isIncognito, boolean hasPdfDownload) {
+            String url, NativePage candidatePage, boolean isIncognito, boolean hasPdfDownload,
+            boolean isAlwaysIncognito) {
         if (url == null) return NativePageType.NONE;
 
         GURL gurl = new GURL(url);
-        return nativePageType(gurl, candidatePage, isIncognito, hasPdfDownload);
+        return nativePageType(gurl, candidatePage, isIncognito, hasPdfDownload, isAlwaysIncognito);
     }
 
     /**
@@ -219,7 +223,8 @@ public interface NativePage {
             GURL url,
             @Nullable NativePage candidatePage,
             boolean isIncognito,
-            boolean hasPdfDownload) {
+            boolean hasPdfDownload,
+            boolean isAlwaysIncognito) {
         if (hasPdfDownload) {
             // For navigation with associated pdf download (e.g. open a pdf link), pdf page should
             // be created.
@@ -237,7 +242,7 @@ public interface NativePage {
             // created after the pdf document is re-downloaded in other parts of the code.
             return NativePageType.NONE;
         } else {
-            return chromePageType(url, candidatePage, isIncognito);
+            return chromePageType(url, candidatePage, isIncognito, isAlwaysIncognito);
         }
     }
 
@@ -249,7 +254,7 @@ public interface NativePage {
      *     which do not have chrome or chrome-native scheme.
      */
     private static @NativePageType int chromePageType(
-            GURL url, @Nullable NativePage candidatePage, boolean isIncognito) {
+            GURL url, @Nullable NativePage candidatePage, boolean isIncognito, boolean isAlwaysIncognito) {
         String host = url.getHost();
         String scheme = url.getScheme();
         if (!UrlConstants.CHROME_NATIVE_SCHEME.equals(scheme)
@@ -269,7 +274,8 @@ public interface NativePage {
             return NativePageType.DOWNLOADS;
         } else if (UrlConstants.HISTORY_HOST.equals(host)) {
             return NativePageType.HISTORY;
-        } else if (UrlConstants.RECENT_TABS_HOST.equals(host) && !isIncognito) {
+        } else if (UrlConstants.RECENT_TABS_HOST.equals(host) &&
+                  (!isIncognito || isAlwaysIncognito)) {
             return NativePageType.RECENT_TABS;
         } else if (UrlConstants.EXPLORE_HOST.equals(host)) {
             return NativePageType.EXPLORE;
