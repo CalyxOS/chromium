ea51c58818332ecdff1d3c6c96fabea51f546bab
uazo <uazo@users.noreply.github.com>
2021-10-05 08:38:52 +0000

    Site setting for images
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: Content-settings-infrastructure.patch
    
    Change-Id: Icd829f513d0d8ef8a63402edb20d8fe6bb06032c

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h
@@ -229,6 +229,7 @@ class PLATFORM_EXPORT ResourceFetcher
   bool StartLoad(Resource*, bool is_potentially_unused_preload = false);
 
   void SetAutoLoadImages(bool);
+  void SetImagesEnabled(bool);
 
   FetchContext& Context() const;
   void ClearContext();
@@ -692,6 +693,7 @@ class PLATFORM_EXPORT ResourceFetcher
 
   // 27 bits left
   bool auto_load_images_ : 1;
+  bool images_enabled_ : 1;
   bool allow_stale_resources_ : 1;
   bool image_fetched_ : 1;
   bool stale_while_revalidate_enabled_ : 1;
