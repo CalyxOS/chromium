7d193862df6323706fc26ed5788264bf0f1e6f92
uazo <uazo@users.noreply.github.com>
2021-10-05 08:38:52 +0000

    Site setting for images
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: Content-settings-infrastructure.patch
    
    Change-Id: Icd829f513d0d8ef8a63402edb20d8fe6bb06032c

--- third_party/blink/renderer/core/html/image_document.cc
+++ third_party/blink/renderer/core/html/image_document.cc
@@ -141,7 +141,14 @@ void ImageDocumentParser::AppendBytes(base::span<const uint8_t> data) {
     return;
 
   LocalFrame* frame = GetDocument()->GetFrame();
-  bool allow_image = frame->ImagesEnabled();
+  Settings* settings = frame->GetSettings();
+  bool allow_image_renderer = !settings || settings->GetImagesEnabled();
+  bool allow_image_content_setting = false;
+  if (auto* client = frame->GetContentSettingsClient()) {
+    allow_image_content_setting = client->AllowImage(allow_image_renderer,
+      GetDocument()->Url());
+  }
+  bool allow_image = allow_image_renderer && allow_image_content_setting;
   if (!allow_image) {
     auto* client = frame->GetContentSettingsClient();
     if (client) {
