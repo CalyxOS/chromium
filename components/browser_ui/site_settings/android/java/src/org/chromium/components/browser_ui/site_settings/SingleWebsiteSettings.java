d2e46455b3938b59bfd04bba33df6ed05eb50d17
uazo <uazo@users.noreply.github.com>
2021-10-05 08:38:52 +0000

    Site setting for images
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Icd829f513d0d8ef8a63402edb20d8fe6bb06032c

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleWebsiteSettings.java
@@ -141,6 +141,8 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
                 return "autoplay_permission_list";
             case ContentSettingsType.IDLE_DETECTION:
                 return "idle_detection_permission_list";
+            case ContentSettingsType.IMAGES:
+                return "images_permission_list";
             case ContentSettingsType.JAVASCRIPT:
                 return "javascript_permission_list";
             case ContentSettingsType.POPUPS:
@@ -533,6 +535,8 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
                 setUpJavascriptPreference(preference);
             } else if (type == ContentSettingsType.GEOLOCATION) {
                 setUpLocationPreference(preference);
+            } else if (type == ContentSettingsType.IMAGES) {
+                setUpImagesPreference(preference);
             } else if (type == ContentSettingsType.NOTIFICATIONS) {
                 setUpNotificationsPreference(preference, mSite.isEmbargoed(type));
             } else if (type == ContentSettingsType.REQUEST_DESKTOP_SITE) {
@@ -1139,6 +1143,24 @@ public class SingleWebsiteSettings extends SiteSettingsPreferenceFragment
                 mSite.getContentSetting(getSiteSettingsDelegate().getBrowserContextHandle(),
                         ContentSettingsType.REQUEST_DESKTOP_SITE),
                 mSite.isEmbargoed(ContentSettingsType.REQUEST_DESKTOP_SITE));
+    }
+
+    private void setUpImagesPreference(Preference preference) {
+        BrowserContextHandle browserContextHandle =
+                getSiteSettingsDelegate().getBrowserContextHandle();
+        @ContentSettingValues
+        @Nullable
+        Integer currentValue =
+                mSite.getContentSetting(browserContextHandle, ContentSettingsType.IMAGES);
+        // Always show the Images permission
+        if (currentValue == null) {
+            currentValue = WebsitePreferenceBridge.isCategoryEnabled(
+                                   browserContextHandle, ContentSettingsType.IMAGES)
+                    ? ContentSettingValues.ALLOW
+                    : ContentSettingValues.BLOCK;
+        }
+        // Not possible to embargo IMAGES.
+        setupContentSettingsPreference(preference, currentValue, false /* isEmbargoed */);
     }
 
     private String getDSECategorySummary(@ContentSettingValues int value) {
