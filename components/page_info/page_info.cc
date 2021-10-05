c5de2abe4ebea18414009d920a9e7925e56ca5b1
uazo <uazo@users.noreply.github.com>
2021-10-05 08:38:52 +0000

    Site setting for images
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- components/page_info/page_info.cc
+++ components/page_info/page_info.cc
@@ -94,9 +94,7 @@ ContentSettingsType kPermissionType[] = {
     ContentSettingsType::SENSORS,
     ContentSettingsType::NOTIFICATIONS,
     ContentSettingsType::JAVASCRIPT,
-#if !BUILDFLAG(IS_ANDROID)
     ContentSettingsType::IMAGES,
-#endif
     ContentSettingsType::POPUPS,
     ContentSettingsType::WINDOW_MANAGEMENT,
     ContentSettingsType::ADS,
@@ -148,6 +146,11 @@ bool ShouldShowPermission(const PageInfo::PermissionInfo& info,
 
   // Always show autoplay when it has a site-specific override
   if (info.type == ContentSettingsType::AUTOPLAY) {
+     return true;
+  }
+
+  // Always show images when it has a site-specific override
+  if (info.type == ContentSettingsType::IMAGES) {
      return true;
   }
 
