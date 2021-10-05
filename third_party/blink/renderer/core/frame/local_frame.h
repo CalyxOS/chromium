d92c934d72002f089af46dd4734fffc2435d6424
uazo <uazo@users.noreply.github.com>
2021-10-05 08:38:52 +0000

    Site setting for images
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: Content-settings-infrastructure.patch
    
    Change-Id: Icd829f513d0d8ef8a63402edb20d8fe6bb06032c

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -926,12 +926,6 @@ class CORE_EXPORT LocalFrame final
     return *v8_local_compile_hints_producer_;
   }
 
-  // Returns whether images are allowed to load for the current frame. This is a
-  // convenience method that checks both renderer content settings and frame
-  // settings.
-  // Can only be called while the frame is not detached.
-  bool ImagesEnabled();
-
   // Returns whether script is allowed to run for the current frame. This is a
   // convenience method that checks both renderer content settings and frame
   // settings.
