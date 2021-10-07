3c3306635e92e4d4c86469d96d59908615d6d108
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-10-08 00:30:26 +0200

    Disable lock icon in address bar by default
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I0fce1c40330743b47ba4ab32f945cf3101b1213b

--- components/omnibox/common/omnibox_features.cc
+++ components/omnibox/common/omnibox_features.cc
@@ -383,8 +383,8 @@ BASE_FEATURE(kOmniboxSteadyStateTextColor,
 // used, on non-Android platforms this is controlled through the
 // ChromeRefresh2023 flag.
 BASE_FEATURE(kUpdatedConnectionSecurityIndicators,
-             "OmniboxUpdatedConnectionSecurityIndicators",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             "OmniboxUpdatedConnectionSecurityIndicators",    // enabled by default
+             base::FEATURE_ENABLED_BY_DEFAULT);               // in Bromite
 
 // Feature used to default typed navigations to use HTTPS instead of HTTP.
 // This only applies to navigations that don't have a scheme such as
