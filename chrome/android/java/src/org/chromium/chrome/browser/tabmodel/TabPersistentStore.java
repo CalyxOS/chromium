f5ed231c7cdaf5c536fe19e59dbdc91f8d31e105
uazo <uazo@users.noreply.github.com>
2021-10-27 10:57:09 +0000

    Keep empty tabs between sessions
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I170ffd473248041f776dd87c3bcae825c4b6113f

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabPersistentStore.java
@@ -1248,25 +1248,10 @@ public class TabPersistentStore {
                 continue;
             }
 
-            if (i == activeIndex) {
-                // If any non-active NTPs have been skipped, the serialized tab model index
-                // needs to be adjusted.
-                modelInfo.index = modelInfo.ids.size();
-            } else if (shouldSkipTab(tab)) {
-                continue;
-            }
             modelInfo.ids.add(tab.getId());
             modelInfo.urls.add(tab.getUrl().getSpec());
         }
         return modelInfo;
-    }
-
-    public static boolean shouldSkipTab(@NonNull Tab tab) {
-        boolean isNtp = tab.isNativePage() && UrlUtilities.isNtpUrl(tab.getUrl());
-        if (!isNtp) return false;
-
-        // Only skip NTP tabs that are not in a tab group.
-        return tab.getTabGroupId() == null;
     }
 
     private void saveListToFile(TabModelSelectorMetadata listData) {
