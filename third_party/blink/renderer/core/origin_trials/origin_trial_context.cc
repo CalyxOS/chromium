1362c8dd6882f6262f6ba6c1eacfe12d95e6e9b9
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-04 09:19:24 +0100

    Disable third-party origin trials
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I3e5ec3477997954b99b12f7d5c7a475b2ff45472

--- third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
+++ third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
@@ -317,6 +317,9 @@ void OriginTrialContext::AddToken(const String& token) {
 void OriginTrialContext::AddTokenFromExternalScript(
     const String& token,
     const Vector<scoped_refptr<SecurityOrigin>>& external_origins) {
+  if ((true)) {
+    return;
+  }
   Vector<OriginInfo> script_origins;
   for (const scoped_refptr<SecurityOrigin>& origin : external_origins) {
     OriginInfo origin_info = {.origin = origin,
@@ -452,8 +455,6 @@ bool OriginTrialContext::InstallSettingFeature(
 }
 
 void OriginTrialContext::AddFeature(mojom::blink::OriginTrialFeature feature) {
-  enabled_features_.insert(feature);
-  InitializePendingFeatures();
 }
 
 bool OriginTrialContext::IsFeatureEnabled(
@@ -497,6 +498,7 @@ void OriginTrialContext::AddForceEnabledTrials(
 }
 
 bool OriginTrialContext::CanEnableTrialFromName(const StringView& trial_name) {
+  if ((true)) return false;
   if (trial_name == "PrivacySandboxAdsAPIs")
     return base::FeatureList::IsEnabled(features::kPrivacySandboxAdsAPIs);
 
