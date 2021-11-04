64c1de1626e1e31c5ff055d71740f5df74a50c92
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-04 09:19:24 +0100

    Disable third-party origin trials
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3e5ec3477997954b99b12f7d5c7a475b2ff45472

--- third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
+++ third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
@@ -316,6 +316,9 @@ void OriginTrialContext::AddToken(const String& token) {
 void OriginTrialContext::AddTokenFromExternalScript(
     const String& token,
     const Vector<scoped_refptr<SecurityOrigin>>& external_origins) {
+  if ((true)) {
+    return;
+  }
   Vector<OriginInfo> script_origins;
   for (const scoped_refptr<SecurityOrigin>& origin : external_origins) {
     OriginInfo origin_info = {.origin = origin,
@@ -451,8 +454,6 @@ bool OriginTrialContext::InstallSettingFeature(
 }
 
 void OriginTrialContext::AddFeature(OriginTrialFeature feature) {
-  enabled_features_.insert(feature);
-  InitializePendingFeatures();
 }
 
 bool OriginTrialContext::IsFeatureEnabled(OriginTrialFeature feature) const {
@@ -494,6 +495,7 @@ void OriginTrialContext::AddForceEnabledTrials(
 }
 
 bool OriginTrialContext::CanEnableTrialFromName(const StringView& trial_name) {
+  if ((true)) return false;
   if (trial_name == "Portals")
     return base::FeatureList::IsEnabled(features::kPortals);
 
