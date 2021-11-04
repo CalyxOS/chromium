7e58fa92670566f628f3d545458a59ff00e353cc
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-04 09:19:24 +0100

    Disable third-party origin trials
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3e5ec3477997954b99b12f7d5c7a475b2ff45472

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -8375,7 +8375,6 @@ void NavigationRequest::SetSourceSiteInstanceToInitiatorIfNeeded() {
 void NavigationRequest::ForceEnableOriginTrials(
     const std::vector<std::string>& trials) {
   DCHECK(!HasCommitted());
-  commit_params_->force_enabled_origin_trials = trials;
 }
 
 network::CrossOriginEmbedderPolicy
