7869be9904040d8b53efeb12dee41e546a73211f
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-04 09:19:24 +0100

    Disable third-party origin trials
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3e5ec3477997954b99b12f7d5c7a475b2ff45472

--- third_party/blink/common/origin_trials/trial_token_validator.cc
+++ third_party/blink/common/origin_trials/trial_token_validator.cc
@@ -425,6 +425,7 @@ TrialTokenValidator::GetValidTokens(const url::Origin& origin,
 
 // static
 bool TrialTokenValidator::IsTrialPossibleOnOrigin(const GURL& url) {
+  if ((true)) return false;
   OriginTrialPolicy* policy = PolicyGetter().Run();
   return policy && policy->IsOriginTrialsSupported() &&
          policy->IsOriginSecure(url);
