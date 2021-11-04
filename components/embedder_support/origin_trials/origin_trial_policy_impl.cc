ab2339c62053d8048409d1b540fe1b0d99aca830
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-04 09:19:24 +0100

    Disable third-party origin trials
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- components/embedder_support/origin_trials/origin_trial_policy_impl.cc
+++ components/embedder_support/origin_trials/origin_trial_policy_impl.cc
@@ -21,10 +21,10 @@
 
 namespace embedder_support {
 
-// This is the default public key used for validating signatures.
+// This is an invalid public key that will match no origin trial signature
 static const blink::OriginTrialPublicKey kDefaultPublicKey = {
     0x7c, 0xc4, 0xb8, 0x9a, 0x93, 0xba, 0x6e, 0xe2, 0xd0, 0xfd, 0x03,
-    0x1d, 0xfb, 0x32, 0x66, 0xc7, 0x3b, 0x72, 0xfd, 0x54, 0x3a, 0x07,
+    0x1d, 0xfb, 0x32, 0x66, 0x00, 0x01, 0x02, 0xfd, 0x54, 0x3a, 0x07,
     0x51, 0x14, 0x66, 0xaa, 0x02, 0x53, 0x4e, 0x33, 0xa1, 0x15,
 };
 
@@ -55,7 +55,8 @@ OriginTrialPolicyImpl::OriginTrialPolicyImpl() {
 OriginTrialPolicyImpl::~OriginTrialPolicyImpl() = default;
 
 bool OriginTrialPolicyImpl::IsOriginTrialsSupported() const {
-  return true;
+  // third-party origin trials are always disabled
+  return false;
 }
 
 const std::vector<blink::OriginTrialPublicKey>&
