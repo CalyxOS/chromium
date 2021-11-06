f6dd97686bffba91af98981906b38ea94d4e0b68
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-06 09:46:24 +0100

    Never use HTTP probes for connection detection
    This patch disables the use of HTTP probes on Android < M devices
    Connection will be always considered valid on such devices
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Iaecb399a49b47ef27a8a5617794eb7e33881be59

--- chrome/android/java/src/org/chromium/chrome/browser/net/connectivitydetector/ConnectivityDetector.java
+++ chrome/android/java/src/org/chromium/chrome/browser/net/connectivitydetector/ConnectivityDetector.java
@@ -182,7 +182,8 @@ public class ConnectivityDetector implements NetworkChangeNotifier.ConnectionTyp
 
         @Override
         public boolean shouldSkipHttpProbes() {
-            return false;
+            // always assume that connection is valid on Android < M
+            return true;
         }
     }
 
