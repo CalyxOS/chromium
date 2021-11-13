e4a16fca33466d48ea494b838590e6c0142a6ef1
uazo <uazo@users.noreply.github.com>
2021-11-13 09:17:06 +0000

    Disable privacy sandbox
    Remove UI from the settings and set the flags to inactive
    Permanently removes FLoC support, disabling the download of LSH clusters,
    the marking the history navigation and the javascript API and permission policies.
    Also disable FirstPartySets and StorageAccessAPI.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I86d3bfd11881e48146811465ea8e5c699100cd63

--- chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
@@ -626,7 +626,7 @@ bool PrivacySandboxServiceImpl::IsRestrictedNoticeEnabled() {
 void PrivacySandboxServiceImpl::SetFirstPartySetsDataAccessEnabled(
     bool enabled) {
   pref_service_->SetBoolean(prefs::kPrivacySandboxRelatedWebsiteSetsEnabled,
-                            enabled);
+                            false);
 }
 
 bool PrivacySandboxServiceImpl::IsFirstPartySetsDataAccessEnabled() const {
--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -253,4 +253,8 @@ BASE_FEATURE(kPrivacySandboxSentimentSurvey,
 const base::FeatureParam<std::string> kPrivacySandboxSentimentSurveyTriggerId{
     &kPrivacySandboxSentimentSurvey, "sentiment-survey-trigger-id", ""};
 
+SET_CROMITE_FEATURE_DISABLED(kPrivacySandboxSettings4);
+SET_CROMITE_FEATURE_ENABLED(kDisablePrivacySandboxPrompts);
+SET_CROMITE_FEATURE_DISABLED(kEnforcePrivacySandboxAttestations);
+SET_CROMITE_FEATURE_DISABLED(kPrivacySandboxFirstPartySetsUI);
 }  // namespace privacy_sandbox
