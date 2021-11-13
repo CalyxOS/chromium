2ef78ee474e95367619dc7ebe4f77f43ba772235
uazo <uazo@users.noreply.github.com>
2021-11-13 09:17:06 +0000

    Disable FLoC and privacy sandbox
    Remove UI from the settings and set the flags to inactive
    Permanently removes FLoC support, disabling the download of LSH clusters,
    the marking the history navigation and the javascript API and permission policies.
    Also added the disabling of blink features through the DisabledForBromite tag
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I86d3bfd11881e48146811465ea8e5c699100cd63

--- components/privacy_sandbox/privacy_sandbox_settings_impl.cc
+++ components/privacy_sandbox/privacy_sandbox_settings_impl.cc
@@ -120,7 +120,8 @@ PrivacySandboxSettingsImpl::GetM1TopicAllowedStatus() const {
   return control_status;
 }
 
-bool PrivacySandboxSettingsImpl::IsTopicsAllowed() const {
+bool PrivacySandboxSettingsImpl::IsTopicsAllowed() const { // disabled in Bromite
+  if ((true)) return false;
   // M1 specific
   if (base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4)) {
     Status status = GetM1TopicAllowedStatus();
@@ -163,7 +164,8 @@ bool PrivacySandboxSettingsImpl::IsTopicsAllowedForContext(
          IsPrivacySandboxEnabledForContext(top_frame_origin, url);
 }
 
-bool PrivacySandboxSettingsImpl::IsTopicAllowed(const CanonicalTopic& topic) {
+bool PrivacySandboxSettingsImpl::IsTopicAllowed(const CanonicalTopic& topic) { // disabled in Bromite
+  if ((true)) return false;
   const auto& blocked_topics =
       pref_service_->GetList(prefs::kPrivacySandboxBlockedTopics);
 
@@ -246,7 +248,8 @@ PrivacySandboxSettingsImpl::GetM1AttributionReportingAllowedStatus(
                                     reporting_origin.GetURL());
 }
 
-bool PrivacySandboxSettingsImpl::IsAttributionReportingEverAllowed() const {
+bool PrivacySandboxSettingsImpl::IsAttributionReportingEverAllowed() const { // disabled in Bromite
+  if ((true)) return false;
   // M1 specific
   if (base::FeatureList::IsEnabled(privacy_sandbox::kPrivacySandboxSettings4)) {
     Status status = GetM1PrivacySandboxApiEnabledStatus(
@@ -378,7 +381,8 @@ void PrivacySandboxSettingsImpl::ClearFledgeJoiningAllowedSettings(
 }
 
 bool PrivacySandboxSettingsImpl::IsFledgeJoiningAllowed(
-    const url::Origin& top_frame_origin) const {
+    const url::Origin& top_frame_origin) const { // disabled in Bromite
+  if ((true)) return false;
   ScopedDictPrefUpdate scoped_pref_update(
       pref_service_, prefs::kPrivacySandboxFledgeJoinBlocked);
   auto& pref_data = scoped_pref_update.Get();
@@ -470,7 +474,8 @@ bool PrivacySandboxSettingsImpl::IsPrivateAggregationAllowed(
 }
 
 bool PrivacySandboxSettingsImpl::IsPrivacySandboxEnabled() const {
-  PrivacySandboxSettingsImpl::Status status = GetPrivacySandboxAllowedStatus();
+  PrivacySandboxSettingsImpl::Status status = GetPrivacySandboxAllowedStatus(); // disabled in Bromite
+  if ((true)) return false;
   if (!IsAllowed(status)) {
     return false;
   }
@@ -568,9 +573,9 @@ bool PrivacySandboxSettingsImpl::IsPrivacySandboxEnabledForContext(
       content_settings::CookieSettings::QueryReason::kPrivacySandbox);
 }
 
-void PrivacySandboxSettingsImpl::SetTopicsDataAccessibleFromNow() const {
-  pref_service_->SetTime(prefs::kPrivacySandboxTopicsDataAccessibleSince,
-                         base::Time::Now());
+void PrivacySandboxSettingsImpl::SetTopicsDataAccessibleFromNow() const { // disabled in Bromite
+  pref_service_->ClearPref(prefs::kPrivacySandboxTopicsDataAccessibleSince);
+  if ((true)) return;
 
   for (auto& observer : observers_) {
     observer.OnTopicsDataAccessibleSinceUpdated();
