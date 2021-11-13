16577ce35d2104870d273102800a69049e46367a
uazo <uazo@users.noreply.github.com>
2021-11-13 09:17:06 +0000

    Disable privacy sandbox
    Remove UI from the settings and set the flags to inactive
    Permanently removes FLoC support, disabling the download of LSH clusters,
    the marking the history navigation and the javascript API and permission policies.
    Also disable FirstPartySets and StorageAccessAPI.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I86d3bfd11881e48146811465ea8e5c699100cd63

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1407,7 +1407,7 @@ void RegisterProfilePrefsForMigration(
   registry->RegisterTimePref(kPlusAddressLastFetchedTime, base::Time());
 
   // Deprecated 03/2024.
-  registry->RegisterBooleanPref(kPrivacySandboxApisEnabled, true);
+  registry->RegisterBooleanPref(kPrivacySandboxApisEnabled, false);
 
   // Deprecated 03/2024.
   registry->RegisterBooleanPref(kShowInternalAccessibilityTree, false);
