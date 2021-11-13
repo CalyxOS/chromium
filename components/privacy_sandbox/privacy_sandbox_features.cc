df81ab898fd872ef5843c970714c3de2ef7de45e
uazo <uazo@users.noreply.github.com>
2021-11-13 09:17:06 +0000

    Disable FLoC and privacy sandbox
    Remove UI from the settings and set the flags to inactive
    Permanently removes FLoC support, disabling the download of LSH clusters,
    the marking the history navigation and the javascript API and permission policies.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I86d3bfd11881e48146811465ea8e5c699100cd63

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -193,4 +193,8 @@ BASE_FEATURE(kTrackingProtectionReminder,
              "TrackingProtectionReminder",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+SET_CROMITE_FEATURE_DISABLED(kPrivacySandboxSettings4);
+SET_CROMITE_FEATURE_ENABLED(kDisablePrivacySandboxPrompts);
+SET_CROMITE_FEATURE_DISABLED(kEnforcePrivacySandboxAttestations);
+SET_CROMITE_FEATURE_DISABLED(kPrivacySandboxFirstPartySetsUI);
 }  // namespace privacy_sandbox
