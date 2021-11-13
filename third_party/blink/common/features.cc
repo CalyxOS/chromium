81c991e524fcc0a2d434f7e86167885e9ceb5d4b
uazo <uazo@users.noreply.github.com>
2021-11-13 09:17:06 +0000

    Disable FLoC and privacy sandbox
    Remove UI from the settings and set the flags to inactive
    Permanently removes FLoC support, disabling the download of LSH clusters,
    the marking the history navigation and the javascript API and permission policies.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I86d3bfd11881e48146811465ea8e5c699100cd63

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2571,6 +2571,7 @@ BASE_FEATURE(kWebviewAccelerateSmallCanvases,
 // constants for features in the section above.
 
 bool IsAllowURNsInIframeEnabled() {
+  if ((true)) return false; // see https://chromium-review.googlesource.com/c/chromium/src/+/3690741
   return base::FeatureList::IsEnabled(blink::features::kAllowURNsInIframes);
 }
 
