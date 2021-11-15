f83c42a33d6356d6dac8ae375aaa5aaf5885a12d
uazo <uazo@users.noreply.github.com>
2021-11-15 09:43:29 +0000

    Disable conversion measurement api
    Disable Conversion Measurement API by disabling the flag and removing
    support for the AttributionReporting provider. it also removes
    the handling of attributions via intents between apps.
    This patch enforces the deactivation by preventing the report from
    being sent and being saved to disk, although it is currently in uncalled code.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I5d97fd708d050e505b451705ca0d95d61b1c76a5

--- content/browser/aggregation_service/aggregation_service_features.cc
+++ content/browser/aggregation_service/aggregation_service_features.cc
@@ -20,4 +20,5 @@ BASE_FEATURE(kPrivacySandboxAggregationServiceFilteringIds,
              "PrivacySandboxAggregationServiceFilteringIds",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+SET_CROMITE_FEATURE_DISABLED(kPrivacySandboxAggregationService);
 }  // namespace content
