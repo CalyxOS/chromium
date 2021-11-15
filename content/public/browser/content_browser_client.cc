17cde7c63367348419cf0f914adbc403ca1c465c
uazo <uazo@users.noreply.github.com>
2021-11-15 09:43:29 +0000

    Disable conversion measurement api
    Disable Conversion Measurement API by disabling the flag and removing
    support for the AttributionReporting provider. it also removes
    the handling of attributions via intents between apps.
    This patch enforces the deactivation by preventing the report from
    being sent and being saved to disk, although it is currently in uncalled code.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I5d97fd708d050e505b451705ca0d95d61b1c76a5

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -554,6 +554,7 @@ void ContentBrowserClient::OnAuctionComplete(
 network::mojom::AttributionSupport ContentBrowserClient::GetAttributionSupport(
     AttributionReportingOsApiState state,
     content::WebContents* web_contents) {
+  if ((true)) return network::mojom::AttributionSupport::kNone;
   switch (state) {
     case AttributionReportingOsApiState::kDisabled:
       return network::mojom::AttributionSupport::kWeb;
@@ -570,17 +571,17 @@ bool ContentBrowserClient::IsAttributionReportingOperationAllowed(
     const url::Origin* destination_origin,
     const url::Origin* reporting_origin,
     bool* can_bypass) {
-  return true;
+  return false;
 }
 
 bool ContentBrowserClient::ShouldUseOsWebSourceAttributionReporting(
     content::RenderFrameHost* rfh) {
-  return true;
+  return false;
 }
 
 bool ContentBrowserClient::ShouldUseOsWebTriggerAttributionReporting(
     content::RenderFrameHost* rfh) {
-  return true;
+  return false;
 }
 
 bool ContentBrowserClient::IsSharedStorageAllowed(
