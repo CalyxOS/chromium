5faabf77c9664d9d59774d1c71a427af33a9baaa
uazo <uazo@users.noreply.github.com>
2021-11-15 09:43:29 +0000

    Disable conversion measurement api
    Disable Conversion Measurement API by disabling the flag and removing
    support for the AttributionReporting provider. it also removes
    the handling of attributions via intents between apps.
    This patch enforces the deactivation by preventing the report from
    being sent and being saved to disk, although it is currently in uncalled code.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I5d97fd708d050e505b451705ca0d95d61b1c76a5

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -811,8 +811,8 @@ BASE_FEATURE(kPrivacySandboxAdsAPIsM1Override,
 // FLEDGE, Topics, along with a number of other features actively in development
 // within these APIs.
 BASE_FEATURE(kPrivacySandboxAdsAPIsOverride,
-             "PrivacySandboxAdsAPIsOverride",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "PrivacySandboxAdsAPIsOverride",       // must be disabled
+             base::FEATURE_DISABLED_BY_DEFAULT);    // in bromite
 
 // Enables Private Network Access checks for all types of web workers.
 //
