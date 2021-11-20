bc590251b62f5a7a76d17f86727662f121a3e4d1
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-20 15:36:54 +0000

    Add option to use home page as NTP
    And allow use about:blank as default homepage
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ida9e229f3e5654a70487bd8a393da9f786aec054

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
@@ -28,6 +28,7 @@ import org.chromium.base.task.TaskTraits;
 import org.chromium.chrome.browser.data_sharing.DataSharingNotificationManager;
 import org.chromium.chrome.browser.data_sharing.SharedImageTilesCoordinator;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.homepage.HomepageManager;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabCreationState;
 import org.chromium.chrome.browser.tab.TabLaunchType;
@@ -60,6 +61,7 @@ import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.text.EmptyTextWatcher;
+import org.chromium.url.GURL;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -748,11 +750,16 @@ public class TabGridDialogMediator
 
             assert relatedTabs.size() > 0;
 
+            String url = UrlConstants.NTP_URL;
+            if (HomepageManager.getInstance().getPrefNTPIsHomepageEnabled()) {
+                GURL gurl = HomepageManager.getInstance().getHomepageGurl();
+                url = gurl != null ? gurl.getSpec() : url;
+            }
             Tab parentTabToAttach = relatedTabs.get(relatedTabs.size() - 1);
             mTabCreatorManager
                     .getTabCreator(currentTab.isIncognito())
                     .createNewTab(
-                            new LoadUrlParams(UrlConstants.NTP_URL),
+                            new LoadUrlParams(url),
                             TabLaunchType.FROM_TAB_GROUP_UI,
                             parentTabToAttach);
             RecordUserAction.record("MobileNewTabOpened." + mComponentName);
