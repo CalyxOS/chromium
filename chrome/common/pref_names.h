9fcf457f6d2f48e2c5dc3b19c08a73b2e6a79331
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-20 15:36:54 +0000

    Add option to use home page as NTP
    And allow use about:blank as default homepage
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ida9e229f3e5654a70487bd8a393da9f786aec054

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -1319,6 +1319,10 @@ inline constexpr char kShowForwardButton[] = "browser.show_forward_button";
 // by enterprise policy.
 inline constexpr char kGeminiSettings[] = "browser.gemini_settings";
 
+// A boolean specifying whether opening a new tab should open the Home page
+// instead of the New Tab page.
+inline constexpr char kNewTabPageIsHomePage[] = "newtabpage_is_homepage";
+
 // Comma separated list of domain names (e.g. "google.com,school.edu").
 // When this pref is set, the user will be able to access Google Apps
 // only using an account that belongs to one of the domains from this pref.
