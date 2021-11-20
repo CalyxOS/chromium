78fd424ed6036f25f109724c03ebef73bde4d62d
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-20 15:36:54 +0000

    Add option to use home page as NTP
    And allow use about:blank as default homepage
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ida9e229f3e5654a70487bd8a393da9f786aec054

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -371,6 +371,7 @@ extern const char kRestrictedManagedGuestSessionExtensionCleanupExemptList[];
 extern const char kUsedPolicyCertificates[];
 #endif  // BUILDFLAG(IS_CHROMEOS)
 extern const char kShowHomeButton[];
+extern const char kNewTabPageIsHomePage[];
 extern const char kSpeechRecognitionFilterProfanities[];
 extern const char kAllowedDomainsForApps[];
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
