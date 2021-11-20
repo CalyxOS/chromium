d6e9f4d22fe859c7d42410e2b25e774ad613c0ef
csagan5 <32685696+csagan5@users.noreply.github.com>
2021-11-20 15:36:54 +0000

    Add option to use home page as NTP
    And allow use about:blank as default homepage
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ida9e229f3e5654a70487bd8a393da9f786aec054

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
@@ -22,6 +22,7 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.chrome.browser.data_sharing.DataSharingServiceFactory;
 import org.chromium.chrome.browser.data_sharing.ui.shared_image_tiles.SharedImageTilesCoordinator;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.homepage.HomepageManager;
 import org.chromium.chrome.browser.layouts.LayoutStateProvider;
 import org.chromium.chrome.browser.layouts.LayoutStateProvider.LayoutStateObserver;
 import org.chromium.chrome.browser.layouts.LayoutType;
@@ -408,10 +409,15 @@ public class TabGroupUiMediator implements BackPressHandler, ThemeColorObserver,
                     assert relatedTabs.size() > 0;
 
                     Tab parentTabToAttach = relatedTabs.get(relatedTabs.size() - 1);
+                    String url = UrlConstants.NTP_URL;
+                    if (HomepageManager.getInstance().getPrefNTPIsHomepageEnabled()) {
+                        GURL gurl = HomepageManager.getInstance().getHomepageGurl();
+                        url = gurl != null ? gurl.getSpec() : url;
+                    }
                     mTabCreatorManager
                             .getTabCreator(currentTab.isIncognito())
                             .createNewTab(
-                                    new LoadUrlParams(UrlConstants.NTP_URL),
+                                    new LoadUrlParams(url),
                                     TabLaunchType.FROM_TAB_GROUP_UI,
                                     parentTabToAttach);
                     RecordUserAction.record(
