caf0fc6527c0c50f2e8f8bfdede4702736be7be4
uazo <uazo@users.noreply.github.com>
2022-01-25 12:22:52 +0000

    Disable all predictors code
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I1d49077020e11828dc4b9ebe1bccd62d21e6c09f

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -240,12 +240,8 @@ BASE_FEATURE(kMixedContentAutoupgrade,
 // in the first viewport after the page load and the metrics of the clicked
 // anchor element will be extracted and recorded.
 BASE_FEATURE(kNavigationPredictor,
-             "NavigationPredictor",
-#if BUILDFLAG(IS_ANDROID)
-             base::FEATURE_ENABLED_BY_DEFAULT
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT
-#endif
+             "NavigationPredictor",                // disabled by default
+             base::FEATURE_DISABLED_BY_DEFAULT     // in Bromite
 );
 
 // Anchor Element Interaction
