2eaf09c8a2d0287e5773c3b700eb2b6740becacd
uazo <uazo@users.noreply.github.com>
2022-01-25 12:22:52 +0000

    Disable all predictors code
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I1d49077020e11828dc4b9ebe1bccd62d21e6c09f

--- chrome/browser/navigation_predictor/preloading_model_keyed_service.cc
+++ chrome/browser/navigation_predictor/preloading_model_keyed_service.cc
@@ -11,6 +11,7 @@
 #include "chrome/browser/navigation_predictor/preloading_model_handler.h"
 #endif
 
+#if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
 namespace {
 
 // The model takes all of its inputs as floats, so this is a convenience
@@ -28,12 +29,14 @@ constexpr float ToInput(base::TimeDelta val) {
 static_assert(1.0f == ToInput(true));
 
 }  // namespace
+#endif
 
 PreloadingModelKeyedService::Inputs::Inputs() = default;
 
 PreloadingModelKeyedService::PreloadingModelKeyedService(
     OptimizationGuideKeyedService* optimization_guide_keyed_service) {
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
+#error BUILD_WITH_TFLITE_LIB must be disabled
   auto* model_provider =
       static_cast<optimization_guide::OptimizationGuideModelProvider*>(
           optimization_guide_keyed_service);
