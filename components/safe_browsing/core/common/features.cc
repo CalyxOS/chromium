d3e78596ee7ec66713deb132c0fa5f9b9af38d33
uazo <uazo@users.noreply.github.com>
2022-01-25 12:22:52 +0000

    Disable all predictors code
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I1d49077020e11828dc4b9ebe1bccd62d21e6c09f

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -268,6 +268,7 @@ constexpr base::FeatureParam<std::string>
 BASE_FEATURE(kOnDeviceNotificationContentDetectionModel,
              "OnDeviceNotificationContentDetectionModel",
              base::FEATURE_ENABLED_BY_DEFAULT);
+SET_CROMITE_FEATURE_DISABLED(kOnDeviceNotificationContentDetectionModel);
 
 BASE_FEATURE(kNotificationTelemetry,
              "NotificationTelemetry",
