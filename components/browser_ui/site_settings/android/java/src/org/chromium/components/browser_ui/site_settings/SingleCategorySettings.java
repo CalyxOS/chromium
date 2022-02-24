fad5db9e3c8bd1de65b184e7ff822b8a3d8c3e81
uazo <uazo@users.noreply.github.com>
2022-02-24 07:54:36 +0000

    Content settings infrastructure
    This patch is used by other patches to provide the UI for Bromite-specific
    site settings.
    
    See BromiteCustomContentSetting_README.md for more information.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: bromite-build-utils.patch
    
    Change-Id: I246d2dc343ebe5150e7eb6ac0988cce0f3bf97e7

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleCategorySettings.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SingleCategorySettings.java
@@ -324,6 +324,10 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
                 website.site()
                         .getContentSetting(
                                 browserContextHandle, mCategory.getContentSettingsType());
+        if (contentSetting != null &&
+                BromiteCustomContentSettingImpl.processOnBlockList(contentSetting, contentSetting)) {
+            return BromiteCustomContentSettingImpl.isOnBlockList(contentSetting, website, contentSetting);
+        }
         if (contentSetting != null) {
             return ContentSettingValues.BLOCK == contentSetting;
         }
@@ -488,6 +492,7 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
                         ? new HashSet<>(getArguments().getStringArrayList(EXTRA_SELECTED_DOMAINS))
                         : null;
 
+        BromiteCustomContentSettingImpl.onActivityCreated(this);
         configureGlobalToggles();
         if (mCategory.getType() == SiteSettingsCategory.Type.REQUEST_DESKTOP_SITE) {
             RecordUserAction.record("DesktopSiteContentSetting.SettingsPage.Entered");
@@ -523,7 +528,8 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
                     if (queryHasChanged) getInfoForOrigins();
                 });
 
-        if (getSiteSettingsDelegate().isHelpAndFeedbackEnabled()) {
+        if (getSiteSettingsDelegate().isHelpAndFeedbackEnabled() ||
+                BromiteCustomContentSettingImpl.isHelpAndFeedbackEnabled(mCategory)) {
             MenuItem help =
                     menu.add(
                             Menu.NONE,
@@ -535,12 +541,20 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
                             getResources(),
                             R.drawable.ic_help_and_feedback,
                             getContext().getTheme()));
+            if (!BromiteCustomContentSettingImpl.isHelpAndFeedbackEnabled(mCategory))
+                help.setVisible(false);
         }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menu_id_site_settings_help) {
+            String url = BromiteCustomContentSettingImpl.getHelpAndFeedbackActivityUrl(mCategory);
+            if (!url.equals("")) {
+                getSiteSettingsDelegate()
+                        .launchHelpAndFeedbackActivity(getActivity(), url);
+                return true;
+            }
             if (mCategory.getType() == SiteSettingsCategory.Type.PROTECTED_MEDIA) {
                 getSiteSettingsDelegate()
                         .launchProtectedContentHelpAndFeedbackActivity(getActivity());
@@ -612,6 +626,11 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
         BrowserContextHandle browserContextHandle =
                 getSiteSettingsDelegate().getBrowserContextHandle();
         PrefService prefService = UserPrefs.get(browserContextHandle);
+        if (BromiteCustomContentSettingImpl.onPreferenceChange(mCategory,
+                    browserContextHandle, preference, newValue) == true) {
+            getInfoForOrigins();
+            return true;
+        }
         if (BINARY_TOGGLE_KEY.equals(preference.getKey())) {
             assert !mCategory.isManaged();
             boolean toggleValue = (boolean) newValue;
@@ -789,7 +808,7 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
                         ? R.string.website_settings_add_site_description_javascript_optimizer_block
                         : R.string.website_settings_add_site_description_javascript_optimizer_allow;
         }
-        return 0;
+        return BromiteCustomContentSettingImpl.getAddExceptionDialogMessage(mCategory);
     }
 
     // OnPreferenceClickListener:
@@ -903,10 +922,11 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
             default:
                 break;
         }
-
-        int exceptionDialogMessageResourceId = getAddExceptionDialogMessageResourceId();
-        assert allowSpecifyingExceptions == (exceptionDialogMessageResourceId != 0);
+        Boolean allow = BromiteCustomContentSettingImpl.allowSpecifyingExceptions(mCategory);
+        if (allow != null) allowSpecifyingExceptions = (boolean)allow;
         if (allowSpecifyingExceptions) {
+            int exceptionDialogMessageResourceId = getAddExceptionDialogMessageResourceId();
+            assert exceptionDialogMessageResourceId != 0;
             getPreferenceScreen()
                     .addPreference(
                             new AddExceptionPreference(
@@ -1091,8 +1111,16 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
     private boolean isBlocked() {
         switch (mGlobalToggleLayout) {
             case GlobalToggleLayout.TRI_STATE_TOGGLE:
+                Preference triStateToggleImpl =
+                        getPreferenceScreen().findPreference(TRI_STATE_TOGGLE_KEY);
+                if (triStateToggleImpl instanceof BromiteCustomTriStateSiteSettingsPreferenceImpl) {
+                    Boolean blocked = BromiteCustomContentSettingImpl.considerException(mCategory,
+                        ((BromiteCustomTriStateSiteSettingsPreferenceImpl)triStateToggleImpl).getCheckedSetting());
+                    if (blocked != null) return (boolean)blocked;
+                }
                 TriStateSiteSettingsPreference triStateToggle =
                         getPreferenceScreen().findPreference(TRI_STATE_TOGGLE_KEY);
+                if (triStateToggle == null) return true;
                 return (triStateToggle.getCheckedSetting() == ContentSettingValues.BLOCK);
             case GlobalToggleLayout.TRI_STATE_COOKIE_TOGGLE:
                 TriStateCookieSettingsPreference triStateCookieToggle =
@@ -1162,7 +1190,11 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
             infoText.setSummary(
                     R.string.website_settings_category_javascript_optimizer_page_description);
         } else {
-            screen.removePreference(infoText);
+            int infoMessage = BromiteCustomContentSettingImpl.getCategoryDescription(mCategory);
+            if (infoMessage == 0)
+                screen.removePreference(infoText);
+            else
+                infoText.setSummary(infoMessage);
         }
 
         // Hide the anti-abuse text preferences, as needed.
@@ -1199,6 +1231,7 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
         } else {
             screen.removePreference(mLocationTriStatePref);
         }
+        BromiteCustomContentSettingImpl.configureGlobalToggles(mCategory, this);
 
         if (permissionBlockedByOs) {
             maybeShowOsWarning(screen);
@@ -1357,7 +1390,7 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
                         getSiteSettingsDelegate().getBrowserContextHandle(), contentType);
         int[] descriptionIds =
                 ContentSettingsResources.getTriStateSettingDescriptionIDs(contentType);
-        triStateToggle.initialize(setting, descriptionIds);
+        triStateToggle.initialize(contentType, setting, descriptionIds);
     }
 
     private void configureBinaryToggle(ChromeSwitchPreference binaryToggle, int contentType) {
@@ -1480,6 +1513,14 @@ public class SingleCategorySettings extends BaseSiteSettingsFragment
 
         @ContentSettingValues
         Integer value = site.getContentSetting(browserContextHandle, contentSettingsType);
+
+        AlertDialog.Builder alertDialogBuilder =
+            BromiteCustomContentSettingImpl.buildPreferenceDialog(site, contentSettingsType,
+                browserContextHandle, getContext(),
+                (dialog, which) -> { getInfoForOrigins(); });
+        if (alertDialogBuilder != null) {
+            return alertDialogBuilder.create();
+        }
 
         AlertDialog alertDialog =
                 new AlertDialog.Builder(getContext(), R.style.ThemeOverlay_BrowserUI_AlertDialog)
