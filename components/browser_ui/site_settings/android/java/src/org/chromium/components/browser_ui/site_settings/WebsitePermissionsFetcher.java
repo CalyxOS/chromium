25196e06c4636592ad7c58605f6b3e8d0875de76
uazo <uazo@users.noreply.github.com>
2022-02-24 07:54:36 +0000

    Content settings infrastructure
    This patch is used by other patches to provide the UI for Bromite-specific
    site settings.
    
    See BromiteCustomContentSetting_README.md for more information.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: bromite-build-utils.patch
    
    Change-Id: I246d2dc343ebe5150e7eb6ac0988cce0f3bf97e7

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePermissionsFetcher.java
@@ -108,7 +108,7 @@ public class WebsitePermissionsFetcher {
             case ContentSettingsType.USB_GUARD:
                 return WebsitePermissionsType.CHOSEN_OBJECT_INFO;
             default:
-                return null;
+                return BromiteCustomContentSettingImpl.getPermissionsType(contentSettingsType);
         }
     }
 
@@ -220,7 +220,7 @@ public class WebsitePermissionsFetcher {
             addFetcherForStorage(queue);
             queue.add(new CookiesInfoFetcher());
             for (@ContentSettingsType.EnumType int type = 0;
-                    type < ContentSettingsType.NUM_TYPES;
+                    type < ContentSettingsType.NUM_TYPES_BROMITE;
                     type++) {
                 addFetcherForContentSettingsType(queue, type);
             }
