222b7babfafb4968ffc3ef4d2f6aeeddc1401046
uazo <uazo@users.noreply.github.com>
2022-02-24 07:54:36 +0000

    Content settings infrastructure
    This patch is used by other patches to provide the UI for Bromite-specific
    site settings.
    
    See BromiteCustomContentSetting_README.md for more information.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: bromite-build-utils.patch
    
    Change-Id: I9da886e6251e7b3b9fdfb71b091039e8da427c59

--- components/content_settings/core/common/content_settings_types.h
+++ components/content_settings/core/common/content_settings_types.h
@@ -356,7 +356,9 @@ enum class ContentSettingsType : int32_t {
   // `net::features::kTpcdMetadataGrants` is enabled.
   TPCD_METADATA_GRANTS,
 
-  NUM_TYPES,
+  NUM_TYPES_CHROMIUM,
+#include "components/content_settings/core/common/bromite_content_settings.inc"
+  NUM_TYPES_BROMITE,
 };
 
 struct ContentSettingsTypeHash {
