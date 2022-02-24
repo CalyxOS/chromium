cdf1110c66ae13b5efa1f61f57f2a56bb850346a
uazo <uazo@users.noreply.github.com>
2022-02-24 07:54:36 +0000

    Content settings infrastructure
    This patch is used by other patches to provide the UI for Bromite-specific
    site settings.
    
    See BromiteCustomContentSetting_README.md for more information.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: bromite-build-utils.patch
    
    Change-Id: I246d2dc343ebe5150e7eb6ac0988cce0f3bf97e7

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsDelegate.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsDelegate.java
@@ -174,6 +174,8 @@ public interface SiteSettingsDelegate {
      */
     String getFirstPartySetOwner(String memberOrigin);
 
+    void launchHelpAndFeedbackActivity(Activity currentActivity, String url);
+
     /**
      * Returns whether the current implementation of the delegate is able to launch the Clear
      * Browsing Data dialog in Settings.
