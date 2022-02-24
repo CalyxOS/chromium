8fa907facc9345df7d99b1c8f0cc06af095afce8
uazo <uazo@users.noreply.github.com>
2022-02-24 07:54:36 +0000

    Content settings infrastructure
    This patch is used by other patches to provide the UI for Bromite-specific
    site settings.
    
    See BromiteCustomContentSetting_README.md for more information.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: bromite-build-utils.patch
    
    Change-Id: I246d2dc343ebe5150e7eb6ac0988cce0f3bf97e7

--- components/page_info/page_info_ui.cc
+++ components/page_info/page_info_ui.cc
@@ -18,6 +18,7 @@
 #include "build/build_config.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/content_settings/core/browser/website_settings_registry.h"
 #include "components/page_info/core/features.h"
 #include "components/page_info/page_info.h"
 #include "components/page_info/page_info_ui_delegate.h"
@@ -330,6 +331,13 @@ void CreateOppositeToDefaultSiteException(
 std::u16string GetPermissionAskStateString(ContentSettingsType type) {
   int message_id = kInvalidResourceID;
 
+  const content_settings::WebsiteSettingsInfo* settingInfo =
+    content_settings::WebsiteSettingsRegistry::GetInstance()->Get(type);
+  if (settingInfo && settingInfo->show_into_info_page()) {
+    if (settingInfo->ask_ui() != 0)
+      return l10n_util::GetStringUTF16(settingInfo->ask_ui());
+  }
+
   switch (type) {
     case ContentSettingsType::GEOLOCATION:
       message_id = IDS_PAGE_INFO_STATE_TEXT_LOCATION_ASK;
@@ -626,6 +634,12 @@ PageInfoUI::~PageInfoUI() = default;
 
 // static
 std::u16string PageInfoUI::PermissionTypeToUIString(ContentSettingsType type) {
+  const content_settings::WebsiteSettingsInfo* settingInfo =
+    content_settings::WebsiteSettingsRegistry::GetInstance()->Get(type);
+  if (settingInfo && settingInfo->show_into_info_page()) {
+    if (settingInfo->title_ui() != 0)
+      return l10n_util::GetStringUTF16(settingInfo->title_ui());
+  }
   for (const PermissionUIInfo& info : GetContentSettingsUIInfo()) {
     if (info.type == type)
       return l10n_util::GetStringUTF16(info.string_id);
@@ -636,6 +650,12 @@ std::u16string PageInfoUI::PermissionTypeToUIString(ContentSettingsType type) {
 // static
 std::u16string PageInfoUI::PermissionTypeToUIStringMidSentence(
     ContentSettingsType type) {
+  const content_settings::WebsiteSettingsInfo* settingInfo =
+    content_settings::WebsiteSettingsRegistry::GetInstance()->Get(type);
+  if (settingInfo && settingInfo->show_into_info_page()) {
+    if (settingInfo->mid_sentence_ui() != 0)
+      return l10n_util::GetStringUTF16(settingInfo->mid_sentence_ui());
+  }
   for (const PermissionUIInfo& info : GetContentSettingsUIInfo()) {
     if (info.type == type)
       return l10n_util::GetStringUTF16(info.string_id_mid_sentence);
@@ -1076,6 +1096,11 @@ bool PageInfoUI::ContentSettingsTypeInPageInfo(ContentSettingsType type) {
   for (const PermissionUIInfo& info : GetContentSettingsUIInfo()) {
     if (info.type == type)
       return true;
+  }
+  const content_settings::WebsiteSettingsInfo* settingInfo =
+    content_settings::WebsiteSettingsRegistry::GetInstance()->Get(type);
+  if (settingInfo) {
+    return settingInfo->show_into_info_page();
   }
   return false;
 }
