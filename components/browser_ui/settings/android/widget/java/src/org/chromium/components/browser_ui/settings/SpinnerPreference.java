345800e69dd0c8e49fc207ed076af1172d1d76f1
uazo <uazo@users.noreply.github.com>
2022-02-24 07:54:36 +0000

    Content settings infrastructure
    This patch is used by other patches to provide the UI for Bromite-specific
    site settings.
    
    See BromiteCustomContentSetting_README.md for more information.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: bromite-build-utils.patch
    
    Change-Id: I246d2dc343ebe5150e7eb6ac0988cce0f3bf97e7

--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/SpinnerPreference.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/SpinnerPreference.java
@@ -22,6 +22,7 @@ public class SpinnerPreference extends Preference {
     private ArrayAdapter<Object> mAdapter;
     private int mSelectedIndex;
     private final boolean mSingleLine;
+    private boolean mIsWidget = false;
 
     /** Constructor for inflating from XML. */
     public SpinnerPreference(Context context, AttributeSet attrs) {
@@ -34,6 +35,19 @@ public class SpinnerPreference extends Preference {
         } else {
             setLayoutResource(R.layout.preference_spinner);
         }
+    }
+
+    public SpinnerPreference(Context context, boolean singleLine) {
+        super(context, null);
+        mSingleLine = singleLine;
+        mIsWidget = true;
+        setLayoutResource(R.layout.preference_spinner_single_widget);
+    }
+
+    @Override
+    protected void onClick() {
+        if (mIsWidget)
+            mSpinner.performClick();
     }
 
     /**
@@ -53,6 +67,12 @@ public class SpinnerPreference extends Preference {
         mAdapter = new ArrayAdapter<>(getContext(), itemLayout, options);
         mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSelectedIndex = selectedIndex;
+    }
+
+    public void setSelectedIndex(int selectedIndex) {
+        mSelectedIndex = selectedIndex;
+        if (mSpinner != null)
+            mSpinner.setSelection(mSelectedIndex);
     }
 
     /** Returns the Spinner instance for introspection during tests. */
@@ -87,6 +107,10 @@ public class SpinnerPreference extends Preference {
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
 
+        if (mIsWidget) {
+            holder.setDividerAllowedAbove(true);
+            holder.setDividerAllowedBelow(true);
+        }
         ((TextView) holder.findViewById(R.id.title)).setText(getTitle());
         mSpinner = (Spinner) holder.findViewById(R.id.spinner);
         mSpinner.setOnItemSelectedListener(
