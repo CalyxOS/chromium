bfef8eede3c9a3b4eed54eb53dd05b722dd79988
uazo <uazo@users.noreply.github.com>
2022-02-24 07:54:36 +0000

    Content settings infrastructure
    This patch is used by other patches to provide the UI for Bromite-specific
    site settings.
    
    See BromiteCustomContentSetting_README.md for more information.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Require: bromite-build-utils.patch
    
    Change-Id: I246d2dc343ebe5150e7eb6ac0988cce0f3bf97e7

--- chrome/browser/ui/webui/settings/site_settings_helper.cc
+++ chrome/browser/ui/webui/settings/site_settings_helper.cc
@@ -44,6 +44,8 @@
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/content_settings/core/browser/website_settings_info.h"
+#include "components/content_settings/core/browser/website_settings_registry.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
@@ -224,13 +226,13 @@ const ContentSettingsTypeNameEntry kContentSettingsTypeGroupNames[] = {
     {ContentSettingsType::TOP_LEVEL_TPCD_ORIGIN_TRIAL, nullptr},
 };
 
-static_assert(
-    std::size(kContentSettingsTypeGroupNames) ==
-        // Add one since the sequence is kMinValue = -1, 0, ..., kMaxValue
-        1 + static_cast<int32_t>(ContentSettingsType::kMaxValue) -
-            static_cast<int32_t>(ContentSettingsType::kMinValue),
-    "kContentSettingsTypeGroupNames should have the correct number "
-    "of elements");
+// static_assert(
+//     std::size(kContentSettingsTypeGroupNames) ==
+//         // Add one since the sequence is kMinValue = -1, 0, ..., kMaxValue
+//         1 + static_cast<int32_t>(ContentSettingsType::kMaxValue) -
+//             static_cast<int32_t>(ContentSettingsType::kMinValue),
+//     "kContentSettingsTypeGroupNames should have the correct number "
+//     "of elements");
 
 struct SiteSettingSourceStringMapping {
   SiteSettingSource source;
@@ -480,6 +482,13 @@ bool HasRegisteredGroupName(ContentSettingsType type) {
       return true;
     }
   }
+  content_settings::WebsiteSettingsRegistry* website_settings =
+      content_settings::WebsiteSettingsRegistry::GetInstance();
+  for (const content_settings::WebsiteSettingsInfo* cs : *website_settings) {
+    if (type == cs->type() && cs->desktop_ui()) {
+      return true;
+    }
+  }
   return false;
 }
 
@@ -493,11 +502,24 @@ ContentSettingsType ContentSettingsTypeFromGroupName(base::StringPiece name) {
       return entry.type;
     }
   }
-
+  content_settings::WebsiteSettingsRegistry* website_settings =
+      content_settings::WebsiteSettingsRegistry::GetInstance();
+  for (const content_settings::WebsiteSettingsInfo* cs : *website_settings) {
+    if (name == cs->name() && cs->desktop_ui()) {
+      return cs->type();
+    }
+  }
   return ContentSettingsType::DEFAULT;
 }
 
 base::StringPiece ContentSettingsTypeToGroupName(ContentSettingsType type) {
+  content_settings::WebsiteSettingsRegistry* website_settings =
+      content_settings::WebsiteSettingsRegistry::GetInstance();
+  for (const content_settings::WebsiteSettingsInfo* cs : *website_settings) {
+    if (type == cs->type() && cs->desktop_ui()) {
+      return cs->name();
+    }
+  }
   for (const auto& entry : kContentSettingsTypeGroupNames) {
     if (type == entry.type) {
       // Content setting types that aren't represented in the settings UI
@@ -512,7 +534,6 @@ base::StringPiece ContentSettingsTypeToGroupName(ContentSettingsType type) {
       return entry.name ? entry.name : base::StringPiece();
     }
   }
-
   NOTREACHED() << static_cast<int32_t>(type)
                << " is not a recognized content settings type.";
   return base::StringPiece();
@@ -610,6 +631,13 @@ std::vector<ContentSettingsType> GetVisiblePermissionCategories(
       base_types->push_back(ContentSettingsType::POINTER_LOCK);
     }
 
+    content_settings::WebsiteSettingsRegistry* website_settings =
+        content_settings::WebsiteSettingsRegistry::GetInstance();
+    for (const content_settings::WebsiteSettingsInfo* cs : *website_settings) {
+      if (cs->desktop_ui()) {
+        base_types->push_back(cs->type());
+      }
+    }
     initialized = true;
   }
 
