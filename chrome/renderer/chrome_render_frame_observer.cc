1de73878c9b842e0e68c6fe07e35b905860df505
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-03-06 18:55:58 +0100

    OpenSearch: miscellaneous
    Fix upstream bug with recently added engines prematurely discarded
    because they have no last-visit timestamp
    Fix upstream bug with visited engines visit time not updated
    Allow adding search engines in incognito mode
    Allow using search engine URLs with non-empty paths
    Add verbose logging
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I713872f2e56beb9ca412b7f6e5249832bdc497bc

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -14,6 +14,7 @@
 
 #include "base/command_line.h"
 #include "base/functional/bind.h"
+#include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
@@ -254,6 +255,7 @@ void ChromeRenderFrameObserver::DidFinishLoad() {
 
   GURL osdd_url = frame->GetDocument().OpenSearchDescriptionURL();
   if (!osdd_url.is_empty()) {
+    DLOG(INFO) << "OpenSearch: found OSDD URL: " << osdd_url;
     mojo::Remote<chrome::mojom::OpenSearchDescriptionDocumentHandler>
         osdd_handler;
     render_frame()->GetBrowserInterfaceBroker().GetInterface(
