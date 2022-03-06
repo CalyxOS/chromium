bb7c412d76994807f0ce7c7d39141d731dd06b35
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-03-06 18:55:58 +0100

    OpenSearch: miscellaneous
    Fix upstream bug with recently added engines prematurely discarded
    because they have no last-visit timestamp
    Fix upstream bug with visited engines visit time not updated
    Allow adding search engines in incognito mode
    Allow using search engine URLs with non-empty paths
    Add verbose logging
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I713872f2e56beb9ca412b7f6e5249832bdc497bc

--- components/search_engines/template_url_service.h
+++ components/search_engines/template_url_service.h
@@ -354,7 +354,10 @@ class TemplateURLService final : public WebDataServiceConsumer,
   void UpdateProviderFavicons(const GURL& potential_search_url,
                               const GURL& favicon_url);
 
-  // Return true if the given |url| can be made the default. This returns false
+   // Updates the last_visited time of |url| to the current time.
+  void UpdateTemplateURLVisitTime(TemplateURL* url);
+
+ // Return true if the given |url| can be made the default. This returns false
   // regardless of |url| if the default search provider is managed by policy or
   // controlled by an extension.
   bool CanMakeDefault(const TemplateURL* url) const;
@@ -762,9 +765,6 @@ class TemplateURLService final : public WebDataServiceConsumer,
   // For each TemplateURL whose url matches the visited url
   // SetKeywordSearchTermsForURL is invoked.
   void UpdateKeywordSearchTermsForURL(const URLVisitedDetails& details);
-
-  // Updates the last_visited time of |url| to the current time.
-  void UpdateTemplateURLVisitTime(TemplateURL* url);
 
   // If necessary, generates a visit for the site http:// + t_url.keyword().
   void AddTabToSearchVisit(const TemplateURL& t_url);
