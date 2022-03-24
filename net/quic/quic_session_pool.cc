70a8ea7007f98b9581c90f47fd06e1475deca11b
uazo <uazo@users.noreply.github.com>
2022-03-24 10:08:00 +0000

    Disable TLS resumption
    Disable resumption feature for all HTTPS and QUIC connections;
    the feature could be used to track users even without cookies.
    
    Sessions are not currently saved to disk in Chromium (although
    there is support for it) but are long enough to constitute a
    privacy risk (2h for TLS 1.2 and 7 days for TLS 1.3) if user
    does not frequently close the browser.
    
    Since session information is not kept in the HTTP cache it is
    not cleared when deleting navigation data (although it is possible
    to clear it by selecting "passwords").
    
    Two new user configurable flags are introduced:
    * kDisableTLSResumption, active by default
    * kLogTLSResumption, that would allow to find in logcat reused
      sessions in lines matching "SSL Log:"
    
    See also:
    * https://arxiv.org/abs/1810.07304
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I9a0ef1a3827d0d3d06303e759c294c20fc7b791c

--- net/quic/quic_session_pool.cc
+++ net/quic/quic_session_pool.cc
@@ -78,6 +78,7 @@
 #include "net/third_party/quiche/src/quiche/quic/platform/api/quic_flags.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "third_party/boringssl/src/include/openssl/aead.h"
+#include "third_party/boringssl/src/include/openssl/ssl.h"
 #include "url/gurl.h"
 #include "url/scheme_host_port.h"
 #include "url/url_constants.h"
@@ -194,6 +195,38 @@ void LogUsingExistingSession(const NetLogWithSource& request_net_log,
 }
 
 }  // namespace
+
+class BromiteSessionCache : public quic::QuicClientSessionCache {
+ public:
+  BromiteSessionCache() = default;
+  ~BromiteSessionCache() override = default;
+
+  void Insert(const quic::QuicServerId& server_id,
+              bssl::UniquePtr<SSL_SESSION> session,
+              const quic::TransportParameters& params,
+              const quic::ApplicationState* application_state) override {
+    if (base::FeatureList::IsEnabled(net::features::kDisableTLSResumption))
+      SSL_SESSION_set_timeout(session.get(), 0);
+    if (base::FeatureList::IsEnabled(net::features::kLogTLSResumption)) {
+      LOG(INFO) << "SSL Log: new quic session created "
+                << server_id.host();
+    }
+    quic::QuicClientSessionCache::Insert(server_id,
+      std::move(session), params, application_state);
+  }
+
+  std::unique_ptr<quic::QuicResumptionState> Lookup(
+        const quic::QuicServerId& server_id, quic::QuicWallTime now,
+        const SSL_CTX* ctx) override {
+    auto value = quic::QuicClientSessionCache::Lookup(server_id, now, ctx);
+    if (value != nullptr &&
+        base::FeatureList::IsEnabled(net::features::kLogTLSResumption)) {
+      LOG(INFO) << "SSL Log: QUIC session resumed "
+                << server_id.host();
+    }
+    return value;
+  }
+};
 
 QuicSessionRequest::QuicSessionRequest(QuicSessionPool* pool) : pool_(pool) {}
 
@@ -1881,7 +1914,7 @@ QuicSessionPool::CreateCryptoConfigHandle(
               cert_verifier_, transport_security_state_, sct_auditing_delegate_,
               HostsFromOrigins(params_.origins_to_force_quic_on),
               actual_network_anonymization_key),
-          std::make_unique<quic::QuicClientSessionCache>(), this);
+          std::make_unique<BromiteSessionCache>(), this);
 
   quic::QuicCryptoClientConfig* crypto_config = crypto_config_owner->config();
   crypto_config->AddCanonicalSuffix(".c.youtube.com");
