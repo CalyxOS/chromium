0869c76580444f39cbd01b82655e73051e653ea7
uazo <uazo@users.noreply.github.com>
2022-03-24 10:08:00 +0000

    Disable TLS resumption
    Disable resumption feature for all HTTPS and QUIC connections;
    the feature could be used to track users even without cookies.
    
    Sessions are not currently saved to disk in Chromium (although
    there is support for it) but are long enough to constitute a
    privacy risk (2h for TLS 1.2 and 7 days for TLS 1.3) if user
    does not frequently close the browser.
    
    Since session information is not kept in the HTTP cache it is
    not cleared when deleting navigation data (although it is possible
    to clear it by selecting "passwords").
    
    Two new user configurable flags are introduced:
    * kDisableTLSResumption, active by default
    * kLogTLSResumption, that would allow to find in logcat reused
      sessions in lines matching "SSL Log:"
    
    See also:
    * https://arxiv.org/abs/1810.07304
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- net/base/features.cc
+++ net/base/features.cc
@@ -61,6 +61,12 @@ const base::FeatureParam<base::TimeDelta> kUseDnsHttpsSvcbSecureExtraTimeMin{
     &kUseDnsHttpsSvcb, "UseDnsHttpsSvcbSecureExtraTimeMin",
     base::Milliseconds(5)};
 
+const base::Feature kDisableTLSResumption{"DisableTLSResumption",
+                                          base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kLogTLSResumption{"LogTLSResumption",
+                                      base::FEATURE_DISABLED_BY_DEFAULT};
+
 BASE_FEATURE(kUseDnsHttpsSvcbAlpn,
              "UseDnsHttpsSvcbAlpn",
              base::FEATURE_DISABLED_BY_DEFAULT);
