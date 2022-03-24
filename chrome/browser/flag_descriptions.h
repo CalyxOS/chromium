32ec63c0502ed03c2294f4213b665587a75f873f
uazo <uazo@users.noreply.github.com>
2022-03-24 10:08:00 +0000

    Disable TLS resumption
    Disable resumption feature for all HTTPS and QUIC connections;
    the feature could be used to track users even without cookies.
    
    Sessions are not currently saved to disk in Chromium (although
    there is support for it) but are long enough to constitute a
    privacy risk (2h for TLS 1.2 and 7 days for TLS 1.3) if user
    does not frequently close the browser.
    
    Since session information is not kept in the HTTP cache it is
    not cleared when deleting navigation data (although it is possible
    to clear it by selecting "passwords").
    
    Two new user configurable flags are introduced:
    * kDisableTLSResumption, active by default
    * kLogTLSResumption, that would allow to find in logcat reused
      sessions in lines matching "SSL Log:"
    
    See also:
    * https://arxiv.org/abs/1810.07304
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -574,6 +574,12 @@ extern const char kEnablePreinstalledWebAppDuplicationFixerDescription[];
 extern const char kEnableTLS13EarlyDataName[];
 extern const char kEnableTLS13EarlyDataDescription[];
 
+extern const char kDisableTLSResumptionName[];
+extern const char kDisableTLSResumptionDescription[];
+
+extern const char kLogTLSResumptionName[];
+extern const char kLogTLSResumptionDescription[];
+
 extern const char kEnhancedNetworkVoicesName[];
 extern const char kEnhancedNetworkVoicesDescription[];
 
