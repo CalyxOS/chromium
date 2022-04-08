12062a4230ea36750a2293a81ca7c58d3fd09ff5
uazo <uazo@users.noreply.github.com>
2022-04-08 11:04:04 +0000

    Add lifetime options for permissions
    Indicate the session mode for content-settings by using the constraint `content_settings::SessionModel` as
    UserSession when setting the value, and also make use of an expiration time value.
    This is used in Chromium for `ClientHints` but it is generally possible to use this functionality when a
    specific value needs to be persisted by origin.
    All content settings of this type are not saved on disk (except for the `Forever` option), allowing user to
    reset the status each time application is restarted.
    
    There are 4 main areas affected to introduce the functionality:
    * components/content_settings
      A new `content_settings::LifetimeMode` enum value is defined to specify the user's
      choice (Always, OnlyThisTime, UntilOriginClosed, UntilBrowserClosed).
      Enumeration is also generated for java by adding it in `content_settings_enums_javagen` (gn).
      This is mainly used in `content_settings_utils.cc` to create a specialised `content_settings::ContentSettingConstraints`
      that is then used in `SetContentSettingDefaultScope()` by `PermissionContextBase::UpdateContentSetting`.
      Existing Chromium data structures do not provide a specific property to define a choice which is instead encoded through
      the `ContentSettingConstraints`; this approach is already used in other parts of the Chromium codebase so it is not
      novel here.
      Therefore, `content_settings::GetConstraintSessionExpiration()` and `content_settings::IsConstraintSessionExpiration()`
      manage the lifetime modes of the session content-settings.
      The modificaiton also adds the session pattern to the ContentSettingPatternSource so that it is available for the UI.
    * components/permissions
      Lifetime support is added to the permissions; most of the changes are caused by the fact that it is necessary to report
      the value selected by the user from the Java UI managed by `components/browser_ui` up to
      `PermissionContextBase::UpdateContentSetting()`,   without necessarily having to modify all requests that are not
      related to geolocation/camera/microphone. The approach used is a new
      `PermissionRequest::PermissionDecidedCallbackWithLifetime` used by an overload of
      `PermissionContextBase::CreatePermissionRequest` so that options are present only for the specific content-settings
      (see `PermissionDialogModel.java`).
      For other permissions no behaviour is changed (see `PermissionDialogDelegate::Accept`); for geolocation it was
      necessary to act directly in the specific context, because, unlike microphone/camera, the content-setting value is
      inserted in its specific method (`FinishNotifyPermissionSet`, that calls the callback), even if the class always
      derives from `PermissionContextBase`.
    * components/page_info
      Some changes needed to see in the summary of the `page_info` the text "(only this session)"
      (aka `page_info_android_permission_session_permission`) through adding a new property "is_user_session" in
      `PageInfoPermissionEntry` (Java).
    * components/browser_ui
      Changes to the Settings UI to show "(only this session)" in the specific content-setting.
      The same view is used both in the settings and in the page_info.
    
    For the management of `UntilOriginClosed` the logic used by flag `kOneTimeGeolocationPermission` was used; this flag
    is active only in the desktop (files `last_tab_standing_tracker_*`). It is a class that manages a list of the active
    origins and allows to perform operations when all the tabs relating to that origin have been closed, in this case
    deleting the session content settings of `UntilOriginClosed`.
    
    See also: https://github.com/bromite/bromite/issues/1549
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Idebdd2b882432fcc3704fba2f4a9da3c91b3497f

--- components/permissions/android/permission_prompt/permission_dialog_delegate.cc
+++ components/permissions/android/permission_prompt/permission_dialog_delegate.cc
@@ -150,6 +150,11 @@ void PermissionDialogJavaDelegate::DismissDialog() {
   Java_PermissionDialogDelegate_dismissFromNative(env, j_delegate_);
 }
 
+int PermissionDialogJavaDelegate::GetSelectedLifetimeOption() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  return Java_PermissionDialogDelegate_getSelectedLifetimeOption(env, j_delegate_);
+}
+
 // static
 void PermissionDialogDelegate::Create(
     content::WebContents* web_contents,
@@ -178,6 +183,13 @@ PermissionDialogDelegate* PermissionDialogDelegate::CreateForTesting(
 void PermissionDialogDelegate::Accept(JNIEnv* env,
                                       const JavaParamRef<jobject>& obj) {
   CHECK(permission_prompt_);
+  content_settings::mojom::LifetimeMode lifetimeOption =
+    static_cast<content_settings::mojom::LifetimeMode>(
+      java_delegate_->GetSelectedLifetimeOption());
+  if (lifetimeOption != content_settings::mojom::LifetimeMode::ALWAYS) {
+    permission_prompt_->AcceptThisTime(lifetimeOption);
+    return;
+  }
   permission_prompt_->Accept();
 }
 
@@ -185,12 +197,22 @@ void PermissionDialogDelegate::AcceptThisTime(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj) {
   CHECK(permission_prompt_);
-  permission_prompt_->AcceptThisTime();
+  content_settings::mojom::LifetimeMode lifetimeOption =
+    static_cast<content_settings::mojom::LifetimeMode>(
+      java_delegate_->GetSelectedLifetimeOption());
+  permission_prompt_->AcceptThisTime(lifetimeOption);
 }
 
 void PermissionDialogDelegate::Cancel(JNIEnv* env,
                                       const JavaParamRef<jobject>& obj) {
   CHECK(permission_prompt_);
+  content_settings::mojom::LifetimeMode lifetimeOption =
+    static_cast<content_settings::mojom::LifetimeMode>(
+      java_delegate_->GetSelectedLifetimeOption());
+  if (lifetimeOption != content_settings::mojom::LifetimeMode::ALWAYS) {
+    permission_prompt_->DenyThisTime(lifetimeOption);
+    return;
+  }
   permission_prompt_->Deny();
 }
 
