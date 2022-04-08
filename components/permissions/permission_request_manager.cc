b67156e405ad64d45bf021cedac2bba7301a4845
uazo <uazo@users.noreply.github.com>
2022-04-08 11:04:04 +0000

    Add lifetime options for permissions
    Indicate the session mode for content-settings by using the constraint `content_settings::SessionModel` as
    UserSession when setting the value, and also make use of an expiration time value.
    This is used in Chromium for `ClientHints` but it is generally possible to use this functionality when a
    specific value needs to be persisted by origin.
    All content settings of this type are not saved on disk (except for the `Forever` option), allowing user to
    reset the status each time application is restarted.
    
    There are 4 main areas affected to introduce the functionality:
    * components/content_settings
      A new `content_settings::LifetimeMode` enum value is defined to specify the user's
      choice (Always, OnlyThisTime, UntilOriginClosed, UntilBrowserClosed).
      Enumeration is also generated for java by adding it in `content_settings_enums_javagen` (gn).
      This is mainly used in `content_settings_utils.cc` to create a specialised `content_settings::ContentSettingConstraints`
      that is then used in `SetContentSettingDefaultScope()` by `PermissionContextBase::UpdateContentSetting`.
      Existing Chromium data structures do not provide a specific property to define a choice which is instead encoded through
      the `ContentSettingConstraints`; this approach is already used in other parts of the Chromium codebase so it is not
      novel here.
      Therefore, `content_settings::GetConstraintSessionExpiration()` and `content_settings::IsConstraintSessionExpiration()`
      manage the lifetime modes of the session content-settings.
      The modificaiton also adds the session pattern to the ContentSettingPatternSource so that it is available for the UI.
    * components/permissions
      Lifetime support is added to the permissions; most of the changes are caused by the fact that it is necessary to report
      the value selected by the user from the Java UI managed by `components/browser_ui` up to
      `PermissionContextBase::UpdateContentSetting()`,   without necessarily having to modify all requests that are not
      related to geolocation/camera/microphone. The approach used is a new
      `PermissionRequest::PermissionDecidedCallbackWithLifetime` used by an overload of
      `PermissionContextBase::CreatePermissionRequest` so that options are present only for the specific content-settings
      (see `PermissionDialogModel.java`).
      For other permissions no behaviour is changed (see `PermissionDialogDelegate::Accept`); for geolocation it was
      necessary to act directly in the specific context, because, unlike microphone/camera, the content-setting value is
      inserted in its specific method (`FinishNotifyPermissionSet`, that calls the callback), even if the class always
      derives from `PermissionContextBase`.
    * components/page_info
      Some changes needed to see in the summary of the `page_info` the text "(only this session)"
      (aka `page_info_android_permission_session_permission`) through adding a new property "is_user_session" in
      `PageInfoPermissionEntry` (Java).
    * components/browser_ui
      Changes to the Settings UI to show "(only this session)" in the specific content-setting.
      The same view is used both in the settings and in the page_info.
    
    For the management of `UntilOriginClosed` the logic used by flag `kOneTimeGeolocationPermission` was used; this flag
    is active only in the desktop (files `last_tab_standing_tracker_*`). It is a class that manages a list of the active
    origins and allows to perform operations when all the tabs relating to that origin have been closed, in this case
    deleting the session content settings of `UntilOriginClosed`.
    
    See also: https://github.com/bromite/bromite/issues/1549
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ic8a115d5b45b5768bec1f2fda45a8bf19da96e48

--- components/permissions/permission_request_manager.cc
+++ components/permissions/permission_request_manager.cc
@@ -155,7 +155,7 @@ void PermissionRequestManager::AddRequest(
 
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDenyPermissionPrompts)) {
-    request->PermissionDenied();
+    request->PermissionDenied(/*is_one_time*/false, content_settings::LifetimeMode::Always);
     request->RequestFinished();
     return;
   }
@@ -231,7 +231,7 @@ void PermissionRequestManager::AddRequest(
   if (auto_approval_origin) {
     if (url::Origin::Create(request->requesting_origin()) ==
         auto_approval_origin.value()) {
-      request->PermissionGranted(/*is_one_time=*/false);
+      request->PermissionGranted(/*is_one_time=*/false, content_settings::LifetimeMode::Always);
     }
     request->RequestFinished();
     return;
@@ -574,7 +574,8 @@ void PermissionRequestManager::Accept() {
                                 (*requests_iter)->request_type(),
                                 PermissionAction::GRANTED);
     PermissionGrantedIncludingDuplicates(*requests_iter,
-                                         /*is_one_time=*/false);
+                                         /*is_one_time=*/false,
+                                         content_settings::LifetimeMode::Always);
 
 #if !BUILDFLAG(IS_ANDROID)
     absl::optional<ContentSettingsType> content_settings_type =
@@ -592,7 +593,7 @@ void PermissionRequestManager::Accept() {
   CurrentRequestsDecided(PermissionAction::GRANTED);
 }
 
-void PermissionRequestManager::AcceptThisTime() {
+void PermissionRequestManager::AcceptThisTime(content_settings::LifetimeMode mode) {
   if (ignore_callbacks_from_prompt_)
     return;
   DCHECK(view_);
@@ -603,7 +604,8 @@ void PermissionRequestManager::AcceptThisTime() {
                                 (*requests_iter)->request_type(),
                                 PermissionAction::GRANTED_ONCE);
     PermissionGrantedIncludingDuplicates(*requests_iter,
-                                         /*is_one_time=*/true);
+                                         /*is_one_time=*/true,
+                                         mode);
   }
 
   NotifyRequestDecided(PermissionAction::GRANTED_ONCE);
@@ -611,6 +613,15 @@ void PermissionRequestManager::AcceptThisTime() {
 }
 
 void PermissionRequestManager::Deny() {
+  Deny_(/*is_one_time*/ false, content_settings::LifetimeMode::Always);
+}
+
+void PermissionRequestManager::DenyThisTime(content_settings::LifetimeMode mode) {
+  Deny_(/*is_one_time*/ true, mode);
+}
+
+void PermissionRequestManager::Deny_(bool is_one_time,
+                                     content_settings::LifetimeMode lifetime_option) {
   if (ignore_callbacks_from_prompt_)
     return;
   DCHECK(view_);
@@ -633,7 +644,7 @@ void PermissionRequestManager::Deny() {
     StorePermissionActionForUMA((*requests_iter)->requesting_origin(),
                                 (*requests_iter)->request_type(),
                                 PermissionAction::DENIED);
-    PermissionDeniedIncludingDuplicates(*requests_iter);
+    PermissionDeniedIncludingDuplicates(*requests_iter, is_one_time, lifetime_option);
   }
 
   NotifyRequestDecided(PermissionAction::DENIED);
@@ -1190,32 +1201,32 @@ PermissionRequestManager::VisitDuplicateRequests(
 
 void PermissionRequestManager::PermissionGrantedIncludingDuplicates(
     PermissionRequest* request,
-    bool is_one_time) {
+    bool is_one_time, content_settings::LifetimeMode lifetime_option) {
   DCHECK_EQ(1ul, base::ranges::count(requests_, request) +
                      pending_permission_requests_.Count(request))
       << "Only requests in [pending_permission_]requests_ can have duplicates";
-  request->PermissionGranted(is_one_time);
+  request->PermissionGranted(is_one_time, lifetime_option);
   VisitDuplicateRequests(
       base::BindRepeating(
-          [](bool is_one_time,
+          [](bool is_one_time, content_settings::LifetimeMode lifetime_option,
              const base::WeakPtr<PermissionRequest>& weak_request) {
-            weak_request->PermissionGranted(is_one_time);
+            weak_request->PermissionGranted(is_one_time, lifetime_option);
           },
-          is_one_time),
+          is_one_time, lifetime_option),
       request);
 }
 
 void PermissionRequestManager::PermissionDeniedIncludingDuplicates(
-    PermissionRequest* request) {
+    PermissionRequest* request, bool is_one_time, content_settings::LifetimeMode lifetime_option) {
   DCHECK_EQ(1ul, base::ranges::count(requests_, request) +
                      pending_permission_requests_.Count(request))
       << "Only requests in [pending_permission_]requests_ can have duplicates";
-  request->PermissionDenied();
+  request->PermissionDenied(is_one_time, lifetime_option);
   VisitDuplicateRequests(
       base::BindRepeating(
-          [](const base::WeakPtr<PermissionRequest>& weak_request) {
-            weak_request->PermissionDenied();
-          }),
+          [](bool is_one_time, content_settings::LifetimeMode lifetime_option, const base::WeakPtr<PermissionRequest>& weak_request) {
+            weak_request->PermissionDenied(is_one_time, lifetime_option);
+          }, is_one_time, lifetime_option),
       request);
 }
 
@@ -1467,7 +1478,7 @@ void PermissionRequestManager::LogWarningToConsole(const char* message) {
 void PermissionRequestManager::DoAutoResponseForTesting() {
   switch (auto_response_for_test_) {
     case ACCEPT_ONCE:
-      AcceptThisTime();
+      AcceptThisTime(content_settings::LifetimeMode::OnlyThisTime);
       break;
     case ACCEPT_ALL:
       Accept();
