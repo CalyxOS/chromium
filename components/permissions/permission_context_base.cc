4d3ee3a393ea2bcdabd0027e028f0a62022ea881
uazo <uazo@users.noreply.github.com>
2022-04-08 11:04:04 +0000

    Add lifetime options for permissions
    Indicate the session mode for content-settings by using the constraint `content_settings::SessionModel` as
    UserSession when setting the value, and also make use of an expiration time value.
    This is used in Chromium for `ClientHints` but it is generally possible to use this functionality when a
    specific value needs to be persisted by origin.
    All content settings of this type are not saved on disk (except for the `Forever` option), allowing user to
    reset the status each time application is restarted.
    
    There are 4 main areas affected to introduce the functionality:
    * components/content_settings
      A new `content_settings::LifetimeMode` enum value is defined to specify the user's
      choice (Always, OnlyThisTime, UntilOriginClosed, UntilBrowserClosed).
      Enumeration is also generated for java by adding it in `content_settings_enums_javagen` (gn).
      This is mainly used in `content_settings_utils.cc` to create a specialised `content_settings::ContentSettingConstraints`
      that is then used in `SetContentSettingDefaultScope()` by `PermissionContextBase::UpdateContentSetting`.
      Existing Chromium data structures do not provide a specific property to define a choice which is instead encoded through
      the `ContentSettingConstraints`; this approach is already used in other parts of the Chromium codebase so it is not
      novel here.
      Therefore, `content_settings::GetConstraintSessionExpiration()` and `content_settings::IsConstraintSessionExpiration()`
      manage the lifetime modes of the session content-settings.
      The modificaiton also adds the session pattern to the ContentSettingPatternSource so that it is available for the UI.
    * components/permissions
      Lifetime support is added to the permissions; most of the changes are caused by the fact that it is necessary to report
      the value selected by the user from the Java UI managed by `components/browser_ui` up to
      `PermissionContextBase::UpdateContentSetting()`,   without necessarily having to modify all requests that are not
      related to geolocation/camera/microphone. The approach used is a new
      `PermissionRequest::PermissionDecidedCallbackWithLifetime` used by an overload of
      `PermissionContextBase::CreatePermissionRequest` so that options are present only for the specific content-settings
      (see `PermissionDialogModel.java`).
      For other permissions no behaviour is changed (see `PermissionDialogDelegate::Accept`); for geolocation it was
      necessary to act directly in the specific context, because, unlike microphone/camera, the content-setting value is
      inserted in its specific method (`FinishNotifyPermissionSet`, that calls the callback), even if the class always
      derives from `PermissionContextBase`.
    * components/page_info
      Some changes needed to see in the summary of the `page_info` the text "(only this session)"
      (aka `page_info_android_permission_session_permission`) through adding a new property "is_user_session" in
      `PageInfoPermissionEntry` (Java).
    * components/browser_ui
      Changes to the Settings UI to show "(only this session)" in the specific content-setting.
      The same view is used both in the settings and in the page_info.
    
    For the management of `UntilOriginClosed` the logic used by flag `kOneTimeGeolocationPermission` was used; this flag
    is active only in the desktop (files `last_tab_standing_tracker_*`). It is a class that manages a list of the active
    origins and allows to perform operations when all the tabs relating to that origin have been closed, in this case
    deleting the session content settings of `UntilOriginClosed`.
    
    See also: https://github.com/bromite/bromite/issues/1549
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ic8a115d5b45b5768bec1f2fda45a8bf19da96e48

--- components/permissions/permission_context_base.cc
+++ components/permissions/permission_context_base.cc
@@ -248,6 +248,20 @@ PermissionContextBase::CreatePermissionRequest(
       std::move(delete_callback));
 }
 
+std::unique_ptr<PermissionRequest>
+PermissionContextBase::CreatePermissionRequest(
+    const GURL& request_origin,
+    ContentSettingsType content_settings_type,
+    bool has_gesture,
+    content::WebContents* web_contents,
+    PermissionRequest::PermissionDecidedCallbackWithLifetime permission_decided_callback,
+    base::OnceClosure delete_callback) const {
+  return std::make_unique<PermissionRequest>(
+      request_origin, ContentSettingsTypeToRequestType(content_settings_type),
+      has_gesture, std::move(permission_decided_callback),
+      std::move(delete_callback));
+}
+
 PermissionResult PermissionContextBase::GetPermissionStatus(
     content::RenderFrameHost* render_frame_host,
     const GURL& requesting_origin,
@@ -427,7 +441,8 @@ void PermissionContextBase::PermissionDecided(const PermissionRequestID& id,
                                               const GURL& embedding_origin,
                                               ContentSetting content_setting,
                                               bool is_one_time,
-                                              bool is_final_decision) {
+                                              bool is_final_decision,
+                                              content_settings::LifetimeMode lifetime_option) {
   DCHECK(content_setting == CONTENT_SETTING_ALLOW ||
          content_setting == CONTENT_SETTING_BLOCK ||
          content_setting == CONTENT_SETTING_DEFAULT);
@@ -442,13 +457,13 @@ void PermissionContextBase::PermissionDecided(const PermissionRequestID& id,
   // missing if a permission prompt was preignored and we already notified an
   // origin about it.
   if (request->second.second) {
-    NotifyPermissionSet(id, requesting_origin, embedding_origin,
+    NotifyPermissionSetWithLifetime(id, requesting_origin, embedding_origin,
                         std::move(request->second.second), persist,
-                        content_setting, is_one_time, is_final_decision);
+                        content_setting, is_one_time, is_final_decision, lifetime_option);
   } else {
-    NotifyPermissionSet(id, requesting_origin, embedding_origin,
+    NotifyPermissionSetWithLifetime(id, requesting_origin, embedding_origin,
                         base::DoNothing(), persist, content_setting,
-                        is_one_time, is_final_decision);
+                        is_one_time, is_final_decision, lifetime_option);
   }
 }
 
@@ -500,11 +515,27 @@ void PermissionContextBase::NotifyPermissionSet(
     ContentSetting content_setting,
     bool is_one_time,
     bool is_final_decision) {
+  DCHECK(is_one_time == false);
+  NotifyPermissionSetWithLifetime(id, requesting_origin, embedding_origin, std::move(callback),
+     persist, content_setting, is_one_time, is_final_decision,
+     content_settings::LifetimeMode::Always);
+}
+
+void PermissionContextBase::NotifyPermissionSetWithLifetime(
+    const PermissionRequestID& id,
+    const GURL& requesting_origin,
+    const GURL& embedding_origin,
+    BrowserPermissionCallback callback,
+    bool persist,
+    ContentSetting content_setting,
+    bool is_one_time,
+    bool is_final_decision,
+    content_settings::LifetimeMode lifetime_option) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   if (persist) {
     UpdateContentSetting(requesting_origin, embedding_origin, content_setting,
-                         is_one_time);
+                         is_one_time, lifetime_option);
   }
 
   if (is_final_decision) {
@@ -534,6 +565,15 @@ void PermissionContextBase::UpdateContentSetting(const GURL& requesting_origin,
                                                  const GURL& embedding_origin,
                                                  ContentSetting content_setting,
                                                  bool is_one_time) {
+  UpdateContentSetting(requesting_origin, embedding_origin, content_setting,
+    is_one_time, content_settings::LifetimeMode::Always);
+}
+
+void PermissionContextBase::UpdateContentSetting(const GURL& requesting_origin,
+                                                 const GURL& embedding_origin,
+                                                 ContentSetting content_setting,
+                                                 bool is_one_time,
+                                                 content_settings::LifetimeMode lifetime_option) {
   DCHECK_EQ(requesting_origin, requesting_origin.DeprecatedGetOriginAsURL());
   DCHECK_EQ(embedding_origin, embedding_origin.DeprecatedGetOriginAsURL());
   DCHECK(content_setting == CONTENT_SETTING_ALLOW ||
@@ -542,6 +582,8 @@ void PermissionContextBase::UpdateContentSetting(const GURL& requesting_origin,
   content_settings::ContentSettingConstraints constraints = {
       base::Time(), is_one_time ? content_settings::SessionModel::OneTime
                                 : content_settings::SessionModel::Durable};
+  if (is_one_time)
+    constraints = content_settings::GetConstraintSessionExpiration(lifetime_option);
 
 #if !BUILDFLAG(IS_ANDROID)
   if (base::FeatureList::IsEnabled(
