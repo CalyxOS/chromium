5f282d4d9507b4c198b9f683a0c636b5c4ff39d6
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-04-09 23:01:55 +0200

    Bookmarks select all menu entry
    Requires: Restore-BookmarkToolbar-setCurrentFolder.patch
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3e974f36f2312d147964b3e856421d45a03afc4b

--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbar.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbar.java
@@ -24,6 +24,7 @@ import org.chromium.components.browser_ui.widget.selectable_list.SelectionDelega
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.HashSet;
 
 /**
  * Main toolbar of bookmark UI. It is responsible for displaying title and buttons associated with
@@ -184,6 +185,11 @@ public class BookmarkToolbar extends SelectableListToolbar<BookmarkId>
     void setCurrentFolder(BookmarkId folder) {
         mCurrentFolder = mBookmarkModel.getBookmarkById(folder);
         enableImportExportMenu();
+        enableSelectAllMenu();
+    }
+
+    void enableSelectAllMenu() {
+        getMenu().findItem(R.id.select_all_menu_id).setVisible(true);
     }
 
     void enableImportExportMenu() {
@@ -215,6 +221,17 @@ public class BookmarkToolbar extends SelectableListToolbar<BookmarkId>
             mExportBookmarkRunnable.run();
             return true;
         }
+        if (menuItem.getItemId() == R.id.select_all_menu_id) {
+            if (mBookmarkModel.isBookmarkModelLoaded()) {
+                List<BookmarkItem> items = mBookmarkModel.getBookmarksForFolder(mCurrentFolder.getId());
+                HashSet<BookmarkId> ids = new HashSet<>(items.size());
+                for (BookmarkItem item : items) {
+                    ids.add(item.getId());
+                }
+                mSelectionDelegate.setSelectedItems(ids);
+            }
+            return true;
+        }
         return mMenuIdClickedFunction.apply(menuItem.getItemId());
     }
 
@@ -232,6 +249,7 @@ public class BookmarkToolbar extends SelectableListToolbar<BookmarkId>
 
         getMenu().findItem(R.id.import_menu_id).setVisible(mCurrentFolder != null);
         getMenu().findItem(R.id.export_menu_id).setVisible(mCurrentFolder != null);
+        getMenu().findItem(R.id.select_all_menu_id).setVisible(mCurrentFolder != null);
 
         // SelectableListToolbar will show/hide the entire group.
         setEditButtonVisible(mEditButtonVisible);
