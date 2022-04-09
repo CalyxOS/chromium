2b18501885c99ce07ed684e5d3c888b5ab9b2f5c
uazo <uazo@users.noreply.github.com>
2022-04-09 21:43:35 +0200

    Disable crash reporting
    Change-Id: I4038e160b194d731c369c314f241814c83551d96

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -15707,6 +15707,7 @@ void RenderFrameHostImpl::OnSameDocumentCommitProcessed(
 void RenderFrameHostImpl::MaybeGenerateCrashReport(
     base::TerminationStatus status,
     int exit_code) {
+  if ((true)) return;
   if (!last_committed_url_.SchemeIsHTTPOrHTTPS())
     return;
 
