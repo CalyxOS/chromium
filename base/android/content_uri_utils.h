60d2d9b2c8b8e9340e5095df1936d1f0ff793f72
uazo <uazo@users.noreply.github.com>
2022-04-12 15:58:01 +0000

    Add support for writing URIs
    Allows native-side URI file writing
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I3843dce4ce3ee107f36de0aec45607602c3f526d

--- base/android/content_uri_utils.h
+++ base/android/content_uri_utils.h
@@ -18,6 +18,10 @@ namespace base {
 // Returns -1 if the URI is invalid.
 BASE_EXPORT File OpenContentUriForRead(const FilePath& content_uri);
 
+// Opens a content URI for write and returns the file descriptor to the caller.
+// Returns -1 if the URI is invalid.
+BASE_EXPORT File OpenContentUriForWrite(const FilePath& content_uri);
+
 // Check whether a content URI exists.
 BASE_EXPORT bool ContentUriExists(const FilePath& content_uri);
 
