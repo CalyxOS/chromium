d8ebe8e66f53718be3e0502bc4bd4a98560f463b
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-04-29 00:31:49 +0200

    Welcome screen
    Allow toggling automatic updates
    
    TODO:
    Removed auto update code for now, need to add it back
    
    CalyxOS Port Note: Regarding ad blocker automatic updates, we remove
    "; they cannot be disabled" from the first-launch text because the
    legacy ad blocker can now have its updates turned off in Settings.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I4a722acfabcd9a4da886d2d164e69ac36e734813

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
@@ -27,14 +27,9 @@ import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomiza
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileProvider;
 import org.chromium.chrome.browser.search_engines.SearchEnginePromoType;
-import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
-import org.chromium.chrome.browser.signin.services.SigninManager;
 import org.chromium.components.crash.CrashKeyIndex;
 import org.chromium.components.crash.CrashKeys;
 import org.chromium.components.embedder_support.util.UrlConstants;
-import org.chromium.components.signin.AccountManagerFacadeProvider;
-import org.chromium.components.signin.identitymanager.ConsentLevel;
-import org.chromium.components.signin.identitymanager.IdentityManager;
 
 /**
  * A helper to determine what should be the sequence of First Run Experience screens, and whether
@@ -62,41 +57,19 @@ public abstract class FirstRunFlowSequencer {
 
         /** Returns true if the sync consent promo page should be shown. */
         boolean shouldShowSyncConsentPage(boolean isChild) {
-            if (isChild) {
-                // Always show the sync consent page for child account.
-                return true;
-            }
-            assert mProfileSupplier.get() != null;
-            Profile profile = mProfileSupplier.get().getOriginalProfile();
-
-            final IdentityManager identityManager =
-                    IdentityServicesProvider.get().getIdentityManager(profile);
-            if (identityManager.hasPrimaryAccount(ConsentLevel.SYNC) || !isSyncAllowed()) {
-                // No need to show the sync consent page if users already consented to sync or
-                // if sync is not allowed.
-                return false;
-            }
-            // Show the sync consent page only to the signed-in users.
-            return identityManager.hasPrimaryAccount(ConsentLevel.SIGNIN);
+            return false;
         }
 
         /** @return true if the Search Engine promo page should be shown. */
         @VisibleForTesting
         public boolean shouldShowSearchEnginePage() {
-            @SearchEnginePromoType
-            int searchPromoType = LocaleManager.getInstance().getSearchEnginePromoShowType();
-            return searchPromoType == SearchEnginePromoType.SHOW_NEW
-                    || searchPromoType == SearchEnginePromoType.SHOW_EXISTING;
+            return false;
         }
 
         /** @return true if Sync is allowed for the current user. */
         @VisibleForTesting
         protected boolean isSyncAllowed() {
-            Profile profile = mProfileSupplier.get().getOriginalProfile();
-            SigninManager signinManager = IdentityServicesProvider.get().getSigninManager(profile);
-            return FirstRunUtils.canAllowSync()
-                    && !signinManager.isSigninDisabledByPolicy()
-                    && signinManager.isSigninSupported(/* requireUpdatedPlayServices= */ false);
+            return false;
         }
     }
 
@@ -152,18 +125,8 @@ public abstract class FirstRunFlowSequencer {
      *                                  method.
      */
     void start() {
-        AccountManagerFacadeProvider.getInstance()
-                .getCoreAccountInfos()
-                .then(
-                        coreAccountInfos -> {
-                            RecordHistogram.recordCount1MHistogram(
-                                    "Signin.AndroidDeviceAccountsNumberWhenEnteringFRE",
-                                    Math.min(coreAccountInfos.size(), 2));
-
-                            assert !mAccountsAvailable;
-                            mAccountsAvailable = true;
-                            maybeProcessFreEnvironmentPreNative();
-                        });
+        mIsChild = false;
+        maybeProcessFreEnvironmentPreNative();
     }
 
     @VisibleForTesting
@@ -182,14 +145,10 @@ public abstract class FirstRunFlowSequencer {
     }
 
     private void maybeProcessFreEnvironmentPreNative() {
-        // Wait till both child account status and the list of accounts are available.
-        if (mIsChild == null || !mAccountsAvailable) return;
-
         if (mIsFlowKnown) return;
         mIsFlowKnown = true;
 
         Bundle freProperties = new Bundle();
-        freProperties.putBoolean(SyncConsentFirstRunFragment.IS_CHILD_ACCOUNT, mIsChild);
 
         onFlowIsKnown(freProperties);
     }
@@ -200,8 +159,8 @@ public abstract class FirstRunFlowSequencer {
      * @param freProperties Resulting FRE properties bundle.
      */
     public void updateFirstRunProperties(Bundle freProperties) {
-        freProperties.putBoolean(
-                FirstRunActivity.SHOW_SYNC_CONSENT_PAGE, shouldShowSyncConsentPage());
+        if (freProperties == null)
+          throw new RuntimeException("attempting to update null FRE properties");
         freProperties.putBoolean(
                 FirstRunActivity.SHOW_SEARCH_ENGINE_PAGE, shouldShowSearchEnginePage());
     }
@@ -303,13 +262,17 @@ public abstract class FirstRunFlowSequencer {
             if (!(caller instanceof Activity)) {
                 freIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             }
-            IntentUtils.safeStartActivity(caller, freIntent);
+            if (!IntentUtils.safeStartActivity(caller, freIntent)) {
+              throw new RuntimeException("Cannot start FirstRunExperience activity");
+            }
         } else {
             // First Run requires that the Intent contains NEW_TASK so that it doesn't sit on top
             // of something else.
             Intent newIntent = new Intent(fromIntent);
             newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            IntentUtils.safeStartActivity(caller, newIntent);
+            if (!IntentUtils.safeStartActivity(caller, newIntent)) {
+              throw new RuntimeException("Cannot start FirstRunExperience activity");
+            }
         }
         return true;
     }
