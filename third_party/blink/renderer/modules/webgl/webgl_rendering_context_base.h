7a3dde18a3a2ff71c43e1c1c343eafec7c94ef25
uazo <uazo@users.noreply.github.com>
2022-05-03 14:44:11 +0000

    Add webGL site setting
    Do not provide any device information when serving context creation errors.
    
    Requires patch: Content-settings-infrastructure.patch
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I07f8332bda840e93246cfb3444bfb96893e01b51

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
@@ -1936,6 +1936,8 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
                         DOMArrayBufferView* pixels,
                         int64_t offset);
 
+  static bool AllowWebglForHost(blink::CanvasRenderingContextHost* host);
+
   void RecordANGLEImplementation();
 
  private:
