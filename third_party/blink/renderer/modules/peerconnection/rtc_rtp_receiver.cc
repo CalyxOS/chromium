8eff028038991410692807703ca3640c5ff191bb
uazo <uazo@users.noreply.github.com>
2022-05-06 14:27:17 +0000

    Add webRTC site settings
    Requires patch: Content-settings-infrastructure.patch
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I5310d06c287f8a0dab28756e54a4d63a7229ae15

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_receiver.cc
@@ -12,6 +12,8 @@
 #include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
 #include "third_party/blink/public/common/privacy_budget/identifiable_token_builder.h"
 #include "third_party/blink/public/platform/modules/webrtc/webrtc_logging.h"
+#include "third_party/blink/public/platform/web_content_settings_client.h"
+#include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_insertable_streams.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_rtcp_parameters.h"
@@ -286,6 +288,14 @@ RTCRtpCapabilities* RTCRtpReceiver::getCapabilities(ScriptState* state,
                                                     const String& kind) {
   if (kind != "audio" && kind != "video")
     return nullptr;
+
+  LocalDOMWindow* window = To<LocalDOMWindow>(ExecutionContext::From(state));
+  auto* web_frame =
+      static_cast<WebLocalFrame*>(WebFrame::FromCoreFrame(window->GetFrame()));
+  blink::WebContentSettingsClient* settings = web_frame->GetContentSettingsClient();
+  if (settings && !settings->AllowContentSetting(ContentSettingsType::WEBRTC, false)) {
+    return nullptr;
+  }
 
   RTCRtpCapabilities* capabilities = RTCRtpCapabilities::Create();
   capabilities->setCodecs(HeapVector<Member<RTCRtpCodecCapability>>());
