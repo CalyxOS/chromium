92fcd2b8c5c406d0edd161dfee4d0b4fe408ff22
uazo <uazo@users.noreply.github.com>
2022-06-09 19:45:03 +0000

    Remove segmentation platform
    Also fixes a crash on startup.
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ief303535f95c44b1bfaa56973fe37e0f01dbc61f

--- components/segmentation_platform/public/features.cc
+++ components/segmentation_platform/public/features.cc
@@ -233,4 +233,7 @@ constexpr base::FeatureParam<int> kMaxDefaultBrowserCardImpressions{
     &kEducationalTipModule, "max_default_browser_card_impressions",
     /*default_value=*/4};
 
+SET_CROMITE_FEATURE_DISABLED(kSegmentationPlatformFeature);
+SET_CROMITE_FEATURE_DISABLED(kSegmentationPlatformFeedSegmentFeature);
+SET_CROMITE_FEATURE_DISABLED(kSegmentationPlatformAdaptiveToolbarV2Feature);
 }  // namespace segmentation_platform::features
