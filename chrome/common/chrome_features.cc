444615e927272d84170b08e99b86abc60cc2fbac
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-06-10 14:20:02 +0200

    Enable Certificate Transparency
    Reporting/auditing functionalities are disabled; a flag is exposed.
    Add guard to make sure that certificate transparency stays enabled
    by default.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I743519ccda7c357578c23b0a074292649b3afb3d

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -1122,13 +1122,7 @@ BASE_FEATURE(kSchedulerConfiguration,
 
 // Controls whether SCT audit reports are queued and the rate at which they
 // should be sampled. Default sampling rate is 1/10,000 certificates.
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && !BUILDFLAG(IS_ANDROID)
-BASE_FEATURE(kSCTAuditing, "SCTAuditing", base::FEATURE_ENABLED_BY_DEFAULT);
-#else
-// This requires backend infrastructure and a data collection policy.
-// Non-Chrome builds should not use Chrome's infrastructure.
-BASE_FEATURE(kSCTAuditing, "SCTAuditing", base::FEATURE_DISABLED_BY_DEFAULT);
-#endif
+BASE_FEATURE(kSCTAuditing, "SCTAuditing", base::FEATURE_DISABLED_BY_DEFAULT); // disabled in Bromite
 constexpr base::FeatureParam<double> kSCTAuditingSamplingRate{
     &kSCTAuditing, "sampling_rate", 0.0001};
 
