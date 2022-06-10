d07fb5c405d58984abfcf0f195c45a645c87b6d9
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-06-10 14:20:02 +0200

    Enable Certificate Transparency
    Reporting/auditing functionalities are disabled; a flag is exposed.
    Add guard to make sure that certificate transparency stays enabled
    by default.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I743519ccda7c357578c23b0a074292649b3afb3d

--- net/http/transport_security_state.cc
+++ net/http/transport_security_state.cc
@@ -186,6 +186,7 @@ class HSTSPreloadDecoder : public net::extras::PreloadDecoder {
 
 bool DecodeHSTSPreload(const std::string& search_hostname, PreloadResult* out) {
 #if !BUILDFLAG(INCLUDE_TRANSPORT_SECURITY_STATE_PRELOAD_LIST)
+#error "BUILDFLAG(INCLUDE_TRANSPORT_SECURITY_STATE_PRELOAD_LIST) must be enabled"
   if (g_hsts_source == nullptr)
     return false;
 #endif
