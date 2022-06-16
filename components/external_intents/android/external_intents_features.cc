69f226649667c6b335338cbb5afae31e27278408
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-06-16 23:23:43 +0200

    Revert "Delete block-external-form-redirects"
    This reverts commit b710cefb53b558a8bcd884f6baf0229ba4225721 and
    enables IntentBlockExternalFormRedirectsNoGesture.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I3b7bde93b5ec12ed4dffbca0105ab9ea3329bae7

--- components/external_intents/android/external_intents_features.cc
+++ components/external_intents/android/external_intents_features.cc
@@ -20,7 +20,6 @@ namespace {
 const base::Feature* kFeaturesExposedToJava[] = {
     &kExternalNavigationDebugLogs, &kBlockFrameRenavigations,
     &kBlockIntentsToSelf, &kTrustedClientGestureBypass};
-
 }  // namespace
 
 // Alphabetical:
@@ -41,7 +40,13 @@ BASE_FEATURE(kTrustedClientGestureBypass,
              "TrustedClientGestureBypass",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+CROMITE_FEATURE(kIntentBlockExternalFormRedirectsNoGesture,
+                "IntentBlockExternalFormRedirectsNoGesture",
+                base::FEATURE_ENABLED_BY_DEFAULT);
+
 static jlong JNI_ExternalIntentsFeatures_GetFeature(JNIEnv* env, jint ordinal) {
+  if (ordinal == -1)
+    return reinterpret_cast<jlong>(&kIntentBlockExternalFormRedirectsNoGesture);
   return reinterpret_cast<jlong>(kFeaturesExposedToJava[ordinal]);
 }
 
