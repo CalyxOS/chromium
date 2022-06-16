0ee6b446f4cdac2701518fd5845d90bfea098623
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-06-16 23:23:43 +0200

    Revert "Delete block-external-form-redirects"
    This reverts commit b710cefb53b558a8bcd884f6baf0229ba4225721 and
    enables IntentBlockExternalFormRedirectsNoGesture.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: I3b7bde93b5ec12ed4dffbca0105ab9ea3329bae7

--- components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
+++ components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
@@ -283,8 +283,7 @@ public class ExternalNavigationHandlerTest {
                 .withPageTransition(PageTransition.FORM_SUBMIT)
                 .withIsRedirect(true)
                 .withHasUserGesture(true)
-                .expecting(OverrideUrlLoadingResultType.OVERRIDE_WITH_EXTERNAL_INTENT,
-                        START_OTHER_ACTIVITY);
+                .expecting(OverrideUrlLoadingResultType.NO_OVERRIDE, IGNORE);
 
         // If the page matches the referrer, then continue loading in Chrome.
         checkUrl("http://youtube.com://", handler)
@@ -2717,6 +2716,11 @@ public class ExternalNavigationHandlerTest {
         protected boolean isValidWebApk(String packageName) {
             return packageName.startsWith(WEBAPK_PACKAGE_PREFIX)
                     && !packageName.equals(INVALID_WEBAPK_PACKAGE_NAME);
+        }
+
+        @Override
+        public boolean blockExternalFormRedirectsWithoutGesture() {
+            return true;
         }
 
         @Override
