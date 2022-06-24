47d06775545086ff9d1774b4ca0b8479413169ed
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-06-24 22:24:22 +0200

    Revert removal of execution context address space
    Revert "[Private Network Access] Remove ExecutionContext::SetAddressSpace()."
    This reverts commit dfa3a4a6c0d023e5126dad31f6c479dc8d20e416.
    
    Revert "[Private Network Access] Remove ExecutionContext::AddressSpace()."
    This reverts commit a5c64b9f5f985cfebab3375b53062321a62e3e31.
    
    Revert "[Private Network Access] Remove GlobalScopeCreationParams address space."
    This reverts commit c32df617780283942d49ca5574fcd6b7f691e9c1.
    
    Revert "[Private Network Access] Remove WorkerClassicScriptLoader address space."
    This reverts commit 11f6a3e29bee9a6590a4fd56b28e8048200c18d0.
    
    Original License: BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I68466e09bf3e643f2362e67c8af614a9bba45a06

--- third_party/blink/renderer/core/workers/dedicated_worker.h
+++ third_party/blink/renderer/core/workers/dedicated_worker.h
@@ -157,6 +157,7 @@ class CORE_EXPORT DedicatedWorker final
       network::mojom::ReferrerPolicy,
       Vector<network::mojom::blink::ContentSecurityPolicyPtr>
           response_content_security_policies,
+      std::optional<network::mojom::IPAddressSpace> response_address_space,
       const String& source_code,
       RejectCoepUnsafeNone reject_coep_unsafe_none,
       mojo::PendingRemote<mojom::blink::BackForwardCacheControllerHost>
@@ -172,6 +173,7 @@ class CORE_EXPORT DedicatedWorker final
       network::mojom::ReferrerPolicy,
       Vector<network::mojom::blink::ContentSecurityPolicyPtr>
           response_content_security_policies,
+      std::optional<network::mojom::IPAddressSpace> response_address_space,
       const String& source_code,
       RejectCoepUnsafeNone reject_coep_unsafe_none,
       mojo::PendingRemote<mojom::blink::BackForwardCacheControllerHost>
@@ -185,6 +187,7 @@ class CORE_EXPORT DedicatedWorker final
       network::mojom::ReferrerPolicy,
       Vector<network::mojom::blink::ContentSecurityPolicyPtr>
           response_content_security_policies,
+      std::optional<network::mojom::IPAddressSpace> response_address_space,
       mojo::PendingReceiver<mojom::blink::ReportingObserver>
           coep_reporting_observer,
       mojo::PendingReceiver<mojom::blink::ReportingObserver>
