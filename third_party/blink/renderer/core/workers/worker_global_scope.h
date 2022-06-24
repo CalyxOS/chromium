16413cb5799a98df1a95c3f4242ec19821ae887d
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-06-24 22:24:22 +0200

    Revert removal of execution context address space
    Revert "[Private Network Access] Remove ExecutionContext::SetAddressSpace()."
    This reverts commit dfa3a4a6c0d023e5126dad31f6c479dc8d20e416.
    
    Revert "[Private Network Access] Remove ExecutionContext::AddressSpace()."
    This reverts commit a5c64b9f5f985cfebab3375b53062321a62e3e31.
    
    Revert "[Private Network Access] Remove GlobalScopeCreationParams address space."
    This reverts commit c32df617780283942d49ca5574fcd6b7f691e9c1.
    
    Revert "[Private Network Access] Remove WorkerClassicScriptLoader address space."
    This reverts commit 11f6a3e29bee9a6590a4fd56b28e8048200c18d0.
    
    Original License: BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I68466e09bf3e643f2362e67c8af614a9bba45a06

--- third_party/blink/renderer/core/workers/worker_global_scope.h
+++ third_party/blink/renderer/core/workers/worker_global_scope.h
@@ -32,6 +32,7 @@
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "services/network/public/mojom/fetch_api.mojom-blink-forward.h"
+#include "services/network/public/mojom/ip_address_space.mojom-blink-forward.h"
 #include "third_party/blink/public/common/loader/worker_main_script_load_parameters.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/mojom/loader/code_cache.mojom-blink-forward.h"
@@ -175,6 +176,7 @@ class CORE_EXPORT WorkerGlobalScope
   virtual void Initialize(
       const KURL& response_url,
       network::mojom::ReferrerPolicy response_referrer_policy,
+      network::mojom::IPAddressSpace response_address_space,
       Vector<network::mojom::blink::ContentSecurityPolicyPtr> response_csp,
       const Vector<String>* response_origin_trial_tokens) = 0;
 
