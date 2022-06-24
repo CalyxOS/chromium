cce98d3c06ae56cd571831910c73c7930480b09d
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-06-24 22:24:22 +0200

    Revert removal of execution context address space
    Revert "[Private Network Access] Remove ExecutionContext::SetAddressSpace()."
    This reverts commit dfa3a4a6c0d023e5126dad31f6c479dc8d20e416.
    
    Revert "[Private Network Access] Remove ExecutionContext::AddressSpace()."
    This reverts commit a5c64b9f5f985cfebab3375b53062321a62e3e31.
    
    Revert "[Private Network Access] Remove GlobalScopeCreationParams address space."
    This reverts commit c32df617780283942d49ca5574fcd6b7f691e9c1.
    
    Revert "[Private Network Access] Remove WorkerClassicScriptLoader address space."
    This reverts commit 11f6a3e29bee9a6590a4fd56b28e8048200c18d0.
    
    Original License: BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I68466e09bf3e643f2362e67c8af614a9bba45a06

--- third_party/blink/renderer/core/loader/modulescript/worker_module_script_fetcher.cc
+++ third_party/blink/renderer/core/loader/modulescript/worker_module_script_fetcher.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "services/network/public/mojom/ip_address_space.mojom-blink.h"
 #include "services/network/public/mojom/referrer_policy.mojom-blink.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/loader/network_utils.h"
@@ -174,7 +175,7 @@ void WorkerModuleScriptFetcher::NotifyClient(
 
     // Step 12.3-12.6 are implemented in Initialize().
     global_scope_->Initialize(
-        response_url, response_referrer_policy,
+        response_url, response_referrer_policy, response.AddressSpace(),
         ParseContentSecurityPolicyHeaders(
             ContentSecurityPolicyResponseHeaders(response)),
         response_origin_trial_tokens.get());
