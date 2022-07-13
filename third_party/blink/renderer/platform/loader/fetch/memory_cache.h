cb1d41263b4da9505b7b67b2bd5a0ea9a2d46040
uazo <uazo@users.noreply.github.com>
2022-07-13 14:51:09 +0000

    Partition Blink memory cache
    Blink's in-memory cache is not partitioned (see also: http://crbug.com/1127971)
    This patch partitions it by the top-level site.
    This mitigation is effective in case the rendering process is re-used, because
    on such case the cache would be re-used as well and transfer information between
    different contexts.
    
    See also:
    * https://github.com/bromite/bromite/pull/2173
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- third_party/blink/renderer/platform/loader/fetch/memory_cache.h
+++ third_party/blink/renderer/platform/loader/fetch/memory_cache.h
@@ -116,9 +116,7 @@ class PLATFORM_EXPORT MemoryCache final : public GarbageCollected<MemoryCache>,
     TypeStatistic other;
   };
 
-  Resource* ResourceForURL(const KURL&) const;
   Resource* ResourceForURL(const KURL&, const String& cache_identifier) const;
-  HeapVector<Member<Resource>> ResourcesForURL(const KURL&) const;
 
   void Add(Resource*);
   void Remove(Resource*);
@@ -163,6 +161,8 @@ class PLATFORM_EXPORT MemoryCache final : public GarbageCollected<MemoryCache>,
       base::MemoryPressureListener::MemoryPressureLevel) override;
 
  private:
+   HeapVector<Member<Resource>> ResourcesForURL(const KURL&) const;
+
   enum PruneStrategy {
     // Automatically decide how much to prune.
     kAutomaticPrune,
