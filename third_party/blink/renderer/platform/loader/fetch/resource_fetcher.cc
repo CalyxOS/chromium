8e8c6ee0146e9e9a0bf133beee4cbe3d4dc33f80
uazo <uazo@users.noreply.github.com>
2022-07-13 14:51:09 +0000

    Partition Blink memory cache
    Blink's in-memory cache is not partitioned (see also: http://crbug.com/1127971)
    This patch partitions it by the top-level site.
    This mitigation is effective in case the rendering process is re-used, because
    on such case the cache would be re-used as well and transfer information between
    different contexts.
    
    See also:
    * https://github.com/bromite/bromite/pull/2173
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ia60195174ffd5701404330b6555738899ea034cc

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -992,7 +992,7 @@ Resource* ResourceFetcher::CreateResourceForStaticData(
   if (!archive_ && factory.GetType() == ResourceType::kRaw)
     return nullptr;
 
-  const String cache_identifier = GetCacheIdentifier(url);
+  const String cache_identifier = GetCacheIdentifier(url, params.GetResourceRequest());
   // Most off-main-thread resource fetches use Resource::kRaw and don't reach
   // this point, but off-main-thread module fetches might.
   if (IsMainThread()) {
@@ -1408,7 +1408,8 @@ Resource* ResourceFetcher::RequestResource(FetchParameters& params,
       MakePreloadedResourceBlockOnloadIfNeeded(resource, params);
     } else if (IsMainThread()) {
       resource = MemoryCache::Get()->ResourceForURL(
-          params.Url(), GetCacheIdentifier(params.Url()));
+          params.Url(),
+            GetCacheIdentifier(params.Url(), params.GetResourceRequest()));
       if (resource) {
         policy = DetermineRevalidationPolicy(resource_type, params, *resource,
                                              is_static_data);
@@ -1719,7 +1720,8 @@ Resource* ResourceFetcher::CreateResourceForLoading(
     const FetchParameters& params,
     const ResourceFactory& factory) {
   const String cache_identifier =
-      GetCacheIdentifier(params.GetResourceRequest().Url());
+      GetCacheIdentifier(params.GetResourceRequest().Url(),
+        params.GetResourceRequest());
   DCHECK(!IsMainThread() || params.IsStaleRevalidation() ||
          !MemoryCache::Get()->ResourceForURL(params.GetResourceRequest().Url(),
                                              cache_identifier));
@@ -2788,10 +2790,23 @@ void ResourceFetcher::UpdateAllImageResourcePriorities() {
   to_be_removed.clear();
 }
 
-String ResourceFetcher::GetCacheIdentifier(const KURL& url) const {
+String ResourceFetcher::GetCacheIdentifier(const KURL& url,
+              const ResourceRequest& resource_request) const {
+  if (const scoped_refptr<const SecurityOrigin> top_origin = resource_request.TopFrameOrigin()) {
+    return ResourceFetcher::GetCacheIdentifier(url, top_origin);
+  } else if (const scoped_refptr<const SecurityOrigin> requestor_origin = resource_request.RequestorOrigin()) {
+    return ResourceFetcher::GetCacheIdentifier(url, requestor_origin);
+  }
+  NOTREACHED();
+}
+
+String ResourceFetcher::GetCacheIdentifier(const KURL& url,
+              scoped_refptr<const blink::SecurityOrigin> origin) const {
+  String origin_url = origin ? origin->ToRawString() : "";
+
   if (properties_->GetControllerServiceWorkerMode() !=
       mojom::ControllerServiceWorkerMode::kNoController) {
-    return String::Number(properties_->ServiceWorkerId());
+    return origin_url + " " + String::Number(properties_->ServiceWorkerId());
   }
 
   // Requests that can be satisfied via `archive_` (i.e. MHTML) or
@@ -2804,7 +2819,7 @@ String ResourceFetcher::GetCacheIdentifier(const KURL& url) const {
   if (bundle)
     return bundle->GetCacheIdentifier();
 
-  return MemoryCache::DefaultCacheIdentifier();
+  return origin_url;
 }
 
 std::optional<base::UnguessableToken>
--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h
@@ -278,7 +278,10 @@ class PLATFORM_EXPORT ResourceFetcher
                          uint32_t inflight_keepalive_bytes);
   blink::mojom::ControllerServiceWorkerMode IsControlledByServiceWorker() const;
 
-  String GetCacheIdentifier(const KURL& url) const;
+  String GetCacheIdentifier(const KURL& url,
+    const ResourceRequest& resource_request) const;
+  String GetCacheIdentifier(const KURL& url,
+    scoped_refptr<const blink::SecurityOrigin> origin) const;
 
   // If `url` exists as a resource in a subresource bundle in this frame,
   // returns its UnguessableToken; otherwise, returns std::nullopt.
