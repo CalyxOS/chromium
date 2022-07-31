abb61249b5a63cce8b907fc9ae26df98a0d61a9d
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-07-31 22:13:11 +0200

    Disable PrivacyGuide
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -492,8 +492,8 @@ BASE_FEATURE(kHaTSDesktopDevToolsIssuesCSP,
 
 // Enables or disables the Happiness Tracking System for Desktop Privacy Guide.
 BASE_FEATURE(kHappinessTrackingSurveysForDesktopPrivacyGuide,
-             "HappinessTrackingSurveysForDesktopPrivacyGuide",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "HappinessTrackingSurveysForDesktopPrivacyGuide",                      // must be disabled
+             base::FEATURE_DISABLED_BY_DEFAULT);                                    // in Bromite
 const base::FeatureParam<base::TimeDelta>
     kHappinessTrackingSurveysForDesktopPrivacyGuideTime{
         &kHappinessTrackingSurveysForDesktopPrivacyGuide, "settings-time",
@@ -917,11 +917,11 @@ BASE_FEATURE(kPrerenderFallbackToPreconnect,
              "PrerenderFallbackToPreconnect",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kPrivacyGuide2, "PrivacyGuide2", base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kPrivacyGuide2, "PrivacyGuide2", base::FEATURE_DISABLED_BY_DEFAULT); // always disabled in Bromite
 
 BASE_FEATURE(kPrivacyGuideAndroid,
-             "PrivacyGuideAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "PrivacyGuideAndroid",               // always disabled
+             base::FEATURE_DISABLED_BY_DEFAULT);  // in Bromite
 
 // Enables or disables push subscriptions keeping Chrome running in the
 // background when closed.
