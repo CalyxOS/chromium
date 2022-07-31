8f5bd0a2dcd1ef4c644abdbbbd01bd08eab97502
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-07-31 22:13:11 +0200

    Disable PrivacyGuide
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I6c5fa2baf41f43c017857ba660029c7844664aa2

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -518,8 +518,8 @@ BASE_FEATURE(kHaTSDesktopDevToolsIssuesCSP,
 
 // Enables or disables the Happiness Tracking System for Desktop Privacy Guide.
 BASE_FEATURE(kHappinessTrackingSurveysForDesktopPrivacyGuide,
-             "HappinessTrackingSurveysForDesktopPrivacyGuide",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "HappinessTrackingSurveysForDesktopPrivacyGuide",                      // must be disabled
+             base::FEATURE_DISABLED_BY_DEFAULT);                                    // in Bromite
 const base::FeatureParam<base::TimeDelta>
     kHappinessTrackingSurveysForDesktopPrivacyGuideTime{
         &kHappinessTrackingSurveysForDesktopPrivacyGuide, "settings-time",
@@ -1042,8 +1042,8 @@ BASE_FEATURE(kPrintPreviewSetupAssistance,
 #endif
 
 BASE_FEATURE(kPrivacyGuideAndroid,
-             "PrivacyGuideAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "PrivacyGuideAndroid",               // always disabled
+             base::FEATURE_DISABLED_BY_DEFAULT);  // in Bromite
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kPrivacyGuideAndroidPostMVP,
