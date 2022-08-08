9513633667b896d49e09788a764a9bf5c09a918c
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-08-08 02:35:35 +0200

    Re-introduce modal dialog flag to close all tabs
    Revert "[Tab Management] Cleanup close all tabs modal dialog flags"
    
    This reverts commit 614a998c5af5dcd36baffa6d65090c0b8decb03f.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    Change-Id: Ia59ed533b4ea5465d2e04ca1438c583b8b95fc65

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/CloseAllTabsDialogUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/CloseAllTabsDialogUnitTest.java
@@ -25,9 +25,11 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
+import org.chromium.base.FeatureList;
 import org.chromium.base.metrics.UmaRecorder;
 import org.chromium.base.metrics.UmaRecorderHolder;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -135,6 +137,7 @@ public class CloseAllTabsDialogUnitTest {
     @Test
     @SmallTest
     public void testDialog() {
+        enableFeature(true);
         final boolean isIncognito = false;
         CloseAllTabsDialog.show(mContext, this::getModalDialogManager,
                 () -> { mRunnableCalled = true; }, isIncognito);
@@ -148,6 +151,7 @@ public class CloseAllTabsDialogUnitTest {
     @Test
     @SmallTest
     public void testDismissButton() {
+        enableFeature(true);
         final boolean isIncognito = true;
         CloseAllTabsDialog.show(mContext, this::getModalDialogManager,
                 () -> { mRunnableCalled = true; }, isIncognito);
@@ -161,6 +165,7 @@ public class CloseAllTabsDialogUnitTest {
     @Test
     @SmallTest
     public void testDismissNoButton() {
+        enableFeature(true);
         final boolean isIncognito = false;
         CloseAllTabsDialog.show(mContext, this::getModalDialogManager,
                 () -> { mRunnableCalled = true; }, isIncognito);
@@ -170,5 +175,23 @@ public class CloseAllTabsDialogUnitTest {
                 DialogDismissalCause.NAVIGATE_BACK_OR_TOUCH_OUTSIDE);
         assertFalse(mRunnableCalled);
         verifyDismissed(false, isIncognito);
+    }
+
+    @Test
+    @SmallTest
+    public void testDialogInactive() {
+        enableFeature(false);
+        final boolean isIncognito = false;
+        CloseAllTabsDialog.show(mContext, this::getModalDialogManager,
+                () -> { mRunnableCalled = true; }, isIncognito);
+
+        assertNull(mMockModalDialogManager.getDialogModel());
+        assertTrue(mRunnableCalled);
+    }
+
+    private void enableFeature(boolean enable) {
+        FeatureList.TestValues values = new FeatureList.TestValues();
+        values.addFeatureFlagOverride(ChromeFeatureList.CLOSE_ALL_TABS_MODAL_DIALOG, enable);
+        FeatureList.setTestValues(values);
     }
 }
