a4e45322016965cf4bf0a9a0e4d33421fba9323f
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-08-08 02:35:35 +0200

    Re-introduce modal dialog flag to close all tabs
    Revert "[Tab Management] Cleanup close all tabs modal dialog flags"
    
    This reverts commit 614a998c5af5dcd36baffa6d65090c0b8decb03f.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ia59ed533b4ea5465d2e04ca1438c583b8b95fc65

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -183,6 +183,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kBackGestureActivityTabProvider,
     &kBackGestureRefactorActivityAndroid,
     &kBackGestureRefactorAndroid,
+    &kCloseAllTabsModalDialog,
     &kBackgroundThreadPool,
     &kBaselineGM3SurfaceColors,
     &kBlockIntentsWhileLocked,
@@ -528,6 +529,10 @@ BASE_FEATURE(kCacheActivityTaskID,
              "CacheActivityTaskID",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kCloseAllTabsModalDialog,
+             "CloseAllTabsModalDialog",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Used in downstream code.
 BASE_FEATURE(kCastDeviceFilter,
              "CastDeviceFilter",
@@ -538,8 +543,8 @@ BASE_FEATURE(kClearOmniboxFocusAfterNavigation,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCloseTabSuggestions,
-             "CloseTabSuggestions",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "CloseTabSuggestions",               // must be enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);   // in Bromite
 
 BASE_FEATURE(kCloseTabSaveTabList,
              "CloseTabSaveTabList",
