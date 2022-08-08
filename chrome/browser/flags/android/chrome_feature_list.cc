8f2cf647eecf2981851d09e7546e1b07c472948a
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-08-08 02:35:35 +0200

    Re-introduce modal dialog flag to close all tabs
    Revert "[Tab Management] Cleanup close all tabs modal dialog flags"
    
    This reverts commit 614a998c5af5dcd36baffa6d65090c0b8decb03f.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ia59ed533b4ea5465d2e04ca1438c583b8b95fc65

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -171,6 +171,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kBackGestureActivityTabProvider,
     &kBackGestureMoveToBackDuringStartup,
     &kBackGestureRefactorAndroid,
+    &kCloseAllTabsModalDialog,
     &kBackgroundThreadPool,
     &kBlockIntentsWhileLocked,
     &kCacheActivityTaskID,
@@ -452,8 +453,8 @@ BASE_FEATURE(kAppSpecificHistory,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kArchiveTabService,
-             "ArchiveTabService",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "ArchiveTabService",                 // must be enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);   // in Bromite
 
 BASE_FEATURE(kAuxiliarySearchDonation,
              "AuxiliarySearchDonation",
@@ -469,6 +470,10 @@ BASE_FEATURE(kBlockIntentsWhileLocked,
 
 BASE_FEATURE(kCacheActivityTaskID,
              "CacheActivityTaskID",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kCloseAllTabsModalDialog,
+             "CloseAllTabsModalDialog",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Used in downstream code.
