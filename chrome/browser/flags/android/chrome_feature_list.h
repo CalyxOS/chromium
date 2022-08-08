5a629c0000e2958db1a48abd5e7da10751bd46be
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-08-08 02:35:35 +0200

    Re-introduce modal dialog flag to close all tabs
    Revert "[Tab Management] Cleanup close all tabs modal dialog flags"
    
    This reverts commit 614a998c5af5dcd36baffa6d65090c0b8decb03f.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Ia59ed533b4ea5465d2e04ca1438c583b8b95fc65

--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -43,6 +43,7 @@ BASE_DECLARE_FEATURE(kBaselineGM3SurfaceColors);
 BASE_DECLARE_FEATURE(kBlockIntentsWhileLocked);
 BASE_DECLARE_FEATURE(kCacheActivityTaskID);
 BASE_DECLARE_FEATURE(kClearOmniboxFocusAfterNavigation);
+BASE_DECLARE_FEATURE(kCloseAllTabsModalDialog);
 BASE_DECLARE_FEATURE(kCloseTabSaveTabList);
 BASE_DECLARE_FEATURE(kCreateNewTabInitializeRenderer);
 BASE_DECLARE_FEATURE(kCriticalPersistedTabData);
