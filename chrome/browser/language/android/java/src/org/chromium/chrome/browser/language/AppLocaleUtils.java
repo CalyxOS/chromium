a69db7e515ae9d1edde03eb7bb1240c8db148db2
uazo <uazo@users.noreply.github.com>
2022-09-02 07:44:58 +0000

    Override Navigator Language
    Uses the first of the accept-languages as the system language for blink
    and fix the selection in the UI for the browser language
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
+++ chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
@@ -34,6 +34,10 @@ import java.util.Locale;
 public class AppLocaleUtils {
     private AppLocaleUtils(){};
 
+    public interface InstallListener {
+        void onComplete(boolean success);
+    }
+
     // Value of AppLocale preference when the system language is used.
     public static final String APP_LOCALE_USE_SYSTEM_LANGUAGE = null;
 
@@ -96,6 +100,22 @@ public class AppLocaleUtils {
             return APP_LOCALE_USE_SYSTEM_LANGUAGE;
         }
         return locale.toLanguageTag();
+    }
+
+    public static void setAppLanguagePref(
+            String languageName, InstallListener listener) {
+        InstallListener wrappedListener = (success) -> {
+            if (success) {
+                if (shouldUseSystemManagedLocale()) {
+                    setSystemManagedAppLanguage(languageName);
+                } else {
+                    SharedPreferencesManager.getInstance().writeString(
+                            ChromePreferenceKeys.APPLICATION_OVERRIDE_LANGUAGE, languageName);
+                }
+            }
+            listener.onComplete(success);
+        };
+        wrappedListener.onComplete(true);
     }
 
     /**
