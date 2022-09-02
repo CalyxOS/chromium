65633168a212cf9a2e28d905c997438779c7d85f
uazo <uazo@users.noreply.github.com>
2022-09-02 07:44:58 +0000

    Override Navigator Language
    Uses the first of the accept-languages as the system language for blink
    and fix the selection in the UI for the browser language
    
    Original License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: Id32e4bd6c443ebbd30e67e0c20329ffc5d04f647

--- chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
+++ chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
@@ -35,6 +35,10 @@ import java.util.Locale;
 public class AppLocaleUtils {
     private AppLocaleUtils() {}
 
+    public interface InstallListener {
+        void onComplete(boolean success);
+    }
+
     // Value of AppLocale preference when the system language is used.
     public static final @Nullable String APP_LOCALE_USE_SYSTEM_LANGUAGE = null;
 
@@ -104,6 +108,22 @@ public class AppLocaleUtils {
             return APP_LOCALE_USE_SYSTEM_LANGUAGE;
         }
         return locales.get(0).toLanguageTag();
+    }
+
+    public static void setAppLanguagePref(
+            String languageName, InstallListener listener) {
+        InstallListener wrappedListener = (success) -> {
+            if (success) {
+                if (shouldUseSystemManagedLocale()) {
+                    setSystemManagedAppLanguage(languageName);
+                } else {
+                    ChromeSharedPreferences.getInstance().writeString(
+                            ChromePreferenceKeys.APPLICATION_OVERRIDE_LANGUAGE, languageName);
+                }
+            }
+            listener.onComplete(success);
+        };
+        wrappedListener.onComplete(true);
     }
 
     /**
@@ -234,7 +254,7 @@ public class AppLocaleUtils {
      */
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.TIRAMISU)
     public static boolean shouldUseSystemManagedLocale() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU;
+        return false; // Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU;
     }
 
     /**
