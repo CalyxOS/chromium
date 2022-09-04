ed964d9415d49cf6daa54b95ce96db45f1dd8bae
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-09-04 18:47:58 +0200

    Re-introduce kWebAuthCable
    This reverts commit ebfd987a57bf4a58588760e8a4342b4fffef36f2.
    
    Feature is disabled by default.
    
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html

--- chrome/browser/webauthn/chrome_webauthn_browsertest.cc
+++ chrome/browser/webauthn/chrome_webauthn_browsertest.cc
@@ -493,6 +493,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthnCableExtension, ServerLinkExperiments) {
 class WebAuthnCableSecondFactor : public WebAuthnBrowserTest {
  public:
   WebAuthnCableSecondFactor() {
+    scoped_feature_list_.InitWithFeatures({features::kWebAuthCable}, {});
     // This makes it a little easier to compare against.
     trace_ << std::endl;
   }
@@ -753,6 +754,7 @@ class WebAuthnCableSecondFactor : public WebAuthnBrowserTest {
   };
 
  protected:
+  base::test::ScopedFeatureList scoped_feature_list_;
   std::ostringstream trace_;
   AuthenticatorRequestDialogModel* model_ = nullptr;
 };
