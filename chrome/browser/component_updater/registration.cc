11e8fdbff4911665e06e7668412c69adb4955a1c
uazo <uazo@users.noreply.github.com>
2022-11-08 12:41:22 +0000

    Disable Component Updates
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -100,6 +100,7 @@
 namespace component_updater {
 
 void RegisterComponentsForUpdate() {
+  if ((true)) return;
   auto* const cus = g_browser_process->component_updater();
 
 #if BUILDFLAG(IS_WIN)
