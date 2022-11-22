eb388f769112fceca1d85378b0ee612f9df32ef8
uazo <uazo@users.noreply.github.com>
2022-11-22 16:49:58 +0000

    Partial "Add browser policy" to prevent some unwanted connections
    Bring in the parts of "Add browser policy" that look like they may
    affect sign-in, to prevent the browser from communicating with
    accounts.google.com on start.
    
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Issue: calyxos#2703
    Change-Id: Ic02dc33e5e860061eb903c412c98b06fb3389359

--- chrome/browser/signin/account_consistency_mode_manager.cc
+++ chrome/browser/signin/account_consistency_mode_manager.cc
@@ -166,7 +166,7 @@ void AccountConsistencyModeManager::SetIgnoreMissingOAuthClientForTesting() {
 // static
 bool AccountConsistencyModeManager::ShouldBuildServiceForProfile(
     Profile* profile) {
-  return profile->IsRegularProfile();
+  return false;
 }
 
 AccountConsistencyMethod
@@ -204,7 +204,8 @@ AccountConsistencyModeManager::ComputeAccountConsistencyMethod(
 #endif
 
 #if BUILDFLAG(ENABLE_MIRROR)
-  return AccountConsistencyMethod::kMirror;
+  // always disabled
+  return AccountConsistencyMethod::kDisabled;
 #endif
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
@@ -214,7 +215,7 @@ AccountConsistencyModeManager::ComputeAccountConsistencyMethod(
     return AccountConsistencyMethod::kDisabled;
   }
 
-  return AccountConsistencyMethod::kDice;
+  return AccountConsistencyMethod::kDisabled;
 #endif
 
   NOTREACHED();
