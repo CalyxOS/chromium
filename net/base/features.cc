3d2074d88f821c9f5eb9503f46d01a340aa751bf
csagan5 <32685696+csagan5@users.noreply.github.com>
2022-12-03 12:02:20 +0100

    Enable third-party storage partitioning
    License: GPL-3.0-only - https://spdx.org/licenses/GPL-3.0-only.html
    
    Change-Id: I2edec90ccb65e5c9e98ed471a538129af52a0541

--- net/base/features.cc
+++ net/base/features.cc
@@ -270,8 +270,8 @@ BASE_FEATURE(kBlockSetCookieHeader,
 // Enables partitioning of third party storage (IndexedDB, CacheStorage, etc.)
 // by the top level site to reduce fingerprinting.
 BASE_FEATURE(kThirdPartyStoragePartitioning,
-             "ThirdPartyStoragePartitioning",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "ThirdPartyStoragePartitioning",       // must be enabled
+             base::FEATURE_ENABLED_BY_DEFAULT);     // in Bromite
 // Whether to use the new code paths needed to support partitioning Blob URLs.
 // This exists as a kill-switch in case an issue is identified with the Blob
 // URL implementation that causes breakage.
