b740bbfbce3fa9a5d167b2b5d076411d221f2f9b
uazo <uazo@users.noreply.github.com>
2022-12-20 11:06:42 +0000

    Viewport Protection flag
    Scale the viewport and the screen by a random factor to
    prevent coordinate-based fingerprinting scripts.
    The factor is changed at each change of origin.
    The feature is controlled by a feature flag (default enabled)
    
    Require: Content-settings-infrastructure.patch
    
    Change-Id: If2b917e8aeb7d818922a97f71e12f02803aa9158

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -1564,6 +1564,10 @@ void LocalFrame::RestoreScrollOffsets() {
   saved_scroll_offsets_ = nullptr;
 }
 
+void LocalFrame::SetPageZoomFactorBaseValue(float factor) {
+  page_zoom_factor_base_value_ = factor;
+}
+
 void LocalFrame::SetPageZoomFactor(float factor) {
   SetPageAndTextZoomFactors(factor, text_zoom_factor_);
 }
@@ -1738,12 +1742,16 @@ mojom::blink::DevicePostureType LocalFrame::GetDevicePosture() {
   return mojo_handler_->GetDevicePosture();
 }
 
-double LocalFrame::DevicePixelRatio() const {
+double LocalFrame::DevicePixelRatio(bool with_zoom_factor) const {
   if (!page_)
     return 0;
 
   double ratio = page_->InspectorDeviceScaleFactorOverride();
-  ratio *= PageZoomFactor();
+  // with_zoom_factor is default true
+  if (with_zoom_factor)
+    ratio *= PageZoomFactor();
+  else
+    ratio = page_zoom_factor_;
   return ratio;
 }
 
@@ -3331,6 +3339,11 @@ void LocalFrame::DidBufferLoadWhileInBackForwardCache(
 void LocalFrame::SetScaleFactor(float scale_factor) {
   DCHECK(!GetDocument() || !GetDocument()->Printing());
   DCHECK(IsMainFrame());
+
+  if (scale_factor <= -999) {
+    GetPage()->DisableScreenEmulated();
+    return;
+  }
 
   const PageScaleConstraints& constraints =
       GetPage()->GetPageScaleConstraintsSet().FinalConstraints();
