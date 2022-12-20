f740f3df4ce52ee8a058c035c03dc83ef5954c9f
uazo <uazo@users.noreply.github.com>
2022-12-20 11:06:42 +0000

    Viewport Protection flag
    Scale the viewport and the screen by a random factor to
    prevent coordinate-based fingerprinting scripts.
    The factor is changed at each change of origin.
    The feature is controlled by a feature flag (default enabled)
    
    Require: Content-settings-infrastructure.patch
    
    Change-Id: If2b917e8aeb7d818922a97f71e12f02803aa9158

--- third_party/blink/renderer/core/page/page.h
+++ third_party/blink/renderer/core/page/page.h
@@ -550,6 +550,10 @@ class CORE_EXPORT Page final : public GarbageCollected<Page>,
   // See https://explainers-by-googlers.github.io/partitioned-popins/
   bool IsPartitionedPopin() const;
 
+  void CalculateEmulatedScreenSetting(LocalFrame* frame, bool force = false);
+  bool IsScreenEmulated() { return is_screen_emulated_; }
+  void DisableScreenEmulated();
+
  private:
   friend class ScopedPagePauser;
   class CloseTaskHandler;
@@ -721,6 +725,11 @@ class CORE_EXPORT Page final : public GarbageCollected<Page>,
   // only set for the MPArch implementation and is true when the corresponding
   // browser side FrameTree has the FrameTree::Type of kFencedFrame.
   bool is_fenced_frame_tree_ = false;
+
+  bool is_screen_emulated_ = false;
+  bool disable_screen_emulated_ = false;
+  double override_window_scale_factor_ = 0;
+  double override_zoom_factor_ = 0;
 
   // This tracks the mode that the fenced frame is set to.
   blink::FencedFrame::DeprecatedFencedFrameMode fenced_frame_mode_ =
