c54831dc85e902c0fda63df0d49cbc7de204a3d3
uazo <uazo@users.noreply.github.com>
2022-12-20 11:06:42 +0000

    Viewport Protection flag
    Scale the viewport and the screen by a random factor to
    prevent coordinate-based fingerprinting scripts.
    The factor is changed at each change of origin.
    The feature is controlled by a feature flag (default enabled)
    
    Require: Content-settings-infrastructure.patch
    
    Change-Id: If2b917e8aeb7d818922a97f71e12f02803aa9158

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -1995,6 +1995,14 @@ void WebFrameWidgetImpl::ApplyVisualPropertiesSizing(
 
     if (auto* device_emulator = DeviceEmulator()) {
       device_emulator->UpdateVisualProperties(visual_properties);
+      // Shink the view according to browsercontrols
+      size_ = widget_base_->DIPsToCeiledBlinkSpace(
+          device_emulator->ViewWindowSize());
+      View()->ResizeWithBrowserControls(
+          size_.value(),
+          widget_base_->DIPsToCeiledBlinkSpace(
+              widget_base_->VisibleViewportSize()),
+          visual_properties.browser_controls_params);
       return;
     }
 
