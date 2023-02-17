ce5973b0ed2d85d120678943a345383fa72000fa
uazo <uazo@users.noreply.github.com>
2023-02-17 16:23:20 +0000

    Improve the browser sandbox
    by enabling network service sandbox and CIG in windows and
    using the new flags on android
    
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Change-Id: Icccc9b31ecfac9b9f7aa91b0f719ebd8456dd607

--- sandbox/policy/features.cc
+++ sandbox/policy/features.cc
@@ -98,7 +98,7 @@ BASE_FEATURE(kWinSboxZeroAppShim,
 // build. See https://blogs.windows.com/blog/tag/code-integrity-guard/.
 BASE_FEATURE(kNetworkServiceCodeIntegrity,
              "NetworkServiceCodeIntegrity",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -201,4 +201,11 @@ bool IsNetworkSandboxEnabled() {
 #endif  // BUILDFLAG(IS_MAC) || BUILDFLAG(IS_FUCHSIA)
 }
 
+#if BUILDFLAG(IS_ANDROID)
+SET_CROMITE_FEATURE_ENABLED(kUseRendererProcessPolicy);
+SET_CROMITE_FEATURE_ENABLED(kRestrictRendererPoliciesInBaseline);
+#endif
+#if BUILDFLAG(IS_WIN)
+SET_CROMITE_FEATURE_ENABLED(kNetworkServiceSandbox);
+#endif
 }  // namespace sandbox::policy::features
