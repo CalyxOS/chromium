9faa7616a193f99cd6806c0bf30ee64725c47588
Tommy Webb <tommy@calyxinstitute.org>
2023-03-08 12:22:58 -0500

    Disable Safety Check minimally
    Remove Safety Check from Settings and the omnibox, but in case it is
    accessed through some other unknown means, ensure that method for
    performing it (called when clicking the button) does nothing.
    
    Change-Id: Ieaa019e878f2cd39a01393d2bcf8425d70f8f02e

--- chrome/browser/ui/omnibox/omnibox_pedal_implementations.cc
+++ chrome/browser/ui/omnibox/omnibox_pedal_implementations.cc
@@ -1987,7 +1987,6 @@ GetPedalImplementations(bool incognito, bool guest, bool testing) {
   add(new OmniboxPedalManagePasswords());
   add(new OmniboxPedalUpdateCreditCard());
   add(new OmniboxPedalLaunchIncognito());
-  add(new OmniboxPedalRunChromeSafetyCheck());
   add(new OmniboxPedalPlayChromeDinoGame());
   add(new OmniboxPedalManageSiteSettings());
   add(new OmniboxPedalManageChromeSettings());
