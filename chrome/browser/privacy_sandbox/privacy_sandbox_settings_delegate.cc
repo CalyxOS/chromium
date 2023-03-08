42e8e03e2d6e5f9f604321161eace9b4083e8092
Tommy Webb <tommy@calyxinstitute.org>
2023-03-08 14:37:56 -0500

    Disable Privacy Sandbox feature
    Always return that Privacy Sandbox is restricted. This disables the
    feature and removes it from the settings UI.
    
    Change-Id: Ia74053b190d14e5e98db68f0cea81a32673e0453

--- chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_settings_delegate.cc
@@ -76,6 +76,7 @@ PrivacySandboxSettingsDelegate::PrivacySandboxSettingsDelegate(
 PrivacySandboxSettingsDelegate::~PrivacySandboxSettingsDelegate() = default;
 
 bool PrivacySandboxSettingsDelegate::IsPrivacySandboxRestricted() const {
+  if ((true)) return true;
   if (privacy_sandbox::kPrivacySandboxSettings4ForceRestrictedUserForTesting
           .Get()) {
     return true;
