5f23a13557e832d9e87c836e258a094f5199d5b1
Tommy Webb <tommy@calyxinstitute.org>
2023-03-08 12:57:12 -0500

    Disable Password Check feature
    Change-Id: I3bcc24dd8d4c691e47b87d869440cf99071f3fe0

--- chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckFactory.java
+++ chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckFactory.java
@@ -24,6 +24,7 @@ public class PasswordCheckFactory {
      * @return A {@link PasswordCheckImpl} or null if the feature is disabled.
      */
     public static @Nullable PasswordCheck getOrCreate() {
+        if (true) return null;
         if (sPasswordCheck == null) {
             sPasswordCheck = new PasswordCheckImpl();
         }
