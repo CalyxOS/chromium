13f41a4df6f7bb55870f9b73a3f3064f40b1e4f6
Tommy Webb <tommy@calyxinstitute.org>
2023-03-08 13:24:32 -0500

    Remove policy-disabled Privacy and Security prefs
    Remove Safe Browsing, Preload pages, and Touch to Search from the
    Privacy and security settings screen.
    
    Change-Id: Ic86dc8dac25b4e9bc89ab73952ec997ce517395b

--- chrome/android/java/src/org/chromium/chrome/browser/privacy/settings/PrivacySettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/privacy/settings/PrivacySettings.java
@@ -125,15 +125,6 @@ public class PrivacySettings extends PreferenceFragmentCompat
         mIncognitoLockSettings = new IncognitoLockSettings(incognitoReauthPreference);
         mIncognitoLockSettings.setUpIncognitoReauthPreference(getActivity());
 
-        Preference safeBrowsingPreference = findPreference(PREF_SAFE_BROWSING);
-        safeBrowsingPreference.setSummary(
-                SafeBrowsingSettingsFragment.getSafeBrowsingSummaryString(getContext()));
-        safeBrowsingPreference.setOnPreferenceClickListener((preference) -> {
-            preference.getExtras().putInt(
-                    SafeBrowsingSettingsFragment.ACCESS_POINT, SettingsAccessPoint.PARENT_SETTINGS);
-            return false;
-        });
-
         setHasOptionsMenu(true);
 
         mManagedPreferenceDelegate = createManagedPreferenceDelegate();
@@ -141,12 +132,6 @@ public class PrivacySettings extends PreferenceFragmentCompat
         mSearchSuggestions = (ChromeSwitchPreference) findPreference(PREF_SEARCH_SUGGESTIONS);
         mSearchSuggestions.setOnPreferenceChangeListener(this);
         mSearchSuggestions.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
-
-        mContextualSearch = findPreference(PREF_CONTEXTUAL_SEARCH);
-        boolean isContextualSearchEnabled =
-                !ContextualSearchManager.isContextualSearchDisabled();
-        mContextualSearch.setSummary(
-                isContextualSearchEnabled ? R.string.text_on : R.string.text_off);
 
         ChromeSwitchPreference canMakePaymentPref =
                 (ChromeSwitchPreference) findPreference(PREF_CAN_MAKE_PAYMENT);
