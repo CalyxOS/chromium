02aad752119bc8ec5ce761a2c62ab0b0d83aa26d
Tommy Webb <tommy@calyxinstitute.org>
2023-03-08 12:22:58 -0500

    Disable Safety Check minimally
    Remove Safety Check from Settings and the omnibox, but in case it is
    accessed through some other unknown means, ensure that method for
    performing it (called when clicking the button) does nothing.
    
    Change-Id: Ieaa019e878f2cd39a01393d2bcf8425d70f8f02e

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
@@ -279,6 +279,7 @@ class SafetyCheckMediator
 
     /** Triggers all safety check child checks. */
     public void performSafetyCheck() {
+        if (true) return;
         // Cancel pending delayed show callbacks if a new check is starting while any existing
         // elements are pending.
         cancelCallbacks();
