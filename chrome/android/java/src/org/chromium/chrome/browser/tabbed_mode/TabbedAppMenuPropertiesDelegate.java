8529ced3940fb4b50a4d9a48fe0d65de116cede4
Tommy Webb <tommy@calyxinstitute.org>
2023-03-08 15:19:00 -0500

    Remove "Managed browser" from menu temporarily
    This is a temporary change to prevent users from being confused or
    learning to ignore the incorrect "Managed browser" menu item.
    
    TODO: Properly implement a way to differentiate between our policies
    and policies set via some other mechanism, e.g. actual Device Policy.
    
    Change-Id: Iabdf615be5fb0f0d65fb66d8de0cb8640aa57bce

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegate.java
@@ -134,6 +134,7 @@ public class TabbedAppMenuPropertiesDelegate extends AppMenuPropertiesDelegateIm
 
     @Override
     protected boolean shouldShowManagedByMenuItem(Tab currentTab) {
+        if (true) return false;
         return ManagedBrowserUtils.isBrowserManaged(currentTab.getProfile());
     }
 
