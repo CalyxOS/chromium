bca6712464bfce11763c00798c77cdebe400e8c8
uazo <uazo@users.noreply.github.com>
2023-04-19 09:59:18 +0000

    Clear CORS Preflight Cache on clearing data
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Change-Id: I4c47b74616ad4ad448c9dd3cfea44331450c1937

--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -1185,6 +1185,8 @@ void NetworkContext::ClearHttpCache(base::Time start_time,
       base::BindOnce(&NetworkContext::OnHttpCacheCleared,
                      base::Unretained(this), std::move(callback))));
 
+  cors_preflight_controller_.ClearCache();
+
   NetworkServiceMemoryCache* memory_cache = GetMemoryCache();
   if (memory_cache)
     memory_cache->Clear();
