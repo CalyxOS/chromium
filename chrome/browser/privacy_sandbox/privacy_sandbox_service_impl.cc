521546b53a83ba635e8384d8c47fabf4a795690c
uazo <uazo@users.noreply.github.com>
2023-05-02 15:26:46 +0000

    Disable FirstPartySets and StorageAccessAPI
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Change-Id: Id8c5b554fb0ada619f8fddeebd56175aaa94b22f

--- chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
@@ -419,7 +419,7 @@ bool PrivacySandboxServiceImpl::IsRestrictedNoticeEnabled() {
 void PrivacySandboxServiceImpl::SetFirstPartySetsDataAccessEnabled(
     bool enabled) {
   pref_service_->SetBoolean(prefs::kPrivacySandboxRelatedWebsiteSetsEnabled,
-                            enabled);
+                            false);
 }
 
 bool PrivacySandboxServiceImpl::IsFirstPartySetsDataAccessEnabled() const {
