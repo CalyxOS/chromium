024297ccf77197eb2c83e5b4e19ead49881be161
uazo <uazo@users.noreply.github.com>
2023-05-02 15:26:46 +0000

    Disable FirstPartySets and StorageAccessAPI
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Change-Id: Id8c5b554fb0ada619f8fddeebd56175aaa94b22f

--- components/privacy_sandbox/tracking_protection_prefs.cc
+++ components/privacy_sandbox/tracking_protection_prefs.cc
@@ -64,13 +64,13 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
 
   // Tracking Protection Settings Prefs
   registry->RegisterBooleanPref(
-      prefs::kBlockAll3pcToggleEnabled, false,
+      prefs::kBlockAll3pcToggleEnabled, false, // with true enables FPS
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterIntegerPref(
       prefs::kTrackingProtectionLevel,
       static_cast<int>(TrackingProtectionLevel::kStandard),
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
-  registry->RegisterBooleanPref(prefs::kTrackingProtection3pcdEnabled, false);
+  registry->RegisterBooleanPref(prefs::kTrackingProtection3pcdEnabled, false); // with true enables FPS
   registry->RegisterBooleanPref(
       prefs::kEnableDoNotTrack, false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
