bf2928abff109c0b2f7abfc59bd87cdd81ce2815
uazo <uazo@users.noreply.github.com>
2023-05-30 15:13:31 +0000

    Restore LastTabStandingTracker
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: Ic63f7c7ddd4fc2336cb4e4f6b7fc14cfa44ca9fd

--- chrome/browser/content_settings/host_content_settings_map_factory.cc
+++ chrome/browser/content_settings/host_content_settings_map_factory.cc
@@ -11,6 +11,7 @@
 #include "build/buildflag.h"
 #include "chrome/browser/content_settings/one_time_permission_provider.h"
 #include "chrome/browser/permissions/one_time_permissions_tracker_factory.h"
+#include "chrome/browser/permissions/last_tab_standing_tracker_factory.h"
 #include "chrome/browser/profiles/off_the_record_profile_impl.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_key.h"
@@ -59,6 +60,7 @@ HostContentSettingsMapFactory::HostContentSettingsMapFactory()
               .WithAshInternals(ProfileSelection::kOwnInstance)
               .Build()) {
   DependsOn(SupervisedUserSettingsServiceFactory::GetInstance());
+  DependsOn(LastTabStandingTrackerFactory::GetInstance());
 #if BUILDFLAG(IS_ANDROID)
   DependsOn(TemplateURLServiceFactory::GetInstance());
 #endif
