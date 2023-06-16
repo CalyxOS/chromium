d89bf3257c15f352cf89eb5f24ddc473e01fa4dc
uazo <uazo@users.noreply.github.com>
2023-06-16 15:56:35 +0000

    Viewport Protection Site Setting
    Require: Content-settings-infrastructure.patch
    Change-Id: I6f4d496fda761c56c15f422617d5464704e10824

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1498,7 +1498,7 @@ BASE_FEATURE(kPrivateAggregationApiMultipleCloudProviders,
 
 BASE_FEATURE(kViewportProtection,
              "ViewportProtection",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kProcessHtmlDataImmediately,
              "ProcessHtmlDataImmediately",
