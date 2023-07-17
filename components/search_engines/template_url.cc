0d1f80b701739470c7ceeb7327d7c9c0853f0e4e
uazo <uazo@users.noreply.github.com>
2023-07-17 15:24:16 +0000

    Keep Side Panel Companion disabled
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Change-Id: I1ac17290f58079af0488cc772095c8af94ee8a20

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -1830,7 +1830,7 @@ GURL TemplateURL::GenerateSuggestionURL(
 }
 
 bool TemplateURL::IsSideSearchSupported() const {
-  return !side_search_param().empty();
+  return false;
 }
 
 bool TemplateURL::IsSideImageSearchSupported() const {
