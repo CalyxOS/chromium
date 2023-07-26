04d7508a776bed3c402a35daa64bba8fa3c3870c
Tommy Webb <tommy@calyxinstitute.org>
2023-07-26 16:57:20 -0400

    Bring in Brave feature states for privacy/security
    Reviewed Brave's feature state overrides as of brave-core tag v1.81.18,
    and added any that were relevant for privacy or security and applicable
    to Chromium 137. Features that were already set a particular way still
    had comments added so that it will be easier to catch if upstream makes
    unwanted changes to their states.
    
    Now using cromite flags as much as possible.
    
    Testing feature changes (two examples):
    1. AnonymousIframe: https://anonymous-iframe.glitch.me
       Will show the state enabled prior to this change, disabled after.
    2. AutofillServerCommunication: Install and activate Rethink as VPN,
       clear browser storage, do a test search from the omnibar, and look
       for content-autofill.googleapis.com in Rethink logs. It will be
       present before this change, absent after.
    
    brave-core can be found here: https://github.com/brave/brave-core
    
    app/feature_defaults_unittest.cc is a highly useful resource,
    and the work of their privacy/security team is greatly appreciated.
    
    Change-Id: Ic0e67157ff4a747bb05ceba264660bfeb1ac73ef

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -281,4 +281,5 @@ BASE_FEATURE(kPrivacySandboxFirstTimeNoticeV2,
 SET_CROMITE_FEATURE_DISABLED(kPrivacySandboxSettings4);
 SET_CROMITE_FEATURE_ENABLED(kDisablePrivacySandboxPrompts);
 SET_CROMITE_FEATURE_DISABLED(kEnforcePrivacySandboxAttestations);
+#include "cromite_flags/components_privacy_sandbox_privacy_sandbox_features_cc.inc"
 }  // namespace privacy_sandbox
