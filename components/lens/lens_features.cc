70ede476b025d53b3aae5ffeeaf4e9df54c5e54d
Tommy Webb <tommy@calyxinstitute.org>
2023-07-26 16:57:20 -0400

    Bring in Brave feature states for privacy/security
    Reviewed Brave's feature state overrides, as of brave-core tag v1.75.9,
    and added any that were relevant for privacy or security and applicable
    to Chromium 131. Features that were already set a particular way still
    had comments added so that it will be easier to catch if upstream makes
    unwanted changes to their states.
    
    Now using cromite flags as much as possible.
    
    Testing feature changes (two examples):
    1. AnonymousIframe: https://anonymous-iframe.glitch.me
       Will show the state enabled prior to this change, disabled after.
    2. AutofillServerCommunication: Install and activate Rethink as VPN,
       clear browser storage, do a test search from the omnibar, and look
       for content-autofill.googleapis.com in Rethink logs. It will be
       present before this change, absent after.
    
    brave-core can be found here: https://github.com/brave/brave-core
    
    app/feature_defaults_unittest.cc is a highly useful resource,
    and the work of their privacy/security team is greatly appreciated.
    
    Change-Id: Ic0e67157ff4a747bb05ceba264660bfeb1ac73ef

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -736,4 +736,5 @@ base::TimeDelta GetLensOverlaySurveyResultsTime() {
   return kLensOverlaySurveyResultsTime.Get();
 }
 
+SET_CROMITE_FEATURE_DISABLED(kLensOverlay);    // disabled in Brave
 }  // namespace lens::features
