73140c84ba4d951ade2e08ca5aa899c91e7a618d
Tommy Webb <tommy@calyxinstitute.org>
2023-07-26 16:57:20 -0400

    Bring in Brave feature states for privacy/security
    Reviewed Brave's feature state overrides, as of brave-core tag v1.65.9,
    and added any that were relevant for privacy or security and applicable
    to Chromium 122. Features that were already set a particular way still
    had comments added so that it will be easier to catch if upstream makes
    unwanted changes to their states.
    
    Testing feature changes (two examples):
    1. AnonymousIframe: https://anonymous-iframe.glitch.me
       Will show the state enabled prior to this change, disabled after.
    2. AutofillServerCommunication: Install and activate Rethink as VPN,
       clear browser storage, do a test search from the omnibar, and look
       for content-autofill.googleapis.com in Rethink logs. It will be
       present before this change, absent after.
    
    brave-core can be found here: https://github.com/brave/brave-core
    
    brave_main_delegate_browsertest.cc is a highly useful resource,
    and the work of their privacy/security team is greatly appreciated.
    
    Change-Id: Ic0e67157ff4a747bb05ceba264660bfeb1ac73ef

--- components/subresource_filter/core/common/common_features.cc
+++ components/subresource_filter/core/common/common_features.cc
@@ -6,7 +6,9 @@
 
 namespace subresource_filter {
 
-BASE_FEATURE(kAdTagging, "AdTagging", base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kAdTagging,
+             "AdTagging",                              // disabled by default
+             base::FEATURE_DISABLED_BY_DEFAULT);       // in Brave
 
 BASE_FEATURE(kTPCDAdHeuristicSubframeRequestTagging,
              "TPCDAdHeuristicSubframeRequestTagging",
