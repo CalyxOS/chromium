f4cbc374f8eab780673cffed9f4fd26cca993447
Tommy Webb <tommy@calyxinstitute.org>
2023-08-09 20:18:08 -0400

    Fix build after "Disable fetching of all field trials"
    Change-Id: Iaa1b158db2ea418b011cc1ae640107716fbd1ad7

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/SafeModeVariationsSeedContentProvider.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/SafeModeVariationsSeedContentProvider.java
@@ -82,15 +82,7 @@ public class SafeModeVariationsSeedContentProvider extends ContentProvider {
     }
 
     private Boolean awaitSeedResults() {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        VariationsSeedHolder.getInstance().hasSeedUpdateCompletedAsync(
-                () -> { countDownLatch.countDown(); });
-        try {
-            return countDownLatch.await(TIMEOUT_IN_MILLIS, TimeUnit.MILLISECONDS);
-        } catch (InterruptedException e) {
-            Log.w(TAG, e.toString());
-            return false;
-        }
+        return false;
     }
 
     @Override
