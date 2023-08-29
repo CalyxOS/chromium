e26e00b20939cde842c207a241ac694e26022adb
Tommy Webb <tommy@calyxinstitute.org>
2023-08-29 10:10:47 -0400

    Override Cromite default site settings states
    Adblock: Default ON
    Autoplay: WebView Default ON, Browser still default off
    WebGL: Default ON
    WebRTC: WebView Default ON, Browser still default off
    
    Change-Id: Ia465ef97d29c8a43f8da7be7d919ba6aad8f95bf

--- third_party/blink/renderer/core/html/media/autoplay_policy.cc
+++ third_party/blink/renderer/core/html/media/autoplay_policy.cc
@@ -458,7 +458,7 @@ bool AutoplayPolicy::IsAutoplayAllowedPerSettings() const {
     return false;
   if (auto* settings_client = frame->GetContentSettingsClient()) {
     return settings_client->AllowContentSetting(
-        ContentSettingsType::AUTOPLAY, /*default_value*/ false);
+        ContentSettingsType::AUTOPLAY, /*default_value*/ true);
   }
   return true;
 }
