c3805d7da31bc0891276feb6237a79c3a42da831
Tommy Webb <tommy@calyxinstitute.org>
2023-09-06 10:54:17 +0000

    Build workarounds for unused functions
    Change-Id: Ifaa3c732a4dd62dc64fc293b44a5fe752813490a

--- components/autofill/core/browser/webdata/autofill_table.cc
+++ components/autofill/core/browser/webdata/autofill_table.cc
@@ -411,6 +411,7 @@ void InsertBuilder(sql::Database* db,
           .c_str()));
 }
 
+#if 0 // unused in Bromite
 // Renames the table `from` into `to` and returns true if successful.
 bool RenameTable(sql::Database* db,
                  std::string_view from,
@@ -418,6 +419,7 @@ bool RenameTable(sql::Database* db,
   return db->Execute(
       base::StrCat({"ALTER TABLE ", from, " RENAME TO ", to}).c_str());
 }
+#endif
 
 // Wrapper around `sql::Database::DoesColumnExist()`, because that function
 // only accepts const char* parameters.
