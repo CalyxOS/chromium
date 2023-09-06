c0d83a4f9df5d11f04229ec90475456d7feb7206
Your Name <you@example.com>
2023-09-06 17:55:01 +0000

    Fix chromium build bugs
    because of
    https://bugs.chromium.org/p/chromium/issues/detail?id=1491776#c10
    
    Change-Id: I9afc6e66b4ab03ccf33ec9b73dcb4e821906da07

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -363,6 +363,7 @@ void BindDateTimeChooserForFrame(
 void BindTextSuggestionHostForFrame(
     RenderFrameHost* host,
     mojo::PendingReceiver<blink::mojom::TextSuggestionHost> receiver) {
+  if ((true)) return;
   auto* view =
       RenderWidgetHostViewAndroid::FromRenderWidgetHostView(host->GetView());
   if (!view || !view->text_suggestion_host())
