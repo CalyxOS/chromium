2e02ab53f95ab293d6a58107ae36bbd5fb619bd0
Tommy Webb <tommy@calyxinstitute.org>
2023-09-06 10:54:17 +0000

    Build workarounds for unused functions
    Change-Id: Ifaa3c732a4dd62dc64fc293b44a5fe752813490a

--- chrome/browser/android/tab_android.cc
+++ chrome/browser/android/tab_android.cc
@@ -19,6 +19,7 @@
 #include "base/metrics/user_metrics.h"
 #include "base/trace_event/trace_event.h"
 #include "cc/slim/layer.h"
+#pragma clang diagnostic ignored "-Wunused-function"
 #include "chrome/android/chrome_jni_headers/TabImpl_jni.h"
 #include "chrome/android/chrome_jni_headers/TabUtils_jni.h"
 #include "chrome/browser/android/background_tab_manager.h"
