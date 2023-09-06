2d82bd73f04e1014e07261659d94e12adc90cb31
Your Name <you@example.com>
2023-09-06 17:55:01 +0000

    Fix chromium build bugs
    because of
    https://bugs.chromium.org/p/chromium/issues/detail?id=1491776#c10
    Change-Id: I9afc6e66b4ab03ccf33ec9b73dcb4e821906da07

--- components/omnibox/browser/autocomplete_classifier.cc
+++ components/omnibox/browser/autocomplete_classifier.cc
@@ -99,8 +99,8 @@ void AutocompleteClassifier::Classify(
     metrics::OmniboxEventProto::PageClassification page_classification,
     AutocompleteMatch* match,
     GURL* alternate_nav_url) {
-  TRACE_EVENT1("omnibox", "AutocompleteClassifier::Classify", "text",
-               base::UTF16ToUTF8(text));
+  // TRACE_EVENT1("omnibox", "AutocompleteClassifier::Classify", "text",
+  //              base::UTF16ToUTF8(text));
   DCHECK(!inside_classify_);
   base::AutoReset<bool> reset(&inside_classify_, true);
   AutocompleteInput input(text, page_classification, *scheme_classifier_);
