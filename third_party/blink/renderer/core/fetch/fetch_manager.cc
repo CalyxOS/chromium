fad62c545eccab9859a64b2029633d664d5ec374
uazo <uazo@users.noreply.github.com>
2023-09-28 08:11:00 +0000

    Keep disabled FetchLaterAPI
    Disallow a network request to survive to context destoy.
    For Request with active keepalive, if the request does not
    start from the same origin.
    It also inhibits the blocked request exception to javascript.
    
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Change-Id: I1f1ec46a06be09724c30e3898b6c58d92de47b40

--- third_party/blink/renderer/core/fetch/fetch_manager.cc
+++ third_party/blink/renderer/core/fetch/fetch_manager.cc
@@ -1176,7 +1176,9 @@ void FetchManager::Loader::Failed(
   if (resolver_) {
     ScriptState::Scope scope(GetScriptState());
     if (dom_exception) {
-      resolver_->Reject(dom_exception);
+      if (!GetFetchRequestData()->Keepalive()) {
+        resolver_->Reject(dom_exception);
+      }
     } else {
       v8::Local<v8::Value> value =
           exception_.Get(GetScriptState()->GetIsolate());
@@ -1196,7 +1198,9 @@ void FetchManager::Loader::Failed(
             V8String(GetScriptState()->GetIsolate(),
                      IdentifiersFactory::IdFromToken(*issue_id)));
       }
-      resolver_->Reject(value);
+      if (!GetFetchRequestData()->Keepalive()) {
+        resolver_->Reject(value);;
+      }
       SendHistogram(FetchManagerLoaderCheckPoint::kFailed);
       LogIfKeepalive("Failed");
     }
