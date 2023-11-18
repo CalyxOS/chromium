2fee3bdc4f77bb118211830caa3849812df7ded0
uazo <uazo@users.noreply.github.com>
2023-11-18 09:41:28 +0000

    Add cromite flags support
    Add SET_CROMITE_FEATURE_ENABLED*, SET_CROMITE_FEATURE_DISABLED*
    and CROMITE_FEATURE macros, logic has been adapted from that found
    in brave.
    Allows flags to be defined in separate files.
    Activates a new cromite tab in chrome://flags with only the flags
    added and changed. In android added chrome://flags/cromite
    in the setting ui.
    Currently in wip
    
    Need: bromite-build-utils.patch
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    Change-Id: Idff60cba408b3a031e0c01940b52cebb836787a5

--- net/base/features.cc
+++ net/base/features.cc
@@ -561,4 +561,5 @@ BASE_FEATURE(kDeviceBoundSessions,
              "DeviceBoundSessions",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+#include "cromite_flags/net_base_features_cc.inc"
 }  // namespace net::features
