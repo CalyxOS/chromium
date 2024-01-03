343a2d0d2a2b381b62efaf983d3d3163b8f9971c
Tommy Webb <tommy@calyxinstitute.org>
2024-01-03 12:32:44 +0000

    Fix Override Navigator Language for Calyx
    We do not incorporate Remove-SMS-integration.patch, so we do not need
    an equivalent setAppLanguagePref to be added to AppLocaleUtils.java;
    it was never removed. Having this as a separate patch makes porting
    from Cromite a bit easier.
    
    Change-Id: I99a28cdd61ac76a996a6562374a9f4ac32ba2c86

--- chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
+++ chrome/browser/language/android/java/src/org/chromium/chrome/browser/language/AppLocaleUtils.java
@@ -35,10 +35,6 @@ import java.util.Locale;
 public class AppLocaleUtils {
     private AppLocaleUtils() {}
 
-    public interface InstallListener {
-        void onComplete(boolean success);
-    }
-
     // Value of AppLocale preference when the system language is used.
     public static final @Nullable String APP_LOCALE_USE_SYSTEM_LANGUAGE = null;
 
@@ -108,22 +104,6 @@ public class AppLocaleUtils {
             return APP_LOCALE_USE_SYSTEM_LANGUAGE;
         }
         return locales.get(0).toLanguageTag();
-    }
-
-    public static void setAppLanguagePref(
-            String languageName, InstallListener listener) {
-        InstallListener wrappedListener = (success) -> {
-            if (success) {
-                if (shouldUseSystemManagedLocale()) {
-                    setSystemManagedAppLanguage(languageName);
-                } else {
-                    ChromeSharedPreferences.getInstance().writeString(
-                            ChromePreferenceKeys.APPLICATION_OVERRIDE_LANGUAGE, languageName);
-                }
-            }
-            listener.onComplete(success);
-        };
-        wrappedListener.onComplete(true);
     }
 
     /**
