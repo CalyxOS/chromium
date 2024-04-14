1a9190837c0767f63613ce9924a8cf99c2f83cb1
uazo <uazo@users.noreply.github.com>
2024-04-14 07:35:57 +0000

    Revert remove AllowScript content settings per secondary url
    https://chromium-review.googlesource.com/c/chromium/src/+/5307559
    
    Change-Id: I33552e9becf86e353f133250885fff59b30e7dd3

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -919,12 +919,6 @@ class CORE_EXPORT LocalFrame final
     return *v8_local_compile_hints_producer_;
   }
 
-  // Returns whether script is allowed to run for the current frame. This is a
-  // convenience method that checks both renderer content settings and frame
-  // settings.
-  // Can only be called while the frame is not detached.
-  bool ScriptEnabled();
-
   const WebPrintParams& GetPrintParams() const;
 
   // Returns the `Frame` for which `provisional_frame_ == this`. May only be
