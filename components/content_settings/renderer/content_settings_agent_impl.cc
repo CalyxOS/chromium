4bb40c491164062867ddc01459308778d845dc6c
uazo <uazo@users.noreply.github.com>
2024-04-14 07:35:57 +0000

    Revert remove AllowScript content settings per secondary url
    https: //chromium-review.googlesource.com/c/chromium/src/+/5307559
    Change-Id: I33552e9becf86e353f133250885fff59b30e7dd3

--- components/content_settings/renderer/content_settings_agent_impl.cc
+++ components/content_settings/renderer/content_settings_agent_impl.cc
@@ -168,7 +168,7 @@ void ContentSettingsAgentImpl::DidCommitProvisionalLoad(
   // `allowPlugins()` is called for the new page so that these functions can
   // correctly detect that a piece of content flipped from "not blocked" to
   // "blocked".
-  ClearBlockedContentSettings();
+  ClearBlockedContentSettings(); // do not remove
 
   blink::WebLocalFrame* frame = render_frame()->GetWebFrame();
   if (frame->Parent())
@@ -330,6 +330,46 @@ bool ContentSettingsAgentImpl::AllowStorageAccessSync(
   return result;
 }
 
+bool ContentSettingsAgentImpl::AllowScript(bool enabled_per_settings) {
+  if (!enabled_per_settings)
+    return false;
+
+  blink::WebLocalFrame* frame = render_frame()->GetWebFrame();
+  const auto it = cached_script_permissions_.find(frame);
+  if (it != cached_script_permissions_.end())
+    return it->second;
+
+  // Evaluate the content setting rules before
+  // IsAllowlistedForContentSettings(); if there is only the default rule
+  // allowing all scripts, it's quicker this way.
+  bool allow = true;
+  if (content_setting_rules_) {
+    ContentSetting setting = GetContentSettingFromRules(
+        content_setting_rules_->script_rules,
+        url::Origin(frame->GetDocument().GetSecurityOrigin()).GetURL());
+    allow = setting != CONTENT_SETTING_BLOCK;
+  }
+  allow = allow || IsAllowlistedForContentSettings();
+
+  cached_script_permissions_[frame] = allow;
+  return allow;
+}
+
+bool ContentSettingsAgentImpl::AllowScriptFromSource(
+    bool enabled_per_settings,
+    const blink::WebURL& script_url) {
+  if (!enabled_per_settings)
+    return false;
+
+  bool allow = true;
+  if (content_setting_rules_) {
+    ContentSetting setting = GetContentSettingFromRules(
+        content_setting_rules_->script_rules, script_url);
+    allow = setting != CONTENT_SETTING_BLOCK;
+  }
+  return allow || IsAllowlistedForContentSettings();
+}
+
 bool ContentSettingsAgentImpl::AllowReadFromClipboard() {
   return delegate_->AllowReadFromClipboard();
 }
@@ -391,6 +431,7 @@ void ContentSettingsAgentImpl::DidNotAllowImage() {
 void ContentSettingsAgentImpl::ClearBlockedContentSettings() {
   content_blocked_.clear();
   cached_storage_permissions_.clear();
+  cached_script_permissions_.clear();
 }
 
 bool ContentSettingsAgentImpl::IsAllowlistedForContentSettings() const {
