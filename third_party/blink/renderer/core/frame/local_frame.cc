f45672eaf89be42bf7587507cdfe240367b8808c
uazo <uazo@users.noreply.github.com>
2024-04-14 07:35:57 +0000

    Revert remove AllowScript content settings per secondary url
    https: //chromium-review.googlesource.com/c/chromium/src/+/5307559
    Change-Id: I33552e9becf86e353f133250885fff59b30e7dd3

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -3953,14 +3953,6 @@ bool LocalFrame::ImagesEnabled() {
   return allow_image_renderer && allow_image_content_setting;
 }
 
-bool LocalFrame::ScriptEnabled() {
-  DCHECK(!IsDetached());
-
-  bool allow_script_renderer = GetSettings()->GetScriptEnabled();
-  bool allow_script_content_setting = GetContentSettings()->allow_script;
-  return allow_script_renderer && allow_script_content_setting;
-}
-
 const WebPrintParams& LocalFrame::GetPrintParams() const {
   // If this fails, it's probably because nobody called StartPrinting().
   DCHECK(GetDocument()->Printing());
