4537d05565a2b9354fee2bee763a9e029b0c6fbe
Tommy Webb <tommy@calyxinstitute.org>
2024-05-09 16:24:53 +0000

    Temporarily revert "Fix renderer content settings (part 13 / N)"
    We still rely on many of the removed pieces for site settings.
    
    This reverts commit bf57905d434309d3be8a72c094edbf75de6e490b.
    
    Change-Id: Ia07b313c9065599a717d3633e5acc7471959efe7

--- components/content_settings/core/common/content_settings.h
+++ components/content_settings/core/common/content_settings.h
@@ -93,7 +93,11 @@ struct RendererContentSettingRules {
 
   bool operator==(const RendererContentSettingRules& other) const;
 
+  ContentSettingsForOneType image_rules;
+  ContentSettingsForOneType script_rules;
+  ContentSettingsForOneType popup_redirect_rules;
   ContentSettingsForOneType mixed_content_rules;
+  ContentSettingsForOneType auto_dark_content_rules;
 };
 
 namespace content_settings {
