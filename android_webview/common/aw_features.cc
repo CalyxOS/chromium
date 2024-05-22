de1bd57a61469aca2fe63ee4329142a559702681
uazo <uazo@users.noreply.github.com>
2024-05-22 07:43:04 +0000

    Keep disabled WebViewMediaIntegrityApi
    Disables a new mechanism similar to Web Environment Integrity API in use in webviews
    see https://issuetracker.google.com/issues/327186031
    https://android-developers.googleblog.com/2023/11/increasing-trust-for-embedded-media.html
    
    need: Add-cromite-flags-support.patch
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: Idecffd4a628cc1d974f587915b63abc0d0a6f99c

--- android_webview/common/aw_features.cc
+++ android_webview/common/aw_features.cc
@@ -248,4 +248,5 @@ BASE_FEATURE(kWebViewAutoGrantSanitizedClipboardWrite,
              "WebViewAutoGrantSanitizedClipboardWrite",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+SET_CROMITE_FEATURE_DISABLED(kWebViewMediaIntegrityApiBlinkExtension);
 }  // namespace android_webview::features
