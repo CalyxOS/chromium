c62724b4c5c197215f465c9c420e8ab3f2deaa8a
uazo <uazo@users.noreply.github.com>
2024-05-29 13:43:50 +0000

    Enable Global Privacy Control
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I4bcbc7b2c8b9dfeb40ecf8a185ba6f06ae13e1b2

--- components/privacy_sandbox/tracking_protection_settings_observer.h
+++ components/privacy_sandbox/tracking_protection_settings_observer.h
@@ -22,6 +22,8 @@ class TrackingProtectionSettingsObserver {
   // For observation of DNT.
   virtual void OnDoNotTrackEnabledChanged() {}
 
+  virtual void OnGPCEnabledChanged() {}
+
   // For observation of IP protection.
   virtual void OnIpProtectionEnabledChanged() {}
 
