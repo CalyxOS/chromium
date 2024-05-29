f7b5b016c062e8c23b427ac5f057e10a4e1975a9
uazo <uazo@users.noreply.github.com>
2024-05-29 13:43:50 +0000

    Enable Global Privacy Control
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I4bcbc7b2c8b9dfeb40ecf8a185ba6f06ae13e1b2

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -3556,6 +3556,9 @@ void WebViewImpl::UpdateRendererPreferences(
   GetSettings()->SetCaretBrowsingEnabled(
       renderer_preferences_.caret_browsing_enabled);
 
+  WebRuntimeFeatures::EnableGlobalPrivacyControl(
+    renderer_preferences_.enable_gpc);
+
 #if BUILDFLAG(IS_OZONE)
   GetSettings()->SetSelectionClipboardBufferAvailable(
       renderer_preferences_.selection_clipboard_buffer_available);
