c6403001c7646bfe0842868824b58841b89d1dec
uazo <uazo@users.noreply.github.com>
2024-05-29 13:43:50 +0000

    Enable Global Privacy Control
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I4bcbc7b2c8b9dfeb40ecf8a185ba6f06ae13e1b2

--- components/privacy_sandbox/tracking_protection_settings.h
+++ components/privacy_sandbox/tracking_protection_settings.h
@@ -38,6 +38,8 @@ class TrackingProtectionSettings : public KeyedService {
   // Returns whether "do not track" is enabled.
   bool IsDoNotTrackEnabled() const;
 
+  bool IsGPCEnabled() const;
+
   // Returns whether tracking protection for 3PCD (prefs + UX) is enabled.
   bool IsTrackingProtection3pcdEnabled() const;
 
@@ -72,6 +74,7 @@ class TrackingProtectionSettings : public KeyedService {
 
   // Callbacks for pref observation.
   void OnDoNotTrackEnabledPrefChanged();
+  void OnGPCEnabledPrefChanged();
   void OnBlockAllThirdPartyCookiesPrefChanged();
   void OnTrackingProtection3pcdPrefChanged();
   void OnIpProtectionPrefChanged();
