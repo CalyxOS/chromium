2547033eac7f39dae3358bd55b8b56320e7ac8df
uazo <uazo@users.noreply.github.com>
2024-07-18 14:03:12 +0000

    Restore BookmarkToolbar#setCurrentFolder
    restore https://chromium-review.googlesource.com/c/chromium/src/+/5554182
    
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I713fc98865c159d45394513af1ae6ae297379f30

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbar.java
@@ -10,11 +10,13 @@ import android.view.MenuItem;
 import android.view.View.OnClickListener;
 
 import androidx.annotation.IdRes;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar.OnMenuItemClickListener;
 import androidx.core.view.MenuCompat;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.bookmarks.BookmarkUiState.BookmarkUiMode;
+import org.chromium.components.bookmarks.BookmarkItem;
 import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.components.browser_ui.util.ToolbarUtils;
 import org.chromium.components.browser_ui.widget.selectable_list.SelectableListToolbar;
@@ -30,9 +32,11 @@ import java.util.function.Function;
  */
 public class BookmarkToolbar extends SelectableListToolbar<BookmarkId>
         implements OnMenuItemClickListener, OnClickListener {
+    private BookmarkModel mBookmarkModel;
     private BookmarkOpener mBookmarkOpener;
     private SelectionDelegate<BookmarkId> mSelectionDelegate;
 
+    private @Nullable BookmarkItem mCurrentFolder;
     private boolean mEditButtonVisible;
     private boolean mNewFolderButtonVisible;
     private boolean mNewFolderButtonEnabled;
@@ -58,6 +62,10 @@ public class BookmarkToolbar extends SelectableListToolbar<BookmarkId>
                 getMenu().findItem(R.id.normal_options_submenu).getSubMenu(), true);
 
         setOnMenuItemClickListener(this);
+    }
+
+    void setBookmarkModel(BookmarkModel bookmarkModel) {
+        mBookmarkModel = bookmarkModel;
     }
 
     void setBookmarkOpener(BookmarkOpener bookmarkOpener) {
@@ -165,6 +173,10 @@ public class BookmarkToolbar extends SelectableListToolbar<BookmarkId>
 
     void setCheckedViewMenuId(@IdRes int id) {
         getMenu().findItem(id).setChecked(true);
+    }
+
+    void setCurrentFolder(BookmarkId folder) {
+        mCurrentFolder = mBookmarkModel.getBookmarkById(folder);
     }
 
     void setNavigateBackRunnable(Runnable navigateBackRunnable) {
