25a65bdc6564b2d88c4fc6a6221bf81aa904ac5f
uazo <uazo@users.noreply.github.com>
2024-07-18 14:03:12 +0000

    Restore BookmarkToolbar#setCurrentFolder
    restore https://chromium-review.googlesource.com/c/chromium/src/+/5554182
    
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I713fc98865c159d45394513af1ae6ae297379f30

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarCoordinator.java
@@ -55,6 +55,7 @@ public class BookmarkToolbarCoordinator {
                 searchDelegate, R.string.bookmark_toolbar_search, R.id.search_menu_id);
 
         mModel = new PropertyModel.Builder(BookmarkToolbarProperties.ALL_KEYS).build();
+        mModel.set(BookmarkToolbarProperties.BOOKMARK_MODEL, bookmarkModel);
         mModel.set(BookmarkToolbarProperties.BOOKMARK_OPENER, bookmarkOpener);
         mModel.set(BookmarkToolbarProperties.SELECTION_DELEGATE, selectionDelegate);
         mModel.set(BookmarkToolbarProperties.BOOKMARK_UI_MODE, BookmarkUiMode.LOADING);
