a05a0fed8564589f4ce6ae7e29a59108395ad35f
uazo <uazo@users.noreply.github.com>
2024-07-18 14:03:12 +0000

    Restore BookmarkToolbar#setCurrentFolder
    restore https://chromium-review.googlesource.com/c/chromium/src/+/5554182
    
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I713fc98865c159d45394513af1ae6ae297379f30

--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarProperties.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarProperties.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.bookmarks;
 
 import android.view.View;
 
+import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.chrome.browser.bookmarks.BookmarkUiState.BookmarkUiMode;
 import org.chromium.components.browser_ui.widget.selectable_list.SelectionDelegate;
 import org.chromium.ui.modelutil.PropertyKey;
@@ -22,6 +23,8 @@ import java.util.function.Function;
  */
 class BookmarkToolbarProperties {
     /** Dependencies */
+    static final WritableObjectPropertyKey<BookmarkModel> BOOKMARK_MODEL =
+            new WritableObjectPropertyKey<>();
     static final WritableObjectPropertyKey<SelectionDelegate> SELECTION_DELEGATE =
             new WritableObjectPropertyKey<>();
 
@@ -68,6 +71,10 @@ class BookmarkToolbarProperties {
     static final WritableObjectPropertyKey<Boolean> FAKE_SELECTION_STATE_CHANGE =
             new WritableObjectPropertyKey<>(/* skipEquality= */ true);
 
+    /** Bookmark state properties. */
+    static final WritableObjectPropertyKey<BookmarkId> CURRENT_FOLDER =
+            new WritableObjectPropertyKey<>(/* skipEquality= */ true);
+
     /** Callables to delegate business logic back to the mediator */
     static final WritableObjectPropertyKey<Function<Integer, Boolean>> MENU_ID_CLICKED_FUNCTION =
             new WritableObjectPropertyKey<>();
@@ -90,6 +97,8 @@ class BookmarkToolbarProperties {
         NEW_FOLDER_BUTTON_VISIBLE,
         NEW_FOLDER_BUTTON_ENABLED,
         NAVIGATION_BUTTON_STATE,
+        BOOKMARK_MODEL,
+        CURRENT_FOLDER,
         SORT_MENU_IDS,
         SORT_MENU_IDS_ENABLED,
         CHECKED_SORT_MENU_ID,
