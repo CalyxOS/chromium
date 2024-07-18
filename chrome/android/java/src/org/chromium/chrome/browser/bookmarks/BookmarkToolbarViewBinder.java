09c998679362a6b9a476846f62060d97de942df0
uazo <uazo@users.noreply.github.com>
2024-07-18 14:03:12 +0000

    Restore BookmarkToolbar#setCurrentFolder
    restore https://chromium-review.googlesource.com/c/chromium/src/+/5554182
    
    License: GPL-2.0-or-later - https://spdx.org/licenses/GPL-2.0-or-later.html
    
    Change-Id: I713fc98865c159d45394513af1ae6ae297379f30

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkToolbarViewBinder.java
@@ -13,6 +13,8 @@ class BookmarkToolbarViewBinder {
     public static void bind(PropertyModel model, BookmarkToolbar bookmarkToolbar, PropertyKey key) {
         if (key == BookmarkToolbarProperties.BOOKMARK_OPENER) {
             bookmarkToolbar.setBookmarkOpener(model.get(BookmarkToolbarProperties.BOOKMARK_OPENER));
+        } else if (key == BookmarkToolbarProperties.BOOKMARK_MODEL) {
+            bookmarkToolbar.setBookmarkModel(model.get(BookmarkToolbarProperties.BOOKMARK_MODEL));
         } else if (key == BookmarkToolbarProperties.SELECTION_DELEGATE) {
             bookmarkToolbar.setSelectionDelegate(
                     model.get(BookmarkToolbarProperties.SELECTION_DELEGATE));
@@ -52,6 +54,8 @@ class BookmarkToolbarViewBinder {
         } else if (key == BookmarkToolbarProperties.CHECKED_VIEW_MENU_ID) {
             bookmarkToolbar.setCheckedViewMenuId(
                     model.get(BookmarkToolbarProperties.CHECKED_VIEW_MENU_ID));
+        } else if (key == BookmarkToolbarProperties.CURRENT_FOLDER) {
+            bookmarkToolbar.setCurrentFolder(model.get(BookmarkToolbarProperties.CURRENT_FOLDER));
         } else if (key == BookmarkToolbarProperties.NAVIGATE_BACK_RUNNABLE) {
             bookmarkToolbar.setNavigateBackRunnable(
                     model.get(BookmarkToolbarProperties.NAVIGATE_BACK_RUNNABLE));
