0ac7137beebba2f64d85af4bca87274e992d228c
Not Root <notroot@localhost>
2024-07-22 21:23:17 +0000

    last standing tab: Add optional header
    Change-Id: I79cfdb0b2402b60053f7c8d018bf532422cb8684

--- chrome/browser/permissions/last_tab_standing_tracker_tab_helper.h
+++ chrome/browser/permissions/last_tab_standing_tracker_tab_helper.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_PERMISSIONS_LAST_TAB_STANDING_TRACKER_TAB_HELPER_H_
 #define CHROME_BROWSER_PERMISSIONS_LAST_TAB_STANDING_TRACKER_TAB_HELPER_H_
 
+#include "absl/types/optional.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
