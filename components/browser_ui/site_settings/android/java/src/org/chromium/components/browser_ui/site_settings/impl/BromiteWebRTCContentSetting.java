81301d93cd4651255a7006730dd28afa03c00e56
Tommy Webb <tommy@calyxinstitute.org>
2024-07-29 18:59:22 +0000

    String improvements for "Add webRTC site settings"
    Change-Id: Ibe66881b4c68380b58eaec62273eb0e7216dcd69

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/impl/BromiteWebRTCContentSetting.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/impl/BromiteWebRTCContentSetting.java
@@ -51,7 +51,7 @@ public class BromiteWebRTCContentSetting extends BromiteCustomContentSetting {
             /*defaultDisabledValue*/ getDefaultDisabledValue(),
             /*enabledSummary*/ R.string.website_settings_category_webrtc_enabled,
             /*disabledSummary*/ R.string.website_settings_category_webrtc_disabled,
-            /*summaryOverrideForScreenReader*/ 0);
+            /*summaryOverrideForScreenReader*/ R.string.website_settings_category_webrtc_a11y);
     }
 
     @Override
