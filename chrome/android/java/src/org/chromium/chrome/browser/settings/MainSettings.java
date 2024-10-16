68c012983bb74d53cca3ae084a4793ef87301d1b
Tommy Webb <tommy@calyxinstitute.org>
2024-10-16 14:27:16 +0000

    Do not use legacy settings order
    We don't want to have to keep editing two different XML files,
    main_preferences.xml and main_preferences_legacy.xml. So we decide
    never to use the legacy ordering.
    
    Change-Id: I5075fd0e7a3a3f57d7b7a4f5ee9940191f864a45

--- chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
@@ -664,7 +664,6 @@ public class MainSettings extends ChromeBaseSettingsFragment
     }
 
     private boolean useLegacySettingsOrder() {
-        return !ChromeFeatureList.isEnabled(
-                AutofillFeatures.AUTOFILL_VIRTUAL_VIEW_STRUCTURE_ANDROID);
+        return false;
     }
 }
